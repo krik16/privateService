3e9880bd16f23b94dbdb92a144287c074719b723
kejun <kejun@kejun-PC>
2015-07-20 11:08:09 +0800

    tms

--- easy-tms/src/main/java/com/rongyi/tms/service/impl/MqReceiverServiceImpl.java
+++ easy-tms/src/main/java/com/rongyi/tms/service/impl/MqReceiverServiceImpl.java
@@ -11,6 +11,7 @@ package com.rongyi.tms.service.impl;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.springframework.amqp.core.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,7 @@ import com.rongyi.core.constant.PaymentEventType;
 import com.rongyi.core.constant.TmsEventTypeEnum;
 import com.rongyi.easy.mq.MessageEvent;
 import com.rongyi.tms.mq.Sender;
+import com.rongyi.tms.service.CouponCommissionService;
 import com.rongyi.tms.service.DrawApplyService;
 import com.rongyi.tms.service.MqReceiverService;
 import com.rongyi.tms.service.SalesCommissionService;
@@ -32,6 +34,8 @@ import com.rongyi.tms.service.SalesCommissionService;
  **/
 @Service
 public class MqReceiverServiceImpl implements MqReceiverService {
+	
+	private static final Logger LOGGER = Logger.getLogger(MqReceiverServiceImpl.class);
 
 	@Autowired
 	DrawApplyService drawApplyService;
@@ -39,6 +43,9 @@ public class MqReceiverServiceImpl implements MqReceiverService {
 	@Autowired
 	SalesCommissionService salesCommissionService;
 
+	@Autowired
+	CouponCommissionService couponCommissionService;
+	
 	@Autowired
 	Sender sender;
 	/**
@@ -52,13 +59,19 @@ public class MqReceiverServiceImpl implements MqReceiverService {
 	public void callBack(Message message, Channel channel, String messageString) {
 		MessageEvent messageEvent = MessageEvent.messageToEvent(messageString);
 		if (TmsEventTypeEnum.DRAWAPPLY.getCode().equals(messageEvent.getType())) {// 提现
+			LOGGER.info("导购提现-->");
 			Map<String, Object> messageMap = new HashMap<String, Object>();
 			messageMap = drawApplyService.addDrawApplyByMQ(messageEvent);
 			sender.rpcSend(messageMap, message, channel);
 		} else if (PaymentEventType.PAY_NOTIFY.equals(messageEvent.getType())) {// 支付成功通知
+			LOGGER.info("支付成功通知-->");
 			drawApplyService.updateDrawApplyByMQ(messageEvent);
 		} else if (TmsEventTypeEnum.COMMISSION_POST.getCode().equals(messageEvent.getType())) {// 佣金提审
+			LOGGER.info("商品佣金提审-->");
 			salesCommissionService.addCommissionByMQ(messageEvent);
+		} else if (TmsEventTypeEnum.COUPON_COMMISSION.getCode().equals(messageEvent.getType())){//优惠券佣金
+			LOGGER.info("优惠券佣金提审-->");
+			couponCommissionService.insertByMq(messageEvent);
 		}
 	}
 
