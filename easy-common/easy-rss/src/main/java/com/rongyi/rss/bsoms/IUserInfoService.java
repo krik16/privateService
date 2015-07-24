9d5d1aebcc6c238c95b77e5b2b3543e013b6099e
pingkang <pingkang@rongyi.com>
2015-07-24 17:31:02 +0800

    interface

---
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/bsoms/IUserInfoService.java
@@ -0,0 +1,37 @@
+package com.rongyi.rss.bsoms;
+
+import java.util.Map;
+
+import com.rongyi.core.common.PagingVO;
+import com.rongyi.easy.bsoms.vo.BusinessAccountVO;
+
+/**
+ * 账户用户相关
+ * @author hebo 2015/07/17
+ *
+ */
+public interface IUserInfoService {
+
+	/**
+	 * 分页查询商家账户列表
+	 * @param paramsMap
+	 * @param currentPage
+	 * @param pageSize
+	 * @return
+	 */
+	PagingVO<BusinessAccountVO> searchAccountListByMap(Map<String, Object> paramsMap, int currentPage, int pageSize);
+	
+	/**
+	 * 资金账户：冻结/解冻
+	 * @param userId
+	 * @param isSuspended
+	 * @return
+	 */
+	boolean updateAccountSuspended(String userId, boolean isSuspended);
+
+	/**
+	 * 根据id修改账户状态：启用/停用
+	 * @param paraMap
+	 */
+	void updateAccountStatus(int id, int isSuspended);
+}
