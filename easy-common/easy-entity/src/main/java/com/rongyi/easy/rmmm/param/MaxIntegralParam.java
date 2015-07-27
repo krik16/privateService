82dbf076ec759d15719d429dcc70f491c4647cd6
chenjun <chenjun@rongyi.com>
2015-07-27 14:54:30 +0800

    最大积分参数

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/param/MaxIntegralParam.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.rongyi.easy.rmmm.param;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Copyright (C),上海容易网电子商务有限公司 author chenjun Description ： time ：2015年7月27日
+ * History: 变更记录 <author> <time> <version> <desc> chenjun 2015年7月27日 1.0 创建文件
+ */
+public class MaxIntegralParam implements Serializable {
+
+	private Double orderPrice;
+
+	private Double expressFee;
+
+	private List<OrderListParam> orderList;
+
+	public Double getOrderPrice() {
+		return orderPrice;
+	}
+
+	public void setOrderPrice(Double orderPrice) {
+		this.orderPrice = orderPrice;
+	}
+
+	public Double getExpressFee() {
+		return expressFee;
+	}
+
+	public void setExpressFee(Double expressFee) {
+		this.expressFee = expressFee;
+	}
+
+	public List<OrderListParam> getOrderList() {
+		return orderList;
+	}
+
+	public void setOrderList(List<OrderListParam> orderList) {
+		this.orderList = orderList;
+	}
+
+}
