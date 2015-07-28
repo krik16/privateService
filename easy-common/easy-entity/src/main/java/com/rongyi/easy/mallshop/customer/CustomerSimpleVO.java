62418689b5c030257592417e5b1d0166f53fe708
xieguoqiang <12345678>
2015-07-28 18:32:40 +0800

    810需求-顾客模块-接口实现。

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/mallshop/customer/CustomerSimpleVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/mallshop/customer/CustomerSimpleVO.java
@@ -31,6 +31,12 @@ public class CustomerSimpleVO implements Serializable {
      */
     private String memoName;
 
+
+    /**
+     * 容易逛用户ID
+     */
+    private String userId;
+
     public Integer getId() {
         return id;
     }
@@ -69,5 +75,13 @@ public class CustomerSimpleVO implements Serializable {
 
     public void setMemoName(String memoName) {
         this.memoName = memoName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 }
