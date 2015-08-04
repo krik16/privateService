ccdad6a917246561698aa591430b465cfdb7eb51
huanglijun@rongyi.com <huanglijun@rongyi.com>
2015-08-04 17:43:10 +0800

    容易逛-优惠券接口-调整名称

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/coupon/MMCouponService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/coupon/MMCouponService.java
@@ -1,24 +1,18 @@
 package com.rongyi.rss.coupon;
 
 
-import java.util.List;
-import java.util.Map;
-
+import com.rongyi.easy.coupon.entity.Coupon;
+import com.rongyi.easy.coupon.old_vo.OldCouponVO;
 import org.bson.types.ObjectId;
 
-
-import com.rongyi.easy.coupon.entity.Coupon;
-
-
-
-import com.rongyi.easy.coupon.old_vo.OldCouponVO;
+import java.util.List;
 
 
 /**
  * 1.优惠券订单提交  2.查询优惠详情浏览次数
  *
  * @author hebo
- * @Description: TODO(优惠券模板接口，提供给mall-shop的controller使用)
+ *         优惠券模板接口，提供给mall-shop的controller使用)
  * @date 2015年6月8日 下午
  */
 public interface MMCouponService {
@@ -31,7 +25,6 @@ public interface MMCouponService {
      * @return
      */
     public Coupon getCouponById(ObjectId id);
-
     /**
      * 老优惠:通过id修改优惠券详情浏览次数
      *
@@ -39,27 +32,19 @@ public interface MMCouponService {
      * @return FavourableVO
      */
     public void updateCouponVistiedCount(String couponId, int visitedCount);
-
-//	/**
-//	 * 通过优惠券id得到优惠券详情，再封装到list返回
-//	 *
-//	 * @param grouponIds
-//	 * @return map
-//	 * @throws Exception
-//	 */
-//	public Map<String, Object> buildGroupMap(String couponId, String userId) throws Exception;
-	
-	  /**
+    /**
      * 通过ids得懂啊oldCOuponVO  List
+     *
      * @param ids
      * @return
      */
-    public List<OldCouponVO> getCouponDetailLIstOne(List<String> ids);
+    public List<OldCouponVO> getCouponDetailList(List<String> ids);
 
     /**
      * 推荐优惠券首页（优化）
+     *
      * @param ids
      * @return
      */
-    public List<OldCouponVO> getCouponIndexLIstOne(List<String> ids);
+    public List<OldCouponVO> getCouponIndex(List<String> ids);
 }
