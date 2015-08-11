9ea73d176227a4a402fd7c9a966a3ee4e2359254
hebo <hebo@rongyi.com>
2015-08-11 13:47:29 +0800

    商家角色修改

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/bsoms/entity/BRolesAuthorities.java
@@ -0,0 +1,35 @@
+package com.rongyi.easy.bsoms.entity;
+
+import java.io.Serializable;
+
+public class BRolesAuthorities implements Serializable{
+    private Integer id;
+
+    private Integer roleId;
+
+    private Integer authorityId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public Integer getAuthorityId() {
+        return authorityId;
+    }
+
+    public void setAuthorityId(Integer authorityId) {
+        this.authorityId = authorityId;
+    }
+}
--- easy-common/easy-rss/src/main/java/com/rongyi/rss/bsoms/IRoleService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/bsoms/IRoleService.java
@@ -4,10 +4,25 @@ import java.util.List;
 import java.util.Map;
 
 import com.rongyi.core.common.PagingVO;
+import com.rongyi.easy.bsoms.entity.BAuthorities;
 import com.rongyi.easy.bsoms.entity.BRoles;
+import com.rongyi.easy.bsoms.vo.MerchantRoleVO;
 
 public interface IRoleService {
-	Integer saveOrUpdate(Map paramsMap,List auths,Integer optId) throws Exception;
+	Integer saveOrUpdate(Map<String, Object> paramsMap,List<String> auths,Integer optId) throws Exception;
 	
 	PagingVO<BRoles> pagingRolesList(Map<String, Object> paramsMap,int startRecordIndex, int pageSize) throws Exception;
+
+	/**
+	 * 通过主键获取角色VO（包含关联信息）
+	 * @param id
+	 * @return
+	 */
+	MerchantRoleVO getRoleVOById(Integer id);
+	/**
+	 * 通过角色id获取改角色对应的权限
+	 * @param id
+	 * @return
+	 */
+	List<BAuthorities> getAuthoritiesByRoleId(Integer id);
 }
