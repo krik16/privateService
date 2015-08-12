cb6b13871429fe1902d1bd243d49d47cbc0e45ee
yujisun <yujisun@rongyi.com>
2015-08-12 16:33:12 +0800

    恢复

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/coupon/IValidCouponCodeService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/coupon/IValidCouponCodeService.java
@@ -11,13 +11,13 @@ public interface IValidCouponCodeService {
 
 	/**
      * 优惠券验证
-     * @param orderNo
+     * @param couponCode
      * @param mallId
      * @param shopId
      * @param useMode
-     * @param checkNum
+     * @param couponId
      * @return
      */
-    public ResponseResult validateCouponCode(String orderNo, String mallId, String shopId, Long useMode, Integer checkNum);
+    public ResponseResult validateCouponCode(String couponCode, String mallId, String shopId, Long useMode, String couponId);
     
 }
