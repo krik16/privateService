8284e8b337f6f957572301416c3084c86e14a38c
lijing <lijing@rongyi.com>
2015-08-13 15:48:01 +0800

    xx

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/coupon/param/operation/RedenvelopeParam.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/coupon/param/operation/RedenvelopeParam.java
@@ -21,18 +21,26 @@ public class RedenvelopeParam implements Serializable{
 	private Integer relatedType;
 	/**商品的Id*/
 	private String productId;
-	/**商场的id*/
+	/**关联商场的id*/
 	private String mallId;
-	/**商场的名字*/
+	/**关联商场的名字*/
 	private String mallName;
-	/**店铺的id*/
+	/**关联店铺的id*/
 	private String shopId;
-	/**店铺的名字*/
+	/**关联店铺的名字*/
 	private String shopName;
 	/**发布渠道  ：卡券发布渠道：大运营平台[0], 商家管理后台[1]  sorceTarget 现在对应 publish_channel   以前的渠道有信息渠道 0：大运营管理平台 1：百货管理平台 2:摩店管理平台 3：摩店App   现在对应关系 除了百货管理平台对应商家关联渠道外 其他都对应 大运营平台*/
 	private Integer publish_channel;
 	/**卷的名字      title对应name*/
 	private String name;
+	/**红包关联的商品的商场id*/
+	private String productMallId;
+	/**红包关联的商品的店铺id*/
+	private String productShopId;
+	/**红包关联的商品的商场名称*/
+	private String productMallName;
+	/**红包关联的商品的店铺名称*/
+	private String productShopName;
 	/**这个是关联活动的    1表示关联翻牌购   2表示关联推送  0表示没有关联*/
 	private Integer activtiyStatus;
 	/**要排序的字段名字*/
@@ -49,8 +57,8 @@ public class RedenvelopeParam implements Serializable{
 	private Date publicEndDate;
 	/**当前卷的价格*/
 	private Double currentPrice;
-	/**这个是以前的状态状态(审核中[0]、已上线[1]、已使用[2]、已过期[3]、已下线[4]  现在不用了*/
-	//public String status;
+	/**这个是以前的状态状态(审核中[0]、已上线[1]、已使用[2]、已过期[3]、已下线[4]  以前传入的是0，1 对应以前的status  现在  1代表未使用 ，未过期，为下架*/
+	public Integer noStatus;
 	/**这个是库存  0代表库存==0  1代表库存要大于0 2代表库存大于等于0*/
 	private Integer stockCount;
 	/**当前页数 */
@@ -173,41 +181,33 @@ public class RedenvelopeParam implements Serializable{
 		this.name = name;
 	}
 
-
 	public Integer getActivtiyStatus() {
 		return activtiyStatus;
 	}
-
 
 	public void setActivtiyStatus(Integer activtiyStatus) {
 		this.activtiyStatus = activtiyStatus;
 	}
 
-
 	public String getOrderByColumName() {
 		return orderByColumName;
 	}
-
 
 	public void setOrderByColumName(String orderByColumName) {
 		this.orderByColumName = orderByColumName;
 	}
 
-
 	public Integer getOrderAscORDesc() {
 		return orderAscORDesc;
 	}
-
 
 	public void setOrderAscORDesc(Integer orderAscORDesc) {
 		this.orderAscORDesc = orderAscORDesc;
 	}
 
-
 	public Integer getStatus() {
 		return status;
 	}
-
 
 	public void setStatus(Integer status) {
 		this.status = status;
@@ -217,49 +217,84 @@ public class RedenvelopeParam implements Serializable{
 		return publishStartDate;
 	}
 
-
 	public void setPublishStartDate(Date publishStartDate) {
 		this.publishStartDate = publishStartDate;
 	}
-
 
 	public Date getPublicEndDate() {
 		return publicEndDate;
 	}
 
-
 	public void setPublicEndDate(Date publicEndDate) {
 		this.publicEndDate = publicEndDate;
 	}
-
 
 	public Double getCurrentPrice() {
 		return currentPrice;
 	}
 
-
 	public void setCurrentPrice(Double currentPrice) {
 		this.currentPrice = currentPrice;
 	}
-
 
 	public int getCurpage() {
 		return curpage;
 	}
 
-
 	public void setCurpage(int curpage) {
 		this.curpage = curpage;
 	}
-
 
 	public int getPagesize() {
 		return pagesize;
 	}
 
-
 	public void setPagesize(int pagesize) {
 		this.pagesize = pagesize;
+	}
+
+	public String getProductMallId() {
+		return productMallId;
+	}
+
+	public void setProductMallId(String productMallId) {
+		this.productMallId = productMallId;
+	}
+
+	public String getProductShopId() {
+		return productShopId;
+	}
+
+	public void setProductShopId(String productShopId) {
+		this.productShopId = productShopId;
+	}
+
+	public String getProductMallName() {
+		return productMallName;
+	}
+
+	public void setProductMallName(String productMallName) {
+		this.productMallName = productMallName;
+	}
+
+
+	public String getProductShopName() {
+		return productShopName;
+	}
+
+
+	public void setProductShopName(String productShopName) {
+		this.productShopName = productShopName;
+	}
+
+
+	public Integer getNoStatus() {
+		return noStatus;
+	}
+
+
+	public void setNoStatus(Integer noStatus) {
+		this.noStatus = noStatus;
 	}
 	
 }
