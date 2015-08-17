8bec4bde5039ad457a8ede3dda10a03289fa3da4
黄利君 <huanglijun@rongyi.com>
2015-08-17 17:59:59 +0800

    用户优惠券接口调整

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/coupon/MMUserCouponService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/coupon/MMUserCouponService.java
@@ -5,10 +5,9 @@ import com.rongyi.easy.coupon.entity.UserCoupon;
 import com.rongyi.easy.coupon.vo.MMUserCouponVO;
 import com.rongyi.easy.roa.entity.MallEntity;
 import com.rongyi.easy.roa.vo.ShopVO;
+import org.bson.types.ObjectId;
 
 import java.util.List;
-
-import org.bson.types.ObjectId;
 
 /**
  * 1.获取优惠列表   (分页)  2.主键获取用户优惠
@@ -31,12 +30,21 @@ public interface MMUserCouponService {
     public PagingVO<UserCoupon> searchUserCouponList(String userId, Integer status, Integer currentPage, Integer pageSize);
 
     /**
-     * 主键查询用户优惠券
+     * 根据Id查询优惠券
      *
      * @param id
      * @return
      */
-    UserCoupon findUserCouponById(Long id, String userId);
+    UserCoupon findUserCouponById(Long userCouponId);
+
+
+    /**
+     * 根据id,用户Id查询优惠券
+     * @param userCouponId
+     * @param userId
+     * @return
+     */
+    UserCoupon findUserCoupon(Long userCouponId, String userId);
 
     /**
      * 订单Id和couponId查询用户优惠列表
