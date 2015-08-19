d70faed5124b89cd72b4960264eb483e06876488
yuanbo <yuanbo@rongyi.com>
2015-08-19 10:21:27 +0800

    couponcode 对外方法

---
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/coupon/merchant/CouponCodeService.java
@@ -0,0 +1,33 @@
+package com.rongyi.rss.coupon.merchant;
+
+import com.rongyi.easy.coupon.entity.CouponCode;
+
+import java.util.List;
+
+/**
+ * Descriptions:
+ * Created by 袁波
+ * Created at 2015/8/17 10:35.
+ */
+public interface CouponCodeService {
+    /**
+     * 批量插入第三方券码
+     * @param list
+     * @return
+     */
+    public int batchInsert(List<CouponCode> list);
+
+    /**
+     * 批量插入生成的券码
+     * @param list
+     * @return
+     */
+    public int batchGenerateInsert(List<CouponCode> list) ;
+
+    /**
+     * 统计某个卡券的券码
+     * @param couponId
+     * @return
+     */
+    public int countByCouponId(String couponId);
+}
