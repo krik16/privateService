84009833982b34a1b5d6285c02ac1baab549d64d
chenjun <chenjun@rongyi.com>
2015-08-19 15:42:57 +0800

    增加弹窗记录service

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/malllife/roa/ROAUserPopService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/malllife/roa/ROAUserPopService.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package com.rongyi.rss.malllife.service;
+package com.rongyi.rss.malllife.roa;
 
 import com.rongyi.easy.entity.UserPopEntity;
 
@@ -9,12 +9,12 @@ import com.rongyi.easy.entity.UserPopEntity;
  * Copyright (C),上海容易网电子商务有限公司
  * author chenjun
  * Description ：验码成功弹窗记录 
- * time ：2015年8月17日
+ * time ：2015年8月19日
  * History: 变更记录
  * <author>           <time>             <version>        <desc>
- * chenjun            2015年8月17日               1.0              创建文件
+ * chenjun            2015年8月19日               1.0              创建文件
  */
-public interface IUserPopService {
+public interface ROAUserPopService {
 	
 	/**
 	 * 添加弹窗记录
