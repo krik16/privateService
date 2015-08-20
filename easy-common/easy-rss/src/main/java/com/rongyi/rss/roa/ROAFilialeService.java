327f1994e44dbe6e6c567676bda284f7a6ecafde
sujuan <sujuan@rongyi.com>
2015-08-20 18:28:11 +0800

    增加分公司实体 与接口

---
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/roa/ROAFilialeService.java
@@ -0,0 +1,26 @@
+package com.rongyi.rss.roa;
+
+import java.util.List;
+import java.util.Map;
+
+import com.rongyi.easy.roa.vo.FilialeVo;
+
+/**
+ * 分公司 
+ * @author user
+ *
+ */
+public interface ROAFilialeService {
+	/**
+	 * 查询分公司列表 
+	 * @param paramMap
+	 *             id  分公司ID
+	 *             brandId  集团ID
+	 *             name  集团名称
+	 * @param currpage 
+	 * @param pageSize
+	 * @return
+	 */
+	public List<FilialeVo> getFilialeList(Map paramMap,int currpage,int pageSize);
+	
+}
