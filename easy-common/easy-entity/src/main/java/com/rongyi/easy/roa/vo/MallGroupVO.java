26fef04f51f3aa31fdb1f25fb3cb1325e9109400
yuanbo <yuanbo@rongyi.com>
2015-09-01 16:16:09 +0800

    更改集团VO

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/roa/vo/MallGroupVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/roa/vo/MallGroupVO.java
@@ -1,31 +1,47 @@
 package com.rongyi.easy.roa.vo;
 
 import java.io.Serializable;
+import java.util.List;
 
-public class MallGroupVO implements Serializable{
+public class MallGroupVO implements Serializable {
     private static final long serialVersionUID = 1L;
-	private String id;//集团ID
-	private String name;//集团名称
-	private Integer status;
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public Integer getStatus() {
-		return status;
-	}
-	public void setStatus(Integer status) {
-		this.status = status;
-	}
-	
-	
-	
+    private String id;//集团ID
+    private String name;//集团名称
+    private Integer status;
+    private List<String> mallIds;
+
+    public List<String> getMallIds() {
+        return mallIds;
+    }
+
+    public void setMallIds(List<String> mallIds) {
+        this.mallIds = mallIds;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+
+
 }
