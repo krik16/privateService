95443efaaf4bd36e7b73306cbc7ead3b4948f128
黄利君 <huanglijun@rongyi.com>
2015-09-10 16:05:00 +0800

    结构调整

---
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/content/RoaHomePageService.java
@@ -0,0 +1,23 @@
+package com.rongyi.rss.content;
+
+import com.rongyi.core.bean.ResponseData;
+
+/**
+ * 容易逛首页配置业务接口
+ * 只提供给malllife使用
+ * 内容首页配置
+ * Created by Breggor on 2015/9/10.
+ */
+public interface ROAHomePageService {
+
+
+    /**
+     * 根据城市与用户查询首页配置信息
+     *
+     * @param cityId 城市Id
+     * @param cityId 城市名
+     * @param userId 用户Id
+     * @return
+     */
+    ResponseData findHomePageBy(String cityId, String cityName, String userId);
+}
