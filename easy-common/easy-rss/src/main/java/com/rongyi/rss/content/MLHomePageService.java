d27934579efeaa832a812954804fcc9b6e443e6a
黄利君 <huanglijun@rongyi.com>
2015-09-10 14:25:00 +0800

    提供malllife首页配置接口

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/content/MLHomePageService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/content/MLHomePageService.java
@@ -1,7 +1,7 @@
 package com.rongyi.rss.content;
 
 
-import java.util.Map;
+import com.rongyi.core.bean.ResponseData;
 
 /**
  * 容易逛首页配置业务接口
@@ -13,11 +13,12 @@ import java.util.Map;
 public interface MLHomePageService {
 
     /**
-     * 根据城市Id/userId查询首页配置信息
+     * 根据城市与用户查询首页配置信息
      *
-     * @param cityId 城市
+     * @param cityId 城市Id
+     * @param cityId 城市名
      * @param userId 用户Id
      * @return
      */
-    Map<String, Object> findHomePageBy(String cityId, String userId);
+    ResponseData findHomePageBy(String cityId, String cityName, String userId);
 }
