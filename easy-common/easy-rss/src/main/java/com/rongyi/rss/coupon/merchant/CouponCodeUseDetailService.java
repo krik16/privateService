394df269b69ff96572a12a2302586576006580af
yuanbo <yuanbo@rongyi.com>
2015-09-11 14:52:41 +0800

    商家卡券修改

---
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/coupon/merchant/CouponCodeUseDetailService.java
@@ -0,0 +1,15 @@
+package com.rongyi.rss.coupon.merchant;
+
+import com.rongyi.easy.coupon.vo.merchant.CouponCodeUseDetailVO;
+import com.rongyi.easy.coupon.vo.merchant.MerchantPaging;
+import com.rongyi.easy.page.QueryParam;
+
+/**
+ * Descriptions:
+ * Created by 袁波
+ * Created at 2015/9/10 19:44.
+ */
+public interface CouponCodeUseDetailService {
+
+    public MerchantPaging<CouponCodeUseDetailVO> findUseDetailsByPage(QueryParam queryParam);
+}
