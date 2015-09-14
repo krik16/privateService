1be2102933cd980ca90286ce84a23a74ad6d153e
xieguoqiang <12345678>
2015-09-14 17:22:51 +0800

    交易网关-接口迁移-新增多条件查询TradeUserCode接口。

---
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/tradecenter/RoaTradeUserCodeService.java
@@ -0,0 +1,11 @@
+package com.rongyi.rss.tradecenter;
+
+import java.util.Map;
+
+/**
+ * Created by xgq on 2015/9/14.
+ */
+public interface RoaTradeUserCodeService {
+
+    Map<String, Object> findTradeUserCodeForStatistics(Map<String, Object> paramsMap);
+}
