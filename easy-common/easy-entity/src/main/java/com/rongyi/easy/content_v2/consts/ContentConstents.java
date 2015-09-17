6948c4f9f339d58a3b0f6b3d49dce67d21d3400d
黄利君 <huanglijun@rongyi.com>
2015-09-17 17:20:21 +0800

    首页板块参数调整

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/content_v2/consts/ContentConstents.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/content_v2/consts/ContentConstents.java
@@ -1,16 +1,61 @@
 package com.rongyi.easy.content_v2.consts;
 
 /**
+ * 内容常量定义类
  * Created by Breggor on 2015/9/17.
  */
 public interface ContentConstents {
-    interface Type {
-        int mall = 1;
-        int shop = 2;
-        int brand = 3;
-        int product = 4;
+    /**
+     * 首页内容类型
+     */
+    interface HomePageType {
+        /**
+         * 商城
+         */
+        int MALL = 1;
+        /**
+         * 店铺
+         */
+        int SHOP = 2;
+        /**
+         * 品牌
+         */
+        int BRAND = 3;
+        /**
+         * 商铺
+         */
+        int PRODUCT = 4;
+        /**
+         * 代金券
+         */
+        int VOUCHER = 5;
+        /**
+         * 活动
+         */
+        int ACTIVITY = 6;
+        /**
+         * 文章
+         */
+        int ARTICLE = 7;
+        /**
+         * 同城
+         */
+        int SAME_CITY = 8;
+        /**
+         * 优惠
+         */
+        int SPECIAL_OFFER = 9;
+        /**
+         * 外链
+         */
+        int OUTER_LINK = 10;
+        /**
+         * 内链
+         */
+        int INNER_LINK = 11;
+        /**
+         * 抵扣券
+         */
+        int REBATE = 12;
     }
-
-    // 1.代表商场,2.店铺、3.品牌、4.商品、5.代金券、6.活动、7.文章、8.同城、9.优惠、10.外链、11.内链活动、12.抵扣卷
-
 }
