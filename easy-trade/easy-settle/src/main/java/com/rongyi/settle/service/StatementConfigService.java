03b1e8aac8f49ca9feaff7937dc9388e699e7a1c
kejun <kejun@rongyi.com>
2015-09-18 18:57:24 +0800

    对账单重命名为结算系统（trade-settle）

---
+++ easy-trade/easy-settle/src/main/java/com/rongyi/settle/service/StatementConfigService.java
@@ -0,0 +1,69 @@
+/**
+ * @Copyright (C),上海容易网电子商务有限公司	
+ * @Author: 柯军 
+ * @datetime:2015年9月17日上午11:09:52
+ * @Description: TODO
+ *
+ **/
+
+package com.rongyi.settle.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.rongyi.easy.settle.entity.StatementConfig;
+
+/**	
+ * @Author:  柯军
+ * @Description: 对账单配置接口 
+ * @datetime:2015年9月17日上午11:09:52
+ *
+ **/
+
+public interface StatementConfigService {
+	
+	/**	
+	 * @Description: 分页查询
+	 * @param map
+	 * @param currentPage
+	 * @param pageSize
+	 * @return	
+	 * @Author:  柯军
+	 * @datetime:2015年9月17日上午11:23:43
+	 **/
+	public abstract List<StatementConfig> selectPageList(Map<String, Object> map, Integer currentPage, Integer pageSize); 
+	
+	/**	
+	 * @Description: 分页总数 
+	 * @param map
+	 * @return	
+	 * @Author:  柯军
+	 * @datetime:2015年9月17日上午11:23:51
+	 **/
+	public abstract Integer selectPageListCount(Map<String,Object> map);
+	
+	/**	
+	 * @Description: 插入 
+	 * @param statementConfig	
+	 * @Author:  柯军
+	 * @datetime:2015年9月17日上午11:24:02
+	 **/
+	public abstract void insert(StatementConfig statementConfig);
+	
+	/**	
+	 * @Description: 更新
+	 * @param statementConfig	
+	 * @Author:  柯军
+	 * @datetime:2015年9月17日上午11:24:08
+	 **/
+	public abstract void update(StatementConfig statementConfig);
+	
+	/**	
+	 * @Description: 根据id查询 
+	 * @param id	
+	 * @Author:  柯军
+	 * @datetime:2015年9月17日上午11:24:15
+	 **/
+	public abstract StatementConfig selectById(Integer id);
+
+}
---
+++ easy-trade/easy-settle/src/main/java/com/rongyi/settle/service/impl/StatementConfigServiceImpl.java
@@ -0,0 +1,61 @@
+/**
+ * @Copyright (C),上海容易网电子商务有限公司	
+ * @Author: 柯军 
+ * @datetime:2015年9月17日上午11:10:16
+ * @Description: TODO
+ *
+ **/
+
+package com.rongyi.settle.service.impl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.rongyi.core.framework.mybatis.service.impl.BaseServiceImpl;
+import com.rongyi.easy.settle.entity.StatementConfig;
+import com.rongyi.settle.service.StatementConfigService;
+
+/**
+ * @Author: 柯军
+ * @Description: 对账单配置实现
+ * @datetime:2015年9月17日上午11:10:16
+ * 
+ **/
+@Service
+public class StatementConfigServiceImpl extends BaseServiceImpl implements StatementConfigService {
+
+	private static final String NAMESPACE = "com.rongyi.settle.mapper.xml.StatementConfigMapper";
+
+	@Override
+	public List<StatementConfig> selectPageList(Map<String, Object> map, Integer currentPage, Integer pageSize) {
+		map.put("currentPage", currentPage * pageSize);
+		map.put("pageSize", pageSize);
+		return this.getBaseDao().selectListBySql(NAMESPACE + ".selectPageList", map);
+	}
+
+	@Override
+	public Integer selectPageListCount(Map<String, Object> map) {
+		return this.getBaseDao().selectOneBySql(NAMESPACE + ".selectPageListCount", map);
+	}
+
+	@Override
+	public void insert(StatementConfig statementConfig) {
+		this.getBaseDao().insertBySql(NAMESPACE + ".insert", statementConfig);
+	}
+
+	@Override
+	public void update(StatementConfig statementConfig) {
+		this.getBaseDao().updateBySql(NAMESPACE + ".update", statementConfig);
+	}
+
+	@Override
+	public StatementConfig selectById(Integer id) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("id", id);
+		return this.getBaseDao().selectOneBySql(NAMESPACE + ".selectById", map);
+	}
+
+}
