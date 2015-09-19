885e16e0b0282a5ff619e29865a63813e683fb2c
pingkang <pingkang@rongyi.com>
2015-09-19 17:43:10 +0800

    修改返回用户信息

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/ryoms/ROARyUserService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/ryoms/ROARyUserService.java
@@ -11,9 +11,11 @@ public interface ROARyUserService {
 	/**
 	 * 通过service ticket获取登录用户信息
 	 * 返回Map{userInfo:com.rongyi.easy.ryoms.entity.RyUserInfo,authorities:List<String>}
-	 * @param bsst
+	 * @param bsst 
+	 * @param bConvertToObj	true:返回为对象
+	 * 						false:返回为json
 	 * @return
 	 * @throws Exception
 	 */
-	public Map getSessionUser(String ryst) throws Exception;
+	public Map getSessionUser(String ryst,boolean bConvertToObj) throws Exception;
 }
