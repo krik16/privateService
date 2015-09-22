3c4a7aa95edc47797d40c821c4b612728ce9a322
xieguoqiang <12345678>
2015-09-22 17:41:50 +0800

    对账系统 - 查询对账单接口。

---
+++ easy-trade/easy-settle/src/main/java/com/rongyi/settle/service/PaymentStatementService.java
@@ -0,0 +1,21 @@
+package com.rongyi.settle.service;
+
+import com.rongyi.easy.settle.dto.PaymentStatementDto;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by xgq on 2015/9/22.
+ */
+public interface PaymentStatementService {
+    /**
+     * @param map
+     * @param currentPage
+     * @param pageSize
+     * @Description: 分页查询
+     **/
+    List<PaymentStatementDto> selectPageList(Map<String, Object> map, Integer currentPage, Integer pageSize);
+
+    Integer selectPageListCount(Map<String, Object> map);
+}
--- easy-trade/easy-settle/src/main/java/com/rongyi/settle/web/controller/PaymentStatementController.java
+++ easy-trade/easy-settle/src/main/java/com/rongyi/settle/web/controller/PaymentStatementController.java
@@ -1,26 +1,30 @@
 /**
- * @Copyright (C),上海容易网电子商务有限公司	
- * @Author: 柯军 
+ * @Copyright (C), 上海容易网电子商务有限公司
+ * @Author: 柯军
  * @datetime:2015年9月21日上午11:15:53
  * @Description: TODO
- *
  **/
 
 package com.rongyi.settle.web.controller;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
+import com.rongyi.core.bean.ResponseData;
+import com.rongyi.easy.settle.dto.PaymentStatementDto;
+import com.rongyi.settle.constants.CodeEnum;
+import com.rongyi.settle.constants.ConstantEnum;
+import com.rongyi.settle.service.PaymentStatementService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.rongyi.core.bean.ResponseData;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
 
-/**	
- * @Author:  柯军
- * @Description: 对账单列表 
+/**
+ * @Author: 柯军
+ * @Description: 对账单列表
  * @datetime:2015年9月21日上午11:15:53
  *
  **/
@@ -28,134 +32,146 @@ import com.rongyi.core.bean.ResponseData;
 @RequestMapping("/paymentStatement")
 public class PaymentStatementController {
 
-	/**	
-	 * @Description: 对账单列表（包括所有列表，审核列表，商家对账单列表） 
-	 * @param request
-	 * @param map
-	 * @return	
-	 * @Author:  柯军
-	 * @datetime:2015年9月21日下午3:01:04
-	 **/
-	@RequestMapping("/list")
-	public ResponseData list(HttpServletRequest request,@RequestBody Map<String, Object> map){
-		return null;
-	}
-	
-	/**	
-	 * @Description: 生成对账单 
-	 * @param request
-	 * @param map
-	 * @return	
-	 * @Author:  柯军
-	 * @datetime:2015年9月21日下午3:02:46
-	 **/
-	@RequestMapping("/generate")
-	public ResponseData generate(HttpServletRequest request,@RequestBody Map<String, Object> map){
-		return null;
-	}
-	
-	/**	
-	 * @Description: 对账单明细 
-	 * @param request
-	 * @param map
-	 * @return	
-	 * @Author:  柯军
-	 * @datetime:2015年9月21日下午3:02:58
-	 **/
-	@RequestMapping("/info")
-	public ResponseData info(HttpServletRequest request,@RequestBody Map<String, Object> map){
-		return null;
-	}
-	
-	/**	
-	 * @Description: 对账单审核 
-	 * @param request
-	 * @param map
-	 * @return	
-	 * @Author:  柯军
-	 * @datetime:2015年9月21日下午3:03:17
-	 **/
-	@RequestMapping("/verify")
-	public ResponseData verify(HttpServletRequest request,@RequestBody Map<String, Object> map){
-		return null;
-	}
-	
-	/**	
-	 * @Description:  导出对账单明细（财务操作）
-	 * @param request
-	 * @param map
-	 * @return	
-	 * @Author:  柯军
-	 * @datetime:2015年9月21日下午3:03:26
-	 **/
-	@RequestMapping("/exportFinanceExcel")
-	public ResponseData exportFinanceExcel(HttpServletRequest request,@RequestBody Map<String, Object> map){
-		return null;
-	}
-	
-	/**	
-	 * @Description: 作废 
-	 * @param request
-	 * @param map
-	 * @return	
-	 * @Author:  柯军
-	 * @datetime:2015年9月21日下午3:04:09
-	 **/
-	@RequestMapping("/invalid")
-	public ResponseData invalid(HttpServletRequest request,@RequestBody Map<String, Object> map){
-		return null;
-	}
-	
-	/**	
-	 * @Description: 商品订单查询(商家操作) 
-	 * @param request
-	 * @param map
-	 * @return	
-	 * @Author:  柯军
-	 * @datetime:2015年9月21日下午3:04:23
-	 **/
-	@RequestMapping("/merchandiseList")
-	public ResponseData merchandiseList(HttpServletRequest request,@RequestBody Map<String, Object> map){
-		return null;
-	}
-	
-	/**	
-	 * @Description: 导出商品订单明细(商家操作) 
-	 * @param request
-	 * @param map
-	 * @return	
-	 * @Author:  柯军
-	 * @datetime:2015年9月21日下午3:04:46
-	 **/
-	@RequestMapping("exportMerchandiseExcel")
-	public ResponseData exportMerchandiseExcel(HttpServletRequest request,@RequestBody Map<String, Object> map){
-		return null;
-	}
-	
-	/**	
-	 * @Description: 优惠券订单查询(商家操作) 
-	 * @param request
-	 * @param map
-	 * @return	
-	 * @Author:  柯军
-	 * @datetime:2015年9月21日下午3:05:09
-	 **/
-	@RequestMapping("/couponList")
-	public ResponseData couponList(HttpServletRequest request,@RequestBody Map<String, Object> map){
-		return null;
-	}
-	/**	
-	 * @Description:  导出优惠券订单明细(商家操作)  
-	 * @param request
-	 * @param map
-	 * @return	
-	 * @Author:  柯军
-	 * @datetime:2015年9月21日下午3:05:30
-	 **/
-	@RequestMapping("exportCouponExcel")
-	public ResponseData exportCouponExcel(HttpServletRequest request,@RequestBody Map<String, Object> map){
-		return null;
-	}
-	
+    @Autowired
+    private PaymentStatementService paymentStatementService;
+
+    /**
+     * @Description: 对账单列表（包括所有列表，审核列表，商家对账单列表）
+     * @param map
+     * @return
+     * @Author: 柯军
+     * @datetime:2015年9月21日下午3:01:04
+     **/
+    @RequestMapping("/list")
+    @ResponseBody
+    public ResponseData list(@RequestBody Map<String, Object> map) {
+        try {
+            Integer currentPage = Integer.valueOf(map.get("currentPage").toString());
+            List<PaymentStatementDto> list = paymentStatementService.selectPageList(map, currentPage, ConstantEnum.PAGE_SIZE.getCodeInt());
+            Integer count = paymentStatementService.selectPageListCount(map);
+            return ResponseData.success(list, currentPage, ConstantEnum.PAGE_SIZE.getCodeInt(), count);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseData.failure(CodeEnum.FIAL_STATEMENT_LIST.getCodeInt(), CodeEnum.FIAL_STATEMENT_LIST.getValueStr());
+        }
+    }
+
+    /**
+     * @Description: 生成对账单
+     * @param request
+     * @param map
+     * @return
+     * @Author: 柯军
+     * @datetime:2015年9月21日下午3:02:46
+     **/
+    @RequestMapping("/generate")
+    public ResponseData generate(HttpServletRequest request, @RequestBody Map<String, Object> map) {
+        return null;
+    }
+
+    /**
+     * @Description: 对账单明细
+     * @param request
+     * @param map
+     * @return
+     * @Author: 柯军
+     * @datetime:2015年9月21日下午3:02:58
+     **/
+    @RequestMapping("/info")
+    public ResponseData info(HttpServletRequest request, @RequestBody Map<String, Object> map) {
+        return null;
+    }
+
+    /**
+     * @Description: 对账单审核
+     * @param request
+     * @param map
+     * @return
+     * @Author: 柯军
+     * @datetime:2015年9月21日下午3:03:17
+     **/
+    @RequestMapping("/verify")
+    public ResponseData verify(HttpServletRequest request, @RequestBody Map<String, Object> map) {
+        return null;
+    }
+
+    /**
+     * @Description: 导出对账单明细（财务操作）
+     * @param request
+     * @param map
+     * @return
+     * @Author: 柯军
+     * @datetime:2015年9月21日下午3:03:26
+     **/
+    @RequestMapping("/exportFinanceExcel")
+    public ResponseData exportFinanceExcel(HttpServletRequest request, @RequestBody Map<String, Object> map) {
+        return null;
+    }
+
+    /**
+     * @Description: 作废
+     * @param request
+     * @param map
+     * @return
+     * @Author: 柯军
+     * @datetime:2015年9月21日下午3:04:09
+     **/
+    @RequestMapping("/invalid")
+    public ResponseData invalid(HttpServletRequest request, @RequestBody Map<String, Object> map) {
+        return null;
+    }
+
+    /**
+     * @Description: 商品订单查询(商家操作)
+     * @param request
+     * @param map
+     * @return
+     * @Author: 柯军
+     * @datetime:2015年9月21日下午3:04:23
+     **/
+    @RequestMapping("/merchandiseList")
+    public ResponseData merchandiseList(HttpServletRequest request, @RequestBody Map<String, Object> map) {
+        return null;
+    }
+
+    /**
+     * @Description: 导出商品订单明细(商家操作)
+     * @param request
+     * @param map
+     * @return
+     * @Author: 柯军
+     * @datetime:2015年9月21日下午3:04:46
+     **/
+    @RequestMapping("exportMerchandiseExcel")
+    public ResponseData exportMerchandiseExcel(HttpServletRequest request, @RequestBody Map<String, Object> map) {
+        return null;
+    }
+
+    /**
+     * @Description: 优惠券订单查询(商家操作)
+     * @param request
+     * @param map
+     * @return
+     * @Author: 柯军
+     * @datetime:2015年9月21日下午3:05:09
+     **/
+    @RequestMapping("/couponList")
+    public ResponseData couponList(HttpServletRequest request, @RequestBody Map<String, Object> map) {
+        return null;
+    }
+
+    /**
+     * @Description: 导出优惠券订单明细(商家操作)
+     * @param request
+     * @param map
+     * @return
+     * @Author: 柯军
+     * @datetime:2015年9月21日下午3:05:30
+     **/
+    @RequestMapping("exportCouponExcel")
+    public ResponseData exportCouponExcel(HttpServletRequest request, @RequestBody Map<String, Object> map) {
+        return null;
+    }
+
 }
 
