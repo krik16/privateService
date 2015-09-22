649cc9db61f7139275798c761000f64129164c28
黄利君 <huanglijun@rongyi.com>
2015-09-22 18:20:40 +0800

    内容管理-首页配置-新增业务字段

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/roa/vo/ShopPositionVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/roa/vo/ShopPositionVO.java
@@ -3,66 +3,82 @@ package com.rongyi.easy.roa.vo;
 import java.io.Serializable;
 
 
-public class ShopPositionVO implements Serializable{
-	/**
-     * 
+public class ShopPositionVO implements Serializable {
+    /**
+     *
      */
     private static final long serialVersionUID = 1L;
     private String pro;//
-	private String city;
-	private String area;
-	private String business;
-	private String mallId;
-	private String mall;
-	private String floor;
-	private String shopNumber;
-	public String getPro() {
-		return pro;
-	}
-	public void setPro(String pro) {
-		this.pro = pro;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getArea() {
-		return area;
-	}
-	public void setArea(String area) {
-		this.area = area;
-	}
-	public String getBusiness() {
-		return business;
-	}
-	public void setBusiness(String business) {
-		this.business = business;
-	}
-	public String getMall() {
-		return mall;
-	}
-	public void setMall(String mall) {
-		this.mall = mall;
-	}
-	public String getFloor() {
-		return floor;
-	}
-	public void setFloor(String floor) {
-		this.floor = floor;
-	}
-	public String getShopNumber() {
-		return shopNumber;
-	}
-	public void setShopNumber(String shopNumber) {
-		this.shopNumber = shopNumber;
-	}
-	public String getMallId() {
-		return mallId;
-	}
-	public void setMallId(String mallId) {
-		this.mallId = mallId;
-	}
-	
+    private String city;
+    private String area;
+    private String business;
+    private String mallId;
+    private String mall;
+    private String floor;
+    private String shopNumber;
+
+    public String getPro() {
+        return pro;
+    }
+
+    public void setPro(String pro) {
+        this.pro = pro;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getBusiness() {
+        return business;
+    }
+
+    public void setBusiness(String business) {
+        this.business = business;
+    }
+
+    public String getMall() {
+        return mall;
+    }
+
+    public void setMall(String mall) {
+        this.mall = mall;
+    }
+
+    public String getFloor() {
+        return floor;
+    }
+
+    public void setFloor(String floor) {
+        this.floor = floor;
+    }
+
+    public String getShopNumber() {
+        return (shopNumber == null) ? "" : shopNumber;
+    }
+
+    public void setShopNumber(String shopNumber) {
+        this.shopNumber = shopNumber;
+    }
+
+    public String getMallId() {
+        return mallId;
+    }
+
+    public void setMallId(String mallId) {
+        this.mallId = mallId;
+    }
+
 }
