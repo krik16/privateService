8737c28b21baef6df6330910499fb41fc0585640
pingkang <pingkang@rongyi.com>
2015-09-25 10:51:15 +0800

    增加分公司查询

--- easy-common/easy-core/src/main/java/com/rongyi/core/enumerate/RoaEnum.java
+++ easy-common/easy-core/src/main/java/com/rongyi/core/enumerate/RoaEnum.java
@@ -13,11 +13,9 @@ public enum RoaEnum {
     ERROR_SIGN_INVALID(1050002, "签名错误"),
     ERROR_PARAM_INVALID(1050003, "请求参数错误"),
     
-    ERROR_CURRENTPAGE_EMPTY(1080004, "[currentPage]参数错误"),
-    ERROR_PAGESIZE_INVALID(1080005, "[pageSize]参数错误"),
+    ERROR_CURRENTPAGE_EMPTY(1050004, "[currentPage]参数错误"),
+    ERROR_PAGESIZE_INVALID(1050005, "[pageSize]参数错误");
     
-    /** 正常 但没有数据*/
-    ERROR_USER_NOFOUND(1080401, "用户不存在或未登录");
     
     
     private Integer errno; 
