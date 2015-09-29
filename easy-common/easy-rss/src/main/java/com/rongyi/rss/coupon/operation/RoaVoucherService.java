2c7829690922007b45deda1eeaa619d338b1cbc3
liaoqianyang <liaoqianyang@rongyi.com>
2015-09-29 17:38:47 +0800

    代金券接口

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/coupon/operation/RoaVoucherService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/coupon/operation/RoaVoucherService.java
@@ -4,6 +4,8 @@ import com.rongyi.core.common.PagingVO;
 import com.rongyi.easy.coupon.entity.mysql.Coupon;
 import com.rongyi.easy.coupon.param.operation.VoucherParam;
 
+import java.util.Map;
+
 /**
  * 代金券对外接口
  * Created by lqy on 2015/9/25.
@@ -11,4 +13,14 @@ import com.rongyi.easy.coupon.param.operation.VoucherParam;
 public interface RoaVoucherService {
 
     PagingVO<Coupon> findVouchers(VoucherParam param) throws Exception;
+
+    Map<String, Object> getCouponList(Map<String, Object> map, int currentPage, int pageSize);
+
+    boolean saveOrUpdateCoupon(com.rongyi.easy.coupon.entity.Coupon oldCoupon);
+
+    com.rongyi.easy.coupon.entity.Coupon getCouponById(Integer id);
+
+    boolean deleteCoupon(Integer id);
+
+    boolean updateCouponStatus(Integer id, String status);
 }
