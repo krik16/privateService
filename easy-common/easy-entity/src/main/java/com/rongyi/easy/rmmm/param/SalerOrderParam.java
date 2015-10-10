7763ea9e00ac7390842cfc349c987a1868c6d875
chenjun <chenjun@rongyi.com>
2015-10-10 16:31:18 +0800

    OrderFormEntity添加guideype字段

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/param/SalerOrderParam.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/param/SalerOrderParam.java
@@ -2,35 +2,37 @@ package com.rongyi.easy.rmmm.param;
 
 import java.io.Serializable;
 
+import com.rongyi.core.constant.Constants;
+
 /**
- * 卖家版大订单列表参数
-<<<<<<< HEAD
-<<<<<<< HEAD
+ * 卖家版大订单列表参数 <<<<<<< HEAD <<<<<<< HEAD
  * 
  * @author zzp
  * 
  */
 public class SalerOrderParam implements Serializable {
 
-
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
+	/*
+	 * private String status;// 订单状态 1待处理，2未付款，3已完成，4已关闭",
+	 * 
+	 * private String currentPage;// （要请求页）
+	 * 
+	 * private String pageSize;// 每页数据显示数量
+	 */
 
-/*	private String status;// 订单状态 1待处理，2未付款，3已完成，4已关闭",
+	private String guideId;// 导购id
+	private String status;// 订单状态 1待处理，2未付款，3已完成，4已关闭",
 
 	private String currentPage;// （要请求页）
 
-	private String pageSize;// 每页数据显示数量*/
+	private String pageSize;// 每页数据显示数量
 
-	private String guideId;// 导购id
-	private String status;//订单状态 1待处理，2未付款，3已完成，4已关闭",
-	
-	private String currentPage;//（要请求页）
-	
-	private String pageSize;//每页数据显示数量
+	private Integer guideType;// 卖家类型 1是导购 2是买手
 
 	public String getStatus() {
 		return status;
@@ -62,6 +64,14 @@ public class SalerOrderParam implements Serializable {
 
 	public void setGuideId(String guideId) {
 		this.guideId = guideId;
+	}
+
+	public Integer getGuideType() {
+		return guideType;
+	}
+
+	public void setGuideType(Integer guideType) {
+		this.guideType = guideType;
 	}
 
 }
