2c9378b0400ee30256eab19429f2795ec09ec2a1
zhangzhengping <zhangzhengping@rongyi.com>
2015-10-14 16:57:09 +0800

    提现增加字段

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/param/user/AccountParam.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/param/user/AccountParam.java
@@ -21,7 +21,7 @@ public class AccountParam implements Serializable {
     private String bankId;//开户行ID
     private String  accountName;//支付宝姓名/储蓄卡人姓名
     private Integer userId;
-    private String userType;//0导购 1买手
+    private String userType;//1和不传是导购 2买手
 
     public String getType() {
         return type;
