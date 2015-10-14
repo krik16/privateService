cdd84f3d681dd57577dbf4ae9deb2e9dda5a17c1
sujuan <sujuan@rongyi.com>
2015-10-14 21:30:12 +0800

    重载根据邀请码进行更新接口

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/merchant/IInviteCodeService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/merchant/IInviteCodeService.java
@@ -35,6 +35,13 @@ public interface IInviteCodeService {
 	 * @param inviteCode
 	 * @throws MallShopException
 	 */
+	public Integer UpdateByCode(String inviteCode,Integer userId) throws Exception;
+	
+	/**
+	 * 根据邀请码 进行更新
+	 * @param inviteCode
+	 * @throws MallShopException
+	 */
 	public Integer UpdateByCode(String inviteCode,Integer userId,Integer type) throws Exception;
 
 	/**
