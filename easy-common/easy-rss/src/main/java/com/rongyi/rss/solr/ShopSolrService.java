f3c1a4a9ebe68ef2e12f3d9cb4c5c038f8505807
zhengyiliang <zhengyiliang@rongyi.com>
2015-10-17 14:40:27 +0800

    B端店铺检索接口

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/solr/ShopSolrService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/solr/ShopSolrService.java
@@ -9,6 +9,8 @@
 */
 package com.rongyi.rss.solr;
 
+import java.util.List;
+
 import com.rongyi.easy.solr.param.ShopSearchParam;
 import com.rongyi.easy.solr.result.ShopSearchResult;
 
@@ -18,7 +20,35 @@ import com.rongyi.easy.solr.result.ShopSearchResult;
  */
 public interface ShopSolrService {
 	
+	/**
+	 * C端店铺商场搜索主入口
+	 * 
+	 * @author ZhengYl
+	 * @date 2015年10月17日 下午2:37:10 
+	 * @param input
+	 * @return
+	 */
 	public ShopSearchResult poiSearch(ShopSearchParam input);
 
+	/**
+	 * poi更新
+	 * 
+	 * @author ZhengYl
+	 * @date 2015年10月17日 下午2:37:12 
+	 * @param id
+	 * @param mid
+	 * @param status
+	 * @return
+	 */
 	public boolean updatePoiStatus(String id, String mid, int status);
+	
+	/**
+	 * B端店铺关键词检索
+	 * 
+	 * @author ZhengYl
+	 * @date 2015年10月17日 下午2:37:15 
+	 * @param input
+	 * @return
+	 */
+	public List<String> selectShopIndexByName(ShopSearchParam input);
 }
