54bfa0685e4b92650664d9537f423691264ba44f
xiaobo <xiaobo@rongyi.com>
2015-10-20 11:55:16 +0800

    添加commodityType

---
+++ easy-common/easy-core/src/main/java/com/rongyi/core/enumerate/mcmc/CommodityType.java
@@ -0,0 +1,35 @@
+package com.rongyi.core.enumerate.mcmc;
+
+/**
+ * 商品类型
+ * 
+ * @author xiaobo
+ *
+ */
+public enum CommodityType {
+	/**
+	 * 导购
+	 */
+	GUIDE(1),
+
+	/**
+	 * 买手
+	 */
+	BULL(2);
+
+	private int value;
+
+	// 构造方法
+	private CommodityType(int value) {
+		this.value = value;
+	}
+
+	/**
+	 * 获取值
+	 * 
+	 * @return
+	 */
+	public int getValue() {
+		return value;
+	}
+}
