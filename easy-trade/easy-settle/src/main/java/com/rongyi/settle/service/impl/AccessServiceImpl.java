1939c0e25094b24b203d8fc4557f0354ce2cb3ac
xieguoqiang <xieguoqiang@rongyi.com>
2015-10-20 19:26:39 +0800

    结算系统 - 商家账号。

--- easy-trade/easy-settle/src/main/java/com/rongyi/settle/service/impl/AccessServiceImpl.java
+++ easy-trade/easy-settle/src/main/java/com/rongyi/settle/service/impl/AccessServiceImpl.java
@@ -79,8 +79,8 @@ public class AccessServiceImpl implements AccessService {
                 return ResponseData.failure(CodeEnum.FIAL_USER_PARAMS_PAYMENT.getCodeInt(), CodeEnum.FIAL_USER_PARAMS_PAYMENT.getValueStr());
             }
             SessionUserInfo sessionUserInfo = (SessionUserInfo) user.get("userInfo");
-            request.getSession().setAttribute("userName", sessionUserInfo.getUserName());
-            logger.info("当前登录账号=" + sessionUserInfo.getUserName() + " id=" + sessionUserInfo.getId() + " phone=" + sessionUserInfo.getUserPhone());
+            request.getSession().setAttribute("userName", sessionUserInfo.getUserPhone());
+            logger.info("当前登录账号=" + sessionUserInfo.getUserPhone());
         }
         List<String> authorities = (List<String>) user.get("authorities");
 //        logger.info("需要权限: " + needAuthority + " 拥有权限:" + authorities.toString());
