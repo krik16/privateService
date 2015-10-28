4089984a32ce26af285526c08c6f66fbaf8e7b64
xieguoqiang <xieguoqiang@rongyi.com>
2015-10-28 17:02:01 +0800

    卡券兼容 - 测试代码调整。

--- easy-trade/easy-rpb/src/test/java/service/PaymentServiceTest.java
+++ easy-trade/easy-rpb/src/test/java/service/PaymentServiceTest.java
@@ -24,7 +24,6 @@ import com.rongyi.rpb.service.PCWebPageAlipayService;
 import com.rongyi.rpb.service.PaymentItemService;
 import com.rongyi.rpb.service.PaymentService;
 import com.rongyi.rpb.service.impl.RpbServiceImpl;
-import com.rongyi.rss.coupon.RoaCouponOrderService;
 
 public class PaymentServiceTest extends BaseTest {
 
@@ -32,21 +31,18 @@ public class PaymentServiceTest extends BaseTest {
 	PaymentService paymentService;
 
 	@Autowired
-	RoaCouponOrderService roaCouponOrderService;
-	
-	@Autowired
 	PaymentItemService paymentItemService;
 
 	@Autowired
 	AliPaymentService aliPaymentService;
-	
+
 	@Autowired
 	PCWebPageAlipayService pcWebPageAlipayService;
-	
+
 	@Autowired
 	RpbServiceImpl rpbServiceImpl;
-	
-//	 @Test
+
+	//	 @Test
 	public void testSelectPageListBySearch() {
 		Map<String, Object> searchValueMap = null;
 		searchValueMap = new HashMap<String, Object>();
@@ -113,7 +109,7 @@ public class PaymentServiceTest extends BaseTest {
 		paymentService.insertByOrderDetailNum(paymentEntity, "001,002");
 	}
 
-//	 @Test
+	//	 @Test
 	public void testSelectByPrimaryKey() {
 		PaymentEntity paymentEntity = paymentService.selectByPrimaryKey("80");
 		System.err.println(paymentEntity.getPayNo());
@@ -151,7 +147,7 @@ public class PaymentServiceTest extends BaseTest {
 
 	}
 
-//	 @Test
+	//	 @Test
 	public void testValidateOrderNumExist() {
 		PaymentEntity paymentEntity = paymentService.validateOrderNumExist("1000000126496953,1000000272983889", 1, 1);
 		System.err.println("payNo=" + paymentEntity.getPayNo());
@@ -175,10 +171,10 @@ public class PaymentServiceTest extends BaseTest {
 	// @Test
 	public void testGetOrderNumStrsByPayNo() {
 //		List<PaymentEntity> list = paymentService.getOrderNumStrsByPayNo("1000001597030813",Constants.PAYMENT_TRADE_TYPE.TRADE_TYPE0);
-		
+
 	}
 
-//	@Test
+	//	@Test
 	public void testInsert() {
 		PaymentEntity paymentEntity = new PaymentEntity();
 		paymentEntity.setOrderNum("11111");
@@ -186,22 +182,17 @@ public class PaymentServiceTest extends BaseTest {
 		paymentService.insert(paymentEntity);
 	}
 
-//	@Test
-	public void testRoaCouponOrder() {
-		roaCouponOrderService.findOneByOrderNo("");
-	}
-
-//	@Test
+	//	@Test
 	public void testSelectItemByPaymentId(){
 		List<PaymentItemEntity> list = paymentItemService.selectByPaymentId(2171);
 		System.err.println(list.get(0).getDetailNum());
 	}
-	
-//	@Test
+
+	//	@Test
 	public void testQueryOrder(){
-		System.err.println(aliPaymentService.queryOrder("1000001063768572","2015080700001000480061032614").toString());	
+		System.err.println(aliPaymentService.queryOrder("1000001063768572","2015080700001000480061032614").toString());
 	}
-	
+
 	public MessageEvent getRpbEvent(String soruce, String type) {
 		MessageEvent event = new MessageEvent();
 		Map<String, Object> bodyMap = new HashMap<String, Object>();
@@ -216,15 +207,15 @@ public class PaymentServiceTest extends BaseTest {
 		event.setType(type);
 		return event;
 	}
-	
-//	@Test
+
+	//	@Test
 	public void testGetBatchRefundBuyerMessage(){
 		String[] idArray = new String[]{"4693"};
 		//20150812192257707
 		pcWebPageAlipayService.getBatchRefundBuyerMessage(idArray, "test");
 	}
-	
-//	@Test
+
+	//	@Test
 	public void testPaysuccess(){
 		rpbServiceImpl.paySuccessNotify("0818930009601625", 0.00);
 	}
@@ -233,29 +224,29 @@ public class PaymentServiceTest extends BaseTest {
 	public void testSelectByBatchNoAndStatus(){
 		System.err.println(paymentService.selectByBatchNoAndStatus("20150812192552139",2).size());
 	}
-	
-//	@Test
+
+	//	@Test
 	public void testSelectByTradeType(){
 		PaymentEntity paymentEntity = paymentService.selectByOrderNumAndTradeType("0825255175681151", Constants.PAYMENT_TRADE_TYPE.TRADE_TYPE5, Constants.PAYMENT_STATUS.STAUS2,0);
 		System.err.println(paymentEntity.getId());
 	}
-	
-//	@Test
+
+	//	@Test
 	public void testBodyToEntity(){
 		String body = "{\"body\":{\"title\":\"c5555555\",\"orderDetailNum\":\"626676\",\"orderType\":\"1\",\"paymentId\":\"1000000459740290\","
 				+ "\"orderNum\":\"0827630333441716\",\"totalPrice\":\"0.01\"},\"source\":\"coupon_order_queue\",\"target\":\"rpb\",\"timestamp\":1440667020557,\"type\":\"2\"}";
 		paymentService.bodyToPaymentEntity(body, "2");
 	}
-	
-//	@Test
+
+	//	@Test
 	@Rollback(false)
 	public void testUpdateRefundRejected(){
 		paymentService.updateRefundRejected(4690,1);
 	}
-	
+
 	@Test
 	public void testValiadteStatus(){
 		String[] ids = new String[]{"1","2","3"};
-		paymentService.valiadteStatus(ids, 2);	
+		paymentService.valiadteStatus(ids, 2);
 	}
 }
