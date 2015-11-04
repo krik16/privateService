c9d26429461d0a1e78a6d4cb1696f9f205cf3105
lijing <lijing@rongyi.com>
2015-11-04 12:22:19 +0800

    买手代码还原

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/content_v2/vo/HomePageContentVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/content_v2/vo/HomePageContentVO.java
@@ -1,5 +1,6 @@
 package com.rongyi.easy.content_v2.vo;
 
+import com.rongyi.easy.mcmc.vo.LiveVO;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 
@@ -17,11 +18,13 @@ public class HomePageContentVO implements Serializable{
     private static final long serialVersionUID = 1L;
     private List<BannerVO> banner;
     private QuickEntryVO quickEntry;
+    private List<QuickEntryNewVO> quickEntries;
     private List<HotHeadlineVO> hotHeadline;
-    private StarShopVO starShop;
+    private List<StarShopVO> starShop;
     private List<ActivityRecommendVO> activityRecommend;
     private List<CouponRecommendVO> couponRecommend;
     private FloatAdVO floatAd;
+    private List<AppBuyerLiveVO> buyerLives;
 
     public List<BannerVO> getBanner() {
         return banner;
@@ -47,11 +50,11 @@ public class HomePageContentVO implements Serializable{
         this.hotHeadline = hotHeadline;
     }
 
-    public StarShopVO getStarShop() {
+    public List<StarShopVO> getStarShop() {
         return starShop;
     }
 
-    public void setStarShop(StarShopVO starShop) {
+    public void setStarShop(List<StarShopVO> starShop) {
         this.starShop = starShop;
     }
 
@@ -79,16 +82,35 @@ public class HomePageContentVO implements Serializable{
         this.floatAd = floatAd;
     }
 
+
+    public List<AppBuyerLiveVO> getBuyerLives() {
+        return buyerLives;
+    }
+
+    public void setBuyerLives(List<AppBuyerLiveVO> buyerLives) {
+        this.buyerLives = buyerLives;
+    }
+
+    public List<QuickEntryNewVO> getQuickEntries() {
+        return quickEntries;
+    }
+
+    public void setQuickEntries(List<QuickEntryNewVO> quickEntries) {
+        this.quickEntries = quickEntries;
+    }
+
     @Override
     public String toString() {
-        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+        return new ToStringBuilder(this)
                 .append("banner", banner)
                 .append("quickEntry", quickEntry)
+                .append("quickEntries", quickEntries)
                 .append("hotHeadline", hotHeadline)
                 .append("starShop", starShop)
                 .append("activityRecommend", activityRecommend)
                 .append("couponRecommend", couponRecommend)
                 .append("floatAd", floatAd)
+                .append("buyerLives", buyerLives)
                 .toString();
     }
 }
