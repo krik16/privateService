9919b3f0916f12c2b91c3af7f1b502f4c0cdd7a7
chenjun <chenjun@rongyi.com>
2015-11-18 16:11:15 +0800

    修改参数

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/param/RebateCommodityParam.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package com.rongyi.easy.rmmm.param;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.rongyi.easy.malllife.param.MalllifeBaseParam;
+
+/**
+ * Copyright (C),上海容易网电子商务有限公司
+ * author chenjun
+ * Description ：
+ * time ：2015年11月18日
+ * History: 变更记录
+ * <author>           <time>             <version>        <desc>
+ * chenjun            2015年11月18日               1.0              创建文件
+ */
+public class RebateCommodityParam extends MalllifeBaseParam implements Serializable{
+	
+	private Double orderPrice;//"订单总价"（不包含邮费）
+	
+	private List<CommodityListParam> commoditys;//商品数据集合
+
+	public Double getOrderPrice() {
+		return orderPrice;
+	}
+
+	public void setOrderPrice(Double orderPrice) {
+		this.orderPrice = orderPrice;
+	}
+
+	public List<CommodityListParam> getCommoditys() {
+		return commoditys;
+	}
+
+	public void setCommoditys(List<CommodityListParam> commoditys) {
+		this.commoditys = commoditys;
+	}
+
+	@Override
+	public String toString() {
+		return "RebateCommodityParam [orderPrice=" + orderPrice + ", commoditys=" + commoditys + "]";
+	}
+
+}
