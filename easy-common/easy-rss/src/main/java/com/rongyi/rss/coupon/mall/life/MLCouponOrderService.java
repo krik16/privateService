ddbf2822e4aba3567a7a15152fc799a69f9b3e68
kejun <kejun@rongyi.com>
2015-11-19 14:46:47 +0800

    积分使用检查接口

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/coupon/mall/life/MLCouponOrderService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/coupon/mall/life/MLCouponOrderService.java
@@ -4,6 +4,7 @@ import com.rongyi.core.bean.ResponseResult;
 import com.rongyi.easy.coupon.entity.CouponOrder;
 import com.rongyi.easy.coupon.entity.CouponOrderItem;
 import com.rongyi.easy.coupon.param.CouponOrderParam;
+import com.rongyi.easy.rmmm.param.OrderListParam;
 
 import java.util.List;
 import java.util.Map;
@@ -219,4 +220,16 @@ public interface MLCouponOrderService {
      * @return
      */
 //    void closeUnpayOrder();
+
+    /**
+     * 获取代金券生成订单最大使用积分
+     *
+     * @param param
+     * @param limit 积分抵扣限制
+     * @param scoreExchangeMoney 积分抵扣金额
+     * @param platformRebateAmount 满减抵扣券抵扣价格
+     * @return
+     * @throws Exception
+     */
+     int getMaxIntegral(OrderListParam param, double limit, double scoreExchangeMoney,double platformRebateAmount);
 }
