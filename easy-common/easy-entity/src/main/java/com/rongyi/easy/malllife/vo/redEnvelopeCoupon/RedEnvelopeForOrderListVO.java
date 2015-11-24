18739f50287aa1b279f39c627951ae7275fd76b9
zhangzhengping <zhangzhengping@rongyi.com>
2015-11-24 14:40:59 +0800

    红包相关返回值

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/malllife/vo/redEnvelopeCoupon/RedEnvelopeForOrderListVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/malllife/vo/redEnvelopeCoupon/RedEnvelopeForOrderListVO.java
@@ -2,7 +2,11 @@ package com.rongyi.easy.malllife.vo.redEnvelopeCoupon;
 
 import java.io.Serializable;
 import java.util.List;
-
+/**
+ * 订单可使用与不可使用的红包列表
+ * @author zhangzhengping
+ *
+ */
 public class RedEnvelopeForOrderListVO implements Serializable{
 	
 	/**
