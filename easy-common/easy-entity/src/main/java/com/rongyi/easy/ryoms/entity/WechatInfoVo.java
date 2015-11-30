6230ad78934c896639809d5dff8e2faf4b2b2cb9
sujuan <sujuan@rongyi.com>
2015-11-30 21:09:08 +0800

    修改版本号2.6.2  新增微信服务号Vo

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/ryoms/entity/WechatInfoVo.java
@@ -0,0 +1,101 @@
+package com.rongyi.easy.ryoms.entity;
+
+/**
+ * 微信 
+ * @author 苏娟
+ *
+ */
+public class WechatInfoVo {
+	private Integer id;
+	private String name;
+	private String serviceName;
+	private String appId;
+	private String appSecret;
+	private String encodingAESKey;
+	private Integer type;
+	private Integer mallId;
+	private String mallMid;
+	private String mallName;
+	private String dockingType;
+	private Integer isMeiwei ;//是否接入美味不用等
+	private String meiweiId;  //美味不用等商场ID
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getServiceName() {
+		return serviceName;
+	}
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+	public String getAppId() {
+		return appId;
+	}
+	public void setAppId(String appId) {
+		this.appId = appId;
+	}
+	public String getAppSecret() {
+		return appSecret;
+	}
+	public void setAppSecret(String appSecret) {
+		this.appSecret = appSecret;
+	}
+	public String getEncodingAESKey() {
+		return encodingAESKey;
+	}
+	public void setEncodingAESKey(String encodingAESKey) {
+		this.encodingAESKey = encodingAESKey;
+	}
+	public Integer getType() {
+		return type;
+	}
+	public void setType(Integer type) {
+		this.type = type;
+	}
+	public Integer getMallId() {
+		return mallId;
+	}
+	public void setMallId(Integer mallId) {
+		this.mallId = mallId;
+	}
+	public String getMallMid() {
+		return mallMid;
+	}
+	public void setMallMid(String mallMid) {
+		this.mallMid = mallMid;
+	}
+	public String getMallName() {
+		return mallName;
+	}
+	public void setMallName(String mallName) {
+		this.mallName = mallName;
+	}
+	public String getDockingType() {
+		return dockingType;
+	}
+	public void setDockingType(String dockingType) {
+		this.dockingType = dockingType;
+	}
+	public Integer getIsMeiwei() {
+		return isMeiwei;
+	}
+	public void setIsMeiwei(Integer isMeiwei) {
+		this.isMeiwei = isMeiwei;
+	}
+	public String getMeiweiId() {
+		return meiweiId;
+	}
+	public void setMeiweiId(String meiweiId) {
+		this.meiweiId = meiweiId;
+	}
+	
+}
---
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/ryoms/IWechatInfoService.java
@@ -0,0 +1,12 @@
+package com.rongyi.rss.ryoms;
+
+import com.rongyi.easy.ryoms.entity.WechatInfoVo;
+
+/**
+ * 微信服务号接口 
+ * @author 苏娟
+ *
+ */
+public interface IWechatInfoService {
+	
+}
