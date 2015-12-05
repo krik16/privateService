debd31686b93042b1b0fef29bd75ab7609db3c93
wangzehui <wangzehui@rongyi.com>
2015-12-05 17:32:28 +0800

    增加规格图片

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/vo/CommoditySpecVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/vo/CommoditySpecVO.java
@@ -19,57 +19,81 @@ public class CommoditySpecVO implements  Serializable {
 	private String specLockedStock;//锁定库存
 	private String specOriginalPrice;
 	private String specCurrentPrice;
+	private String specPictureUrl;//规格图片
 	private List<CommoditySpecColumnVO> specColumnValues;
+	
 	public List<CommoditySpecColumnVO> getSpecColumnValues() {
 		return specColumnValues;
 	}
+	
 	public void setSpecColumnValues(List<CommoditySpecColumnVO> specColumnValues) {
 		this.specColumnValues = specColumnValues;
 	}
+	
 	public CommoditySpecVO(){
 		
 	}
+	
 	public CommoditySpecVO(CommoditySpec commoditySpec){
 		this.specId = commoditySpec.getId().toString();
 		this.specStock = commoditySpec.getStock();
 		this.specOriginalPrice = commoditySpec.getOriginalPrice();
 		this.specCurrentPrice = commoditySpec.getCurrentPrice();
 	}
+	
 	public String getSpecId() {
 		return specId;
 	}
+	
 	public void setSpecId(String specId) {
 		this.specId = specId;
 	}
+	
 	public String getSpecStock() {
 		return specStock;
 	}
+	
 	public void setSpecStock(String specStock) {
 		this.specStock = specStock;
 	}
+	
 	public String getSpecOriginalPrice() {
 		return specOriginalPrice;
 	}
+	
 	public void setSpecOriginalPrice(String specOriginalPrice) {
 		this.specOriginalPrice = specOriginalPrice;
 	}
+	
 	public String getSpecCurrentPrice() {
 		return specCurrentPrice;
 	}
+	
 	public void setSpecCurrentPrice(String specCurrentPrice) {
 		this.specCurrentPrice = specCurrentPrice;
 	}
+	
 	public String getSpecTotalStock() {
 		return specTotalStock;
 	}
+	
 	public void setSpecTotalStock(String specTotalStock) {
 		this.specTotalStock = specTotalStock;
 	}
+	
 	public String getSpecLockedStock() {
 		return specLockedStock;
 	}
+	
 	public void setSpecLockedStock(String specLockedStock) {
 		this.specLockedStock = specLockedStock;
 	}
-	
+
+	public String getSpecPictureUrl() {
+		return specPictureUrl;
+	}
+
+	public void setSpecPictureUrl(String specPictureUrl) {
+		this.specPictureUrl = specPictureUrl;
+	}
 }
