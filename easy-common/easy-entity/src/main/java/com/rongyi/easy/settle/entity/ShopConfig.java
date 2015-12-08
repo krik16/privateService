8b36380980942ebeb66ef3c60354d4d8ac96692e
hebo <hebo@rongyi.com>
2015-12-08 20:11:50 +0800

    关联表

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/settle/entity/ShopConfig.java
@@ -0,0 +1,54 @@
+package com.rongyi.easy.settle.entity;
+
+import java.io.Serializable;
+
+/**
+ * Created by Ventures on 2015/12/8.
+ */
+public class ShopConfig implements Serializable{
+    private Integer id;
+    private Integer configId;
+    private String shopId;
+    private String userList;
+    private String realUserList;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getConfigId() {
+        return configId;
+    }
+
+    public void setConfigId(Integer configId) {
+        this.configId = configId;
+    }
+
+    public String getShopId() {
+        return shopId;
+    }
+
+    public void setShopId(String shopId) {
+        this.shopId = shopId;
+    }
+
+    public String getUserList() {
+        return userList;
+    }
+
+    public void setUserList(String userList) {
+        this.userList = userList;
+    }
+
+    public String getRealUserList() {
+        return realUserList;
+    }
+
+    public void setRealUserList(String realUserList) {
+        this.realUserList = realUserList;
+    }
+}
