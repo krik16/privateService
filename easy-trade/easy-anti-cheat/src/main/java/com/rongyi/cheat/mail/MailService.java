7d9a502f42b269edd593b924f0e413e05c639e9b
kejun <kejun@rongyi.com>
2015-12-09 10:37:34 +0800

    对账单邮件相关

--- easy-trade/easy-anti-cheat/src/main/java/com/rongyi/cheat/mail/MailService.java
+++ easy-trade/easy-anti-cheat/src/main/java/com/rongyi/cheat/mail/MailService.java
@@ -1,20 +1,5 @@
 package com.rongyi.cheat.mail;
 
-import java.io.File;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeUtility;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +9,15 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeUtility;
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.util.*;
 
 /**
  * @Author: 柯军
@@ -43,7 +37,7 @@ public class MailService {
 
 	private long retryInterval = 60_000;
 
-	public void sendAttachmentEmail() throws AddressException, MessagingException, UnsupportedEncodingException {
+	public void sendAttachmentEmail() throws  MessagingException, UnsupportedEncodingException {
 		Set<String> toAdrs = new HashSet<String>();
 		toAdrs.add("kejun@rongyi.com");
 		List<String> filenames = new ArrayList<String>();
---
+++ easy-trade/easy-settle/src/test/java/com/rongyi/settle/service/PaymentStatementServiceTest.java
@@ -0,0 +1,10 @@
+package com.rongyi.settle.service;
+
+import com.rongyi.settle.BaseTest;
+
+/**
+ * Created by kejun on 2015/12/8.
+ */
+public class PaymentStatementServiceTest extends BaseTest{
+
+}
