538be0ac31e821f74163f9dcf844d7cab4d10818
shaozhou <shaozhou@rongyi.com>
2015-12-12 13:59:41 +0800

    commit

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/roa/ROAFilialeService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/roa/ROAFilialeService.java
@@ -3,6 +3,8 @@ package com.rongyi.rss.roa;
 import java.util.List;
 import java.util.Map;
 
+import com.rongyi.easy.roa.entity.FilialeEntity;
+import com.rongyi.easy.roa.param.FilialeParam;
 import com.rongyi.easy.roa.vo.FilialeVo;
 
 /**
@@ -30,4 +32,15 @@ public interface ROAFilialeService {
 	 * @return
 	 */
 	public FilialeVo getFilialeVoByMongoId(String id);
-	}
+	/**
+	 * 
+	* @Title: saveOrUpdate 
+	* @Description: 新增或修改分公司接口
+	* @param @param param    设定文件 
+	* @return void    返回类型 
+	* @author shaozhou 
+	* @date 2015年12月12日 下午12:15:29
+	* @throws
+	 */
+	public void saveOrUpdate(FilialeParam param);
+}
