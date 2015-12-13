b5e20484587c0cf7d4950c8c7474a31a7ae48633
黄利君 <huanglijun@rongyi.com>
2015-12-13 16:51:40 +0800

    fix order detail add coupon info

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/roa/vo/ActivityDetailVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/roa/vo/ActivityDetailVO.java
@@ -3,22 +3,23 @@ package com.rongyi.easy.roa.vo;
 import java.io.Serializable;
 import java.util.Date;
 
-public class ActivityDetailVO implements Serializable{
+public class ActivityDetailVO implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private Integer activityDetailId; // 优惠Id
+    private Integer activityDetailId; // 优惠Id
 
-	private String activityCode; // 优惠券码
+    private String activityCode; // 优惠券码
 
-	private String itemName; // 优惠券名称 ， 商品名称，团购中对应团购名称 title 字段
+    private String itemName; // 优惠券名称 ， 商品名称，团购中对应团购名称 title 字段
 
-	private Integer status; // 使用状态
+    private Integer status; // 使用状态
 
-	private Date startTime; // 有效期始
+    private Date startTime; // 有效期始
 
-	private Date endTime; // 有效期止
+    private Date endTime; // 有效期止
 
+    private String couponId;//卡券Id
     /**
      * 原价
      */
@@ -30,53 +31,53 @@ public class ActivityDetailVO implements Serializable{
     private double currPrice;
 
 
-	public Integer getActivityDetailId() {
-		return activityDetailId;
-	}
+    public Integer getActivityDetailId() {
+        return activityDetailId;
+    }
 
-	public void setActivityDetailId(Integer activityDetailId) {
-		this.activityDetailId = activityDetailId;
-	}
+    public void setActivityDetailId(Integer activityDetailId) {
+        this.activityDetailId = activityDetailId;
+    }
 
-	public String getActivityCode() {
-		return activityCode;
-	}
+    public String getActivityCode() {
+        return activityCode;
+    }
 
-	public void setActivityCode(String activityCode) {
-		this.activityCode = activityCode;
-	}
+    public void setActivityCode(String activityCode) {
+        this.activityCode = activityCode;
+    }
 
-	public Integer getStatus() {
-		return status;
-	}
+    public Integer getStatus() {
+        return status;
+    }
 
-	public void setStatus(Integer status) {
-		this.status = status;
-	}
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
 
-	public Date getStartTime() {
-		return startTime;
-	}
+    public Date getStartTime() {
+        return startTime;
+    }
 
-	public void setStartTime(Date startTime) {
-		this.startTime = startTime;
-	}
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
 
-	public Date getEndTime() {
-		return endTime;
-	}
+    public Date getEndTime() {
+        return endTime;
+    }
 
-	public void setEndTime(Date endTime) {
-		this.endTime = endTime;
-	}
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
 
-	public String getItemName() {
-		return itemName;
-	}
+    public String getItemName() {
+        return itemName;
+    }
 
-	public void setItemName(String itemName) {
-		this.itemName = itemName;
-	}
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
 
     public double getOrigPrice() {
         return origPrice;
@@ -92,5 +93,13 @@ public class ActivityDetailVO implements Serializable{
 
     public void setCurrPrice(double currPrice) {
         this.currPrice = currPrice;
+    }
+
+    public String getCouponId() {
+        return couponId;
+    }
+
+    public void setCouponId(String couponId) {
+        this.couponId = couponId;
     }
 }
