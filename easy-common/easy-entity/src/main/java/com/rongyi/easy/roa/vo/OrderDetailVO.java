59d16c89a145b9b063cd7bf3022cfebaafa046b9
黄利君 <huanglijun@rongyi.com>
2015-12-13 16:20:30 +0800

    修复返库存bug

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/roa/vo/OrderDetailVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/roa/vo/OrderDetailVO.java
@@ -1,97 +1,109 @@
 package com.rongyi.easy.roa.vo;
 
+
+import com.rongyi.easy.coupon.entity.Coupon;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.List;
 
 /**
  * 订单详情VO类
- * 
- * @author LiuChao
  *
+ * @author LiuChao
  */
 
-public class OrderDetailVO implements Serializable{
+public class OrderDetailVO implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private Integer tradeStatus;// 交易状态
+    private Integer tradeStatus;// 交易状态
 
-	private String orderTime;// 下单时间
+    private String orderTime;// 下单时间
 
-	private String payTime;// 付款时间
+    private String payTime;// 付款时间
 
-	private BigDecimal unitPrice;// 单价
+    private BigDecimal unitPrice;// 单价
 
-	private Integer unitNum;// 下单数量
+    private Integer unitNum;// 下单数量
 
-	private BigDecimal orderPrice;// 下单总价
+    private BigDecimal orderPrice;// 下单总价
 
-	private String itemName; // 商品名称
+    private String itemName; // 商品名称
 
-	private List<ActivityDetailVO> activityDetailVOs; // 本订单所含的优惠券信息
+    private Coupon coupon; //卡券信息
 
-	public Integer getTradeStatus() {
-		return tradeStatus;
-	}
+    private List<ActivityDetailVO> activityDetailVOs; // 本订单所含的优惠券信息
 
-	public String getOrderTime() {
-		return orderTime;
-	}
+    public Integer getTradeStatus() {
+        return tradeStatus;
+    }
 
-	public String getPayTime() {
-		return payTime;
-	}
+    public String getOrderTime() {
+        return orderTime;
+    }
 
-	public BigDecimal getUnitPrice() {
-		return unitPrice;
-	}
+    public String getPayTime() {
+        return payTime;
+    }
 
-	public Integer getUnitNum() {
-		return unitNum;
-	}
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
 
-	public BigDecimal getOrderPrice() {
-		return orderPrice;
-	}
+    public Integer getUnitNum() {
+        return unitNum;
+    }
 
-	public String getItemName() {
-		return itemName;
-	}
+    public BigDecimal getOrderPrice() {
+        return orderPrice;
+    }
 
-	public List<ActivityDetailVO> getActivityDetailVOs() {
-		return activityDetailVOs;
-	}
+    public String getItemName() {
+        return itemName;
+    }
 
-	public void setTradeStatus(Integer tradeStatus) {
-		this.tradeStatus = tradeStatus;
-	}
+    public List<ActivityDetailVO> getActivityDetailVOs() {
+        return activityDetailVOs;
+    }
 
-	public void setOrderTime(String orderTime) {
-		this.orderTime = orderTime;
-	}
+    public void setTradeStatus(Integer tradeStatus) {
+        this.tradeStatus = tradeStatus;
+    }
 
-	public void setPayTime(String payTime) {
-		this.payTime = payTime;
-	}
+    public void setOrderTime(String orderTime) {
+        this.orderTime = orderTime;
+    }
 
-	public void setUnitPrice(BigDecimal unitPrice) {
-		this.unitPrice = unitPrice;
-	}
+    public void setPayTime(String payTime) {
+        this.payTime = payTime;
+    }
 
-	public void setUnitNum(Integer unitNum) {
-		this.unitNum = unitNum;
-	}
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
 
-	public void setOrderPrice(BigDecimal orderPrice) {
-		this.orderPrice = orderPrice;
-	}
+    public void setUnitNum(Integer unitNum) {
+        this.unitNum = unitNum;
+    }
 
-	public void setItemName(String itemName) {
-		this.itemName = itemName;
-	}
+    public void setOrderPrice(BigDecimal orderPrice) {
+        this.orderPrice = orderPrice;
+    }
 
-	public void setActivityDetailVOs(List<ActivityDetailVO> activityDetailVOs) {
-		this.activityDetailVOs = activityDetailVOs;
-	}
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public void setActivityDetailVOs(List<ActivityDetailVO> activityDetailVOs) {
+        this.activityDetailVOs = activityDetailVOs;
+    }
+
+    public Coupon getCoupon() {
+        return coupon;
+    }
+
+    public void setCoupon(Coupon coupon) {
+        this.coupon = coupon;
+    }
 }
