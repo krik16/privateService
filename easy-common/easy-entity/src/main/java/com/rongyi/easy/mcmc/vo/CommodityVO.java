48dda5e1e32f0ba271f641d2c5407bd6cf1d55fb
xieguoqiang <xieguoqiang@rongyi.com>
2015-12-22 14:11:32 +0800

    common develop合入 - 1221分公司功能代码。

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/vo/CommodityVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/vo/CommodityVO.java
@@ -6,7 +6,8 @@ import java.util.List;
 
 import com.rongyi.easy.mcmc.Commodity;
 
-public class CommodityVO implements Serializable {
+
+public class CommodityVO  implements  Serializable {
 
 	/**
 	 * 
@@ -21,19 +22,18 @@ public class CommodityVO implements Serializable {
 	private String commoditySold;
 	private String commodityPubDate;
 	private int commodityStatus;
-	private int commodityAppStatus;// 商品APP显示状态 (0下架 1上架 3待上架)
 
-	private String commodityOPriceMax;// 我是最高原价”,
-	private String commodityOPriceMin;// 我是最低原价”,
-	private String commodityCPriceMax;// 我是最高现价”,
-	private String commodityCPriceMin;// 我是最低现价”,
-	private String commodityOPOfLCP;// 我是最低现价对应的原价
-
-	private int commodityType;// 渠道 0商家，1买手
-	private int isSpot;// 是否现货
+	private String commodityOPriceMax;//我是最高原价”,
+	private String commodityOPriceMin;//我是最低原价”,
+	private String commodityCPriceMax;//我是最高现价”,
+	private String commodityCPriceMin;//我是最低现价”,
+	private String commodityOPOfLCP;//我是最低现价对应的原价
+	
+	private int commodityType;//渠道  0商家，1买手
+	private int isSpot;//是否现货	
 	private Date liveStartTime; // 直播开始时间
 	private Date liveEndTime; // 直播结束时间
-	private String create_by; // 创建人
+	private String create_by; // 创建人 
 	private String liveId; // 直播Id
 	private String commodityOriginalPrice;
 	private String commodityCurrentPrice;
@@ -44,174 +44,135 @@ public class CommodityVO implements Serializable {
 	private List<CommoditySpecVO> commoditySpecList;
 	private String commodityCode;
 	private String commodityCommission;
-	private String brandMid;// 品牌mongoId
-	private String mallMid;// 商场mongoId
-	private String shopName;// 店铺名称
-	private boolean supportCourierDeliver = true;// true是 false否
-	private boolean supportSelfPickup = true;// 支持到店自提 true 是 false否
-	private Date registerAt;// 上架时间
-	private Date soldOutAt;// 下架时间
-	private Integer source;// 来源0表示页面添加1表示批量导入2表示app添加的商品
-	// private Integer distribution;//配送方式 1表示到店自提2快递3表示支持两种方式
-	private Integer freight;// 1表示商家承担运费,0表示买家承担运费
-	private Integer terminalType;// 上架终端：1.表示容易逛2.表示互动屏3.表示容易逛和互动屏4.表示微商5.微商,容易逛6.微商,互动屏7.容易逛,
-									// 互动屏, 微商(转换成二进制数个位1有容易逛第二位1有 互动屏第三位1有 微商)
-	private Integer stockStatus;// 0表示统一库存1表示分管库存
-	private String reason;// 下架原因
-	private String mallId;// 商场mysql Id
+	private String brandMid;//品牌mongoId
+	private String mallMid;//商场mongoId
+	private String shopName;//店铺名称
+	private boolean supportCourierDeliver=true;//true是  false否
+	private boolean supportSelfPickup=true;//支持到店自提  true 是    false否
+	private Date registerAt;//上架时间
+	private Date soldOutAt;//下架时间
+	private Integer source;//来源0表示页面添加1表示批量导入2表示app添加的商品
+	//private Integer distribution;//配送方式 1表示到店自提2快递3表示支持两种方式
+	private Integer freight;//1表示商家承担运费,0表示买家承担运费
+	private Integer terminalType;//上架终端：1.表示容易逛2.表示互动屏3.表示容易逛和互动屏4.表示微商5.微商,容易逛6.微商,互动屏7.容易逛, 互动屏, 微商(转换成二进制数个位1有容易逛第二位1有 互动屏第三位1有 微商)
+	private Integer stockStatus;//0表示统一库存1表示分管库存
+	private String reason;//下架原因
+	private String mallId;//商场mysql Id
 	private String brandName;
 	private String shopNum;
-	private int brandId;// 品牌mysqlId
-	private String filialeMid;// 分公司id
+	private int brandId;//品牌mysqlId
+	private String filialeMid;//分公司id
+	private int identity = 5;//-1表示定时任务0集团管理员、1商场管理员、2品牌管理员、3分公司、4店长、5导购6买手
 
-	public int getCommodityAppStatus() {
-		return commodityAppStatus;
-	}
-
-	public void setCommodityAppStatus(int commodityAppStatus) {
-		this.commodityAppStatus = commodityAppStatus;
-	}
-
+	private Integer processIdentity;//当前登录人的身份
 	public String getShopName() {
 		return shopName;
 	}
-
 	public void setShopName(String shopName) {
 		this.shopName = shopName;
 	}
-
 	public String getBrandMid() {
 		return brandMid;
 	}
-
 	public void setBrandMid(String brandMid) {
 		this.brandMid = brandMid;
 	}
-
 	public String getMallMid() {
 		return mallMid;
 	}
-
 	public void setMallMid(String mallMid) {
 		this.mallMid = mallMid;
 	}
-
 	public String getLiveId() {
 		return liveId;
 	}
-
 	public void setLiveId(String liveId) {
 		this.liveId = liveId;
 	}
-
 	public Date getLiveStartTime() {
 		return liveStartTime;
 	}
-
 	public void setLiveStartTime(Date liveStartTime) {
 		this.liveStartTime = liveStartTime;
 	}
-
 	public Date getLiveEndTime() {
 		return liveEndTime;
 	}
-
 	public void setLiveEndTime(Date liveEndTime) {
 		this.liveEndTime = liveEndTime;
 	}
-
 	public String getCreate_by() {
 		return create_by;
 	}
-
 	public void setCreate_by(String create_by) {
 		this.create_by = create_by;
 	}
-
 	public int getCommodityType() {
 		return commodityType;
 	}
-
 	public void setCommodityType(int commodityType) {
 		this.commodityType = commodityType;
 	}
-
+	
 	public int getIsSpot() {
 		return isSpot;
 	}
-
 	public void setIsSpot(int isSpot) {
 		this.isSpot = isSpot;
 	}
-
 	public String getCommodityOPriceMax() {
 		return commodityOPriceMax;
 	}
-
 	public void setCommodityOPriceMax(String commodityOPriceMax) {
 		this.commodityOPriceMax = commodityOPriceMax;
 	}
-
 	public String getCommodityOPriceMin() {
 		return commodityOPriceMin;
 	}
-
 	public void setCommodityOPriceMin(String commodityOPriceMin) {
 		this.commodityOPriceMin = commodityOPriceMin;
 	}
-
 	public String getCommodityCPriceMax() {
 		return commodityCPriceMax;
 	}
-
 	public void setCommodityCPriceMax(String commodityCPriceMax) {
 		this.commodityCPriceMax = commodityCPriceMax;
 	}
-
 	public String getCommodityCPriceMin() {
 		return commodityCPriceMin;
 	}
-
 	public void setCommodityCPriceMin(String commodityCPriceMin) {
 		this.commodityCPriceMin = commodityCPriceMin;
 	}
-
 	public String getCommodityOPOfLCP() {
 		return commodityOPOfLCP;
 	}
-
 	public void setCommodityOPOfLCP(String commodityOPOfLCP) {
 		this.commodityOPOfLCP = commodityOPOfLCP;
 	}
-
 	public String getCommodityShopNumber() {
 		return commodityShopNumber;
 	}
-
 	public void setCommodityShopNumber(String commodityShopNumber) {
 		this.commodityShopNumber = commodityShopNumber;
 	}
-
 	public int getCommodityStatus() {
 		return commodityStatus;
 	}
-
 	public void setCommodityStatus(int commodityStatus) {
 		this.commodityStatus = commodityStatus;
 	}
-
+	
+	
 	public String getShopMid() {
 		return shopMid;
 	}
-
 	public void setShopMid(String shopMid) {
 		this.shopMid = shopMid;
 	}
-
 	public String getCommodityCommission() {
 		return commodityCommission;
 	}
-
 	public void setCommodityCommission(String commodityCommission) {
 		this.commodityCommission = commodityCommission;
 	}
@@ -219,69 +180,74 @@ public class CommodityVO implements Serializable {
 	public String getCommodityCode() {
 		return commodityCode;
 	}
-
 	public void setCommodityCode(String commodityCode) {
 		this.commodityCode = commodityCode;
 	}
-
 	public String getShopId() {
 		return shopId;
 	}
-
 	public void setShopId(String shopId) {
 		this.shopId = shopId;
 	}
 
-	public CommodityVO() {
-
+	public Integer getProcessIdentity() {
+		return processIdentity;
 	}
 
-	public CommodityVO(Commodity commodity) {
+	public void setProcessIdentity(Integer processIdentity) {
+		this.processIdentity = processIdentity;
+	}
+
+	public CommodityVO(){
+		
+	}
+	public CommodityVO(Commodity commodity){
 		this.commodityType = commodity.getType();
 		this.shopId = commodity.getShopId();
-		this.shopMid = commodity.getShopMid();// 店铺mongoId
-		this.create_by = commodity.getCreate_by();// 创建人
-		this.liveId = commodity.getLiveId();// 直播id
-		this.liveStartTime = commodity.getLiveStartTime();// 直播开始时间
-		this.liveEndTime = commodity.getLiveEndTime();// 直播结束时间
+		this.shopMid = commodity.getShopMid();//店铺mongoId
+		this.create_by = commodity.getCreate_by();//创建人
+		this.liveId = commodity.getLiveId();//直播id
+		this.liveStartTime = commodity.getLiveStartTime();//直播开始时间
+		this.liveEndTime = commodity.getLiveEndTime();//直播结束时间
 		this.commodityId = commodity.getId().toString();
 		this.commodityName = commodity.getName();
 		this.commodityCategory = commodity.getCategory();
 		this.commodityDescription = commodity.getDescription();
-		this.brandMid = commodity.getBrandMid();// 品牌mongoId
-		this.mallMid = commodity.getMallMid();// 商场mongoId
-		if (commodity.isSpot()) {
-			this.isSpot = 1;// 现货
-		} else {
-			this.isSpot = 0;// 非现货
+		this.brandMid = commodity.getBrandMid();//品牌mongoId
+		this.mallMid = commodity.getMallMid();//商场mongoId
+		if(commodity.isSpot()){
+			this.isSpot = 1;//现货
+		}else{
+			this.isSpot = 0;//非现货
 		}
+		
 
-		if (commodity.getPostage() != null && !commodity.getPostage().isEmpty()) {
-			try {
+		if(commodity.getPostage() != null && !commodity.getPostage().isEmpty()){
+			try{
 				double postage = Double.parseDouble(commodity.getPostage());
 				this.commodityPostage = commodity.getPostage();
-			} catch (Exception e) {
+			}catch(Exception e){
 				this.commodityPostage = "0.0";
 			}
-		} else {
+		}else{
 			this.commodityPostage = "0.0";
 		}
 
-		// this.commodityPostage = commodity.getPostage();
+//		this.commodityPostage = commodity.getPostage();
 		this.commodityStock = String.valueOf(commodity.getStock());
 		this.commoditySold = String.valueOf(commodity.getSold());
 		this.commodityPubDate = commodity.getCreateAt().toString();
 		this.commodityOriginalPrice = commodity.getOriginalPrice();
 		this.commodityCurrentPrice = commodity.getCurrentPrice();
 		this.commodityPicList = commodity.getPicList();
-		if (commodity.getCode() != null) {
+		if(commodity.getCode() !=null){
 			this.commodityCode = commodity.getCode();
-		} else {
+		}else{
 			this.commodityCode = "";
 		}
 		this.commodityStatus = commodity.getStatus();
 		this.commodityShopNumber = "";
-		if (commodity.getShopNum() != null) {
+		if(commodity.getShopNum() != null){
 			this.commodityShopNumber = commodity.getShopNum();
 		}
 
@@ -291,127 +257,85 @@ public class CommodityVO implements Serializable {
 		this.commodityCPriceMin = commodity.getcPriceMin();
 		this.commodityOPOfLCP = commodity.getoPriceOfLowestCPrice();
 		this.supportCourierDeliver = commodity.isSupportCourierDeliver();
-		this.registerAt = commodity.getRegisterAt();
-		this.soldOutAt = commodity.getSoldOutAt();
+		this.registerAt=commodity.getRegisterAt();
+		this.soldOutAt=commodity.getSoldOutAt();
 		this.supportSelfPickup = commodity.isSupportSelfPickup();
-		// 商品待上架且上架时间大于当前时间，app商品状态为 待上架
-		// 商品上架或待上架，且上架时间小于当前时间，且下架时间大于当前时间，app商品状态为 上架
-		// 其他 下架
-		// 状态 -1：非现货初始化(直播使用） 0下架 1上架 (当前时间在上架时间和下架时间之间)2是删除3待上架4待处理5待审核 6审核失败
-		if (this.commodityStatus == 3
-				&& (commodity.getRegisterAt() != null && commodity.getRegisterAt().getTime() - new Date().getTime() > 0)) {
-			this.commodityAppStatus = 3;
-		} else if ((commodityStatus == 1 || commodityStatus == 3)
-				&& (commodity.getRegisterAt() != null && commodity.getRegisterAt().getTime() - new Date().getTime() <= 0)
-				&& (commodity.getSoldOutAt() != null && commodity.getSoldOutAt().getTime() - new Date().getTime() > 0)) {
-			this.commodityAppStatus = 1;
-		} else if (commodityAppStatus != 0 && commodityAppStatus != 1 && commodityAppStatus != 3) {
-			this.commodityAppStatus = 0;
-		} else {
-			this.commodityAppStatus = this.commodityStatus;
-		}
 	}
-
 	public String getCommodityId() {
 		return commodityId;
 	}
-
 	public void setCommodityId(String commodityId) {
 		this.commodityId = commodityId;
 	}
-
 	public String getCommodityName() {
 		return commodityName;
 	}
-
 	public void setCommodityName(String commodityName) {
 		this.commodityName = commodityName;
 	}
-
 	public String getCommodityCategory() {
 		return commodityCategory;
 	}
-
 	public void setCommodityCategory(String commodityCategory) {
 		this.commodityCategory = commodityCategory;
 	}
-
 	public String getCommodityDescription() {
 		return commodityDescription;
 	}
-
 	public void setCommodityDescription(String commodityDescription) {
 		this.commodityDescription = commodityDescription;
 	}
-
 	public String getCommodityPostage() {
 		return commodityPostage;
 	}
-
 	public void setCommodityPostage(String commodityPostage) {
 		this.commodityPostage = commodityPostage;
 	}
-
 	public String getCommodityStock() {
 		return commodityStock;
 	}
-
 	public void setCommodityStock(String commodityStock) {
 		this.commodityStock = commodityStock;
 	}
-
 	public String getCommoditySold() {
 		return commoditySold;
 	}
-
 	public void setCommoditySold(String commoditySold) {
 		this.commoditySold = commoditySold;
 	}
-
 	public String getCommodityPubDate() {
 		return commodityPubDate;
 	}
-
 	public void setCommodityPubDate(String commodityPubDate) {
 		this.commodityPubDate = commodityPubDate;
 	}
-
 	public String getCommodityOriginalPrice() {
 		return commodityOriginalPrice;
 	}
-
 	public void setCommodityOriginalPrice(String commodityOriginalPrice) {
 		this.commodityOriginalPrice = commodityOriginalPrice;
 	}
-
 	public String getCommodityCurrentPrice() {
 		return commodityCurrentPrice;
 	}
-
 	public void setCommodityCurrentPrice(String commodityCurrentPrice) {
 		this.commodityCurrentPrice = commodityCurrentPrice;
 	}
-
 	public List<String> getCommodityPicList() {
 		return commodityPicList;
 	}
-
 	public void setCommodityPicList(List<String> commodityPicList) {
 		this.commodityPicList = commodityPicList;
 	}
-
 	public List<CommoditySpecVO> getCommoditySpecList() {
 		return commoditySpecList;
 	}
-
 	public void setCommoditySpecList(List<CommoditySpecVO> commoditySpecList) {
 		this.commoditySpecList = commoditySpecList;
 	}
-
 	public boolean isSupportCourierDeliver() {
 		return supportCourierDeliver;
 	}
-
 	public void setSupportCourierDeliver(boolean supportCourierDeliver) {
 		this.supportCourierDeliver = supportCourierDeliver;
 	}
@@ -419,130 +343,134 @@ public class CommodityVO implements Serializable {
 	public Date getRegisterAt() {
 		return registerAt;
 	}
-
 	public void setRegisterAt(Date registerAt) {
 		this.registerAt = registerAt;
 	}
-
 	public Date getSoldOutAt() {
 		return soldOutAt;
 	}
-
 	public void setSoldOutAt(Date soldOutAt) {
 		this.soldOutAt = soldOutAt;
 	}
-
 	@Override
 	public String toString() {
-		return "CommodityVO{" + "commodityId='" + commodityId + '\'' + ", commodityName='" + commodityName + '\''
-				+ ", commodityCategory='" + commodityCategory + '\'' + ", commodityDescription='" + commodityDescription + '\''
-				+ ", commodityPostage='" + commodityPostage + '\'' + ", commodityStock='" + commodityStock + '\''
-				+ ", commoditySold='" + commoditySold + '\'' + ", commodityPubDate='" + commodityPubDate + '\''
-				+ ", commodityStatus=" + commodityStatus + ", commodityOPriceMax='" + commodityOPriceMax + '\''
-				+ ", commodityOPriceMin='" + commodityOPriceMin + '\'' + ", commodityCPriceMax='" + commodityCPriceMax + '\''
-				+ ", commodityCPriceMin='" + commodityCPriceMin + '\'' + ", commodityOPOfLCP='" + commodityOPOfLCP + '\''
-				+ ", commodityType=" + commodityType + ", isSpot=" + isSpot + ", liveStartTime=" + liveStartTime
-				+ ", liveEndTime=" + liveEndTime + ", create_by='" + create_by + '\'' + ", liveId='" + liveId + '\''
-				+ ", commodityOriginalPrice='" + commodityOriginalPrice + '\'' + ", commodityCurrentPrice='"
-				+ commodityCurrentPrice + '\'' + ", shopId='" + shopId + '\'' + ", shopMid='" + shopMid + '\''
-				+ ", commodityShopNumber='" + commodityShopNumber + '\'' + ", commodityPicList=" + commodityPicList
-				+ ", commoditySpecList=" + commoditySpecList + ", commodityCode='" + commodityCode + '\''
-				+ ", commodityCommission='" + commodityCommission + '\'' + ", brandMid='" + brandMid + '\'' + ", mallMid='"
-				+ mallMid + '\'' + ", shopName='" + shopName + '\'' + ", supportCourierDeliver=" + supportCourierDeliver
-				+ ", supportSelfPickup=" + supportSelfPickup + '}';
+		return "CommodityVO{" +
+				"commodityId='" + commodityId + '\'' +
+				", commodityName='" + commodityName + '\'' +
+				", commodityCategory='" + commodityCategory + '\'' +
+				", commodityDescription='" + commodityDescription + '\'' +
+				", commodityPostage='" + commodityPostage + '\'' +
+				", commodityStock='" + commodityStock + '\'' +
+				", commoditySold='" + commoditySold + '\'' +
+				", commodityPubDate='" + commodityPubDate + '\'' +
+				", commodityStatus=" + commodityStatus +
+				", commodityOPriceMax='" + commodityOPriceMax + '\'' +
+				", commodityOPriceMin='" + commodityOPriceMin + '\'' +
+				", commodityCPriceMax='" + commodityCPriceMax + '\'' +
+				", commodityCPriceMin='" + commodityCPriceMin + '\'' +
+				", commodityOPOfLCP='" + commodityOPOfLCP + '\'' +
+				", commodityType=" + commodityType +
+				", isSpot=" + isSpot +
+				", liveStartTime=" + liveStartTime +
+				", liveEndTime=" + liveEndTime +
+				", create_by='" + create_by + '\'' +
+				", liveId='" + liveId + '\'' +
+				", commodityOriginalPrice='" + commodityOriginalPrice + '\'' +
+				", commodityCurrentPrice='" + commodityCurrentPrice + '\'' +
+				", shopId='" + shopId + '\'' +
+				", shopMid='" + shopMid + '\'' +
+				", commodityShopNumber='" + commodityShopNumber + '\'' +
+				", commodityPicList=" + commodityPicList +
+				", commoditySpecList=" + commoditySpecList +
+				", commodityCode='" + commodityCode + '\'' +
+				", commodityCommission='" + commodityCommission + '\'' +
+				", brandMid='" + brandMid + '\'' +
+				", mallMid='" + mallMid + '\'' +
+				", shopName='" + shopName + '\'' +
+				", supportCourierDeliver=" + supportCourierDeliver +
+				", supportSelfPickup=" + supportSelfPickup +
+				", identity=" + identity +
+				", processIdentity=" + processIdentity +
+				'}';
 	}
-
 	public Integer getSource() {
 		return source;
 	}
-
 	public void setSource(Integer source) {
 		this.source = source;
 	}
-
-	/*
-	 * public Integer getDistribution() { return distribution; } public void
-	 * setDistribution(Integer distribution) { this.distribution = distribution;
-	 * }
-	 */
+	/*public Integer getDistribution() {
+		return distribution;
+	}
+	public void setDistribution(Integer distribution) {
+		this.distribution = distribution;
+	}*/
 	public Integer getFreight() {
 		return freight;
 	}
-
 	public boolean isSupportSelfPickup() {
 		return supportSelfPickup;
 	}
-
 	public void setSupportSelfPickup(boolean supportSelfPickup) {
 		this.supportSelfPickup = supportSelfPickup;
 	}
-
 	public void setFreight(Integer freight) {
 		this.freight = freight;
 	}
-
 	public Integer getTerminalType() {
 		return terminalType;
 	}
-
 	public void setTerminalType(Integer terminalType) {
 		this.terminalType = terminalType;
 	}
-
 	public Integer getStockStatus() {
 		return stockStatus;
 	}
-
 	public void setStockStatus(Integer stockStatus) {
 		this.stockStatus = stockStatus;
 	}
-
 	public String getReason() {
 		return reason;
 	}
-
 	public void setReason(String reason) {
 		this.reason = reason;
 	}
-
 	public String getMallId() {
 		return mallId;
 	}
-
 	public void setMallId(String mallId) {
 		this.mallId = mallId;
 	}
-
 	public String getBrandName() {
 		return brandName;
 	}
-
 	public void setBrandName(String brandName) {
 		this.brandName = brandName;
 	}
-
 	public String getShopNum() {
 		return shopNum;
 	}
-
 	public void setShopNum(String shopNum) {
 		this.shopNum = shopNum;
 	}
-
 	public int getBrandId() {
 		return brandId;
 	}
-
 	public void setBrandId(int brandId) {
 		this.brandId = brandId;
 	}
-
 	public String getFilialeMid() {
 		return filialeMid;
 	}
-
 	public void setFilialeMid(String filialeMid) {
 		this.filialeMid = filialeMid;
 	}
 
+	public int getIdentity() {
+		return identity;
+	}
+
+	public void setIdentity(int identity) {
+		this.identity = identity;
+	}
 }
--- easy-common/easy-entity/src/main/java/com/rongyi/easy/roa/param/FilialeParam.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/roa/param/FilialeParam.java
@@ -10,12 +10,52 @@ import java.io.Serializable;
 public class FilialeParam implements Serializable {
 
 	private static final long serialVersionUID = 5321646214285676988L;
-
+	/**
+	 * id
+	 */
 	private String id;
+	/**
+	 * 关联品牌id
+	 */
 	private String brandMid;
+	/**
+	 * 名称
+	 */
 	private String name;
+	/**
+	 * 负责人
+	 */
+	private String manager;
+	/**
+	 * 联系电话
+	 */
+	private String telephone;
+	//省id
+	private String proId;
+	private String cityId;
+	private String areaId;
+	//查询用区域id
+	private String zoneId;
+	/**
+	 * 详细地址
+	 */
+	private String address;
+	/**
+	 * 关联店铺ids
+	 */
+	private String shopIds;
+	/**
+	 * 当前页
+	 */
 	private Integer currentPage;
+	/**
+	 * 每页个数
+	 */
 	private Integer pageSize;
+	/**
+	 * 状态
+	 */
+	private Integer valid;
 	public String getId() {
 		return id;
 	}
@@ -34,6 +74,54 @@ public class FilialeParam implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public String getManager() {
+		return manager;
+	}
+	public void setManager(String manager) {
+		this.manager = manager;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getProId() {
+		return proId;
+	}
+	public void setProId(String proId) {
+		this.proId = proId;
+	}
+	public String getCityId() {
+		return cityId;
+	}
+	public void setCityId(String cityId) {
+		this.cityId = cityId;
+	}
+	public String getAreaId() {
+		return areaId;
+	}
+	public void setAreaId(String areaId) {
+		this.areaId = areaId;
+	}
+	public String getZoneId() {
+		return zoneId;
+	}
+	public void setZoneId(String zoneId) {
+		this.zoneId = zoneId;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getShopIds() {
+		return shopIds;
+	}
+	public void setShopIds(String shopIds) {
+		this.shopIds = shopIds;
+	}
 	public Integer getCurrentPage() {
 		return currentPage;
 	}
@@ -45,5 +133,18 @@ public class FilialeParam implements Serializable {
 	}
 	public void setPageSize(Integer pageSize) {
 		this.pageSize = pageSize;
+	}
+	public Integer getValid() {
+		return valid;
+	}
+	public void setValid(Integer valid) {
+		this.valid = valid;
+	}
+	@Override
+	public String toString() {
+		return "FilialeParam [id=" + id + ", brandMid=" + brandMid + ", name=" + name + ", manager=" + manager
+				+ ", telephone=" + telephone + ", proId=" + proId + ", cityId=" + cityId + ", areaId=" + areaId
+				+ ", zoneId=" + zoneId + ", address=" + address + ", shopIds=" + shopIds + ", currentPage="
+				+ currentPage + ", pageSize=" + pageSize + ", valid=" + valid + "]";
 	}
 }
