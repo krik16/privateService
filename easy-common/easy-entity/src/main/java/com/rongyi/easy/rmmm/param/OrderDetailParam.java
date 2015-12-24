234891c46be9cb9094fcdd01aecc104e15b2d2e0
chenjun <chenjun@rongyi.com>
2015-12-24 10:06:33 +0800

    增加买家关闭订单接口

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/param/OrderDetailParam.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/param/OrderDetailParam.java
@@ -1,15 +1,28 @@
 package com.rongyi.easy.rmmm.param;
 
+import java.io.Serializable;
+
+import com.rongyi.easy.malllife.param.MalllifeBaseParam;
+
 /**
  * 订单详情参数
  * @author chenjun
  *
  */
-public class OrderDetailParam {
+public class OrderDetailParam extends MalllifeBaseParam implements Serializable{
 
 	private String orderId;// 大订单id
 	private boolean cartOrder = false;//true 是购物车订单，false:普通订单
+	private String orderNum;//订单号
 	
+	public String getOrderNum() {
+		return orderNum;
+	}
+
+	public void setOrderNum(String orderNum) {
+		this.orderNum = orderNum;
+	}
+
 	public boolean isCartOrder() {
 		return cartOrder;
 	}
