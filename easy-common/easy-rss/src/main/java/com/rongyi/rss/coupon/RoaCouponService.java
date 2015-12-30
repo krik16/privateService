f76637aacd298d2ec7aa73c1f2eadc6dc3613014
liaoqianyang <liaoqianyang@rongyi.com>
2015-12-30 10:17:26 +0800

    摩店卡券权限校验

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/coupon/RoaCouponService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/coupon/RoaCouponService.java
@@ -1,6 +1,7 @@
 package com.rongyi.rss.coupon;
 
 import com.rongyi.core.bean.ResponseResult;
+import com.rongyi.core.bean.ResponseVO;
 import com.rongyi.core.common.PagingVO;
 import com.rongyi.easy.coupon.entity.Coupon;
 import com.rongyi.easy.coupon.entity.CouponActivity;
@@ -184,4 +185,16 @@ public interface RoaCouponService {
      * @return true:可退 | false：不可退
      */
     boolean isCanRefundCoupon(String couponId);
+
+    /**
+     * 校验摩店登录的人是否有操作这张券的权限
+     * 摩店两种角色：店长、导购，两种角色只能操作关联类型是店铺的券
+     * @param userId 用户id
+     * @param couponId 卡券id
+     * @return ResponseVO 有权限errno=0，没权限errno=1020831, msg="您无权限操作，如需修改请联系集团管理员"
+     *                                         errno=1020832, msg="您无权限操作，如需修改请联系品牌管理员"
+     *                                         errno=1020833, msg="您无权限操作，如需修改请联系商场管理员"
+     * @author lqy
+     */
+    ResponseVO checkMallShopAuthority(String userId, String couponId);
 }
