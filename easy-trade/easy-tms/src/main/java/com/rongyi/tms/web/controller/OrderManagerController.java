2750b6327d1ae2ae2dd0426e0329a845637738d8
hebo <hebo@rongyi.com>
2016-01-12 15:18:15 +0800

    运营订单

--- easy-trade/easy-tms/src/main/java/com/rongyi/tms/web/controller/OrderManagerController.java
+++ easy-trade/easy-tms/src/main/java/com/rongyi/tms/web/controller/OrderManagerController.java
@@ -34,7 +34,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
