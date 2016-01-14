8d3338c7d3a10458908f3d9e37dcd28db4a148b1
袁波 <yuanbo@rongyi.com>
2016-01-14 14:23:38 +0800

    微商城  发送微信消息

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/msgcenter/OrderPushParam.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/msgcenter/OrderPushParam.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
  * Package:com.rongyi.easy.msgcenter
  * Project:message-center
  */
-public class OrderPushParam {
+public class OrderPushParam implements Serializable {
 
     /**
      * 通知类型：
@@ -17,21 +17,21 @@ public class OrderPushParam {
      * 2：订单付款；
      * 3：订单发货；
      * 25：交易完成；
-     * 5：买家指定时间未付款，订单取消；
+     * // 5：买家指定时间未付款，订单取消；暂不支持
      * 21：卖家关闭订单，订单取消；
-     * 8：退款申请；
-     * //退款结果
+     * //8：退款申请；暂不支持
+     * //退款结果；暂不支持
      */
     private Integer type;
     private String userId;
-    private String orderNo;
+    private Integer orderId;
 
     @Override
     public String toString() {
         final StringBuffer sb = new StringBuffer("OrderUpdatePushParam{");
         sb.append("type=").append(type);
         sb.append(", userId='").append(userId).append('\'');
-        sb.append(", orderNo='").append(orderNo).append('\'');
+        sb.append(", orderId='").append(orderId).append('\'');
         sb.append('}');
         return sb.toString();
     }
@@ -52,11 +52,11 @@ public class OrderPushParam {
         this.userId = userId;
     }
 
-    public String getOrderNo() {
-        return orderNo;
+    public Integer getOrderId() {
+        return orderId;
     }
 
-    public void setOrderNo(String orderNo) {
-        this.orderNo = orderNo;
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
     }
 }
