bcd27d5f5d6a25008c6ec126e294664d578fde3d
liaoqianyang <liaoqianyang@rongyi.com>
2016-01-20 18:36:20 +0800

    活动一期

---
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/activitymanage/ActivityService.java
@@ -0,0 +1,12 @@
+package com.rongyi.rss.activitymanage;
+
+import com.rongyi.easy.activitymanage.entity.ActivityInfo;
+
+/**
+ * 活动后台管理接口
+ * Created by Leon on 2016/1/20.
+ */
+public interface ActivityService {
+
+    boolean saveActivityInfo(ActivityInfo activityInfo);
+}
