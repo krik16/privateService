34241f58131eb425f70f924f0e7f1ae27fe91cec
zhangzhengping <zhangzhengping@rongyi.com>
2016-01-21 11:55:13 +0800

    0202商品详情导购规则改变

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/msob/IShopService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/msob/IShopService.java
@@ -95,6 +95,14 @@ public interface IShopService{
 	 * @throws Exception
 	 */
 	public BrandShopDetailVO getCommodityShopDetail(String shopId, UserInfoVO user) throws Exception;
+	/**
+	 * 商品所在店铺信息V0202
+	 * @param shopId
+	 * @param user
+	 * @return
+	 * @throws Exception
+	 */
+	public BrandShopDetailVO getCommodityShopInfoDetail(String shopId, UserInfoVO user) throws Exception;
 	public List<String> getSellerImIdByShopId(String shopId);
 
 	public ShopInfoEntity getShopByShopMId(String shopMId) throws RmmmException;
