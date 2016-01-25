76cc5adc12b2ad1c3812ec4a8c50df22bc5b3a59
yuzhijian <yuxia2217@163.com>
2016-01-25 15:38:19 +0800

    埋点

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/malllife/LogsType.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/malllife/LogsType.java
@@ -13,20 +13,20 @@ public interface LogsType {
 
 
     interface FORUM{
-        String MALL_INFO = "商场详情";
-        String SHOP_INFO = "店铺详情";
-        String BRAND_INFO = "品牌详情";
-        String COMMODITY_INFO="商品详情";
-        String COMMODITY_LIST="商品列表";
-        String COUPON_INFO="优惠券详情";
-        String COUPON_LIST="优惠券列表";
-        String ARTICLE_INFO="文章详情";
-        String ARTICLE_LIST="文章列表";
-        String CLEARANCECOMMODITY_INFO="临时特卖列表";
-        String LIVESEARCH_INFO="直播详情";
-        String LIVESEARCH_LIST="直播列表";
-        String SHOPSEARCH_INFO="店铺列表";
-        String ACTIVITY_INFO="活动详情";
-        String ACTIVITY_RECOMMEND="同城活动推荐";
+        String MALL_INFO = "mallInfo";//"商场详情";
+        String SHOP_INFO = "shopInfo";//"店铺详情";
+        String SHOPSEARCH_INFO="shopList";//"店铺列表";
+        String BRAND_INFO = "brandInfo";//"品牌详情";
+        String COMMODITY_INFO="commodityInfo";//"商品详情";
+        String COMMODITY_LIST="commodityList";//"商品列表";
+        String COUPON_INFO="couponInfo";//"优惠券详情";
+        String COUPON_LIST="couponList";//"优惠券列表";
+        String ARTICLE_INFO="articleInfo";//"文章详情";
+        String ARTICLE_LIST="articleList";//"文章列表";
+        String CLEARANCECOMMODITY_INFO="saleList";//"特卖列表";
+        String LIVESEARCH_INFO="liveInfo";//"直播详情";
+        String LIVESEARCH_LIST="liveList";//"直播列表";
+        String ACTIVITY_INFO="activityInfo";//"活动详情";
+        String ACTIVITY_RECOMMEND="activityList";//"同城活动列表(推荐)";
     }
 }
