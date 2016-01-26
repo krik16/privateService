c1e27f7b564dc065f9380465a162f8c8e9a58f06
zhengyiliang <zhengyiliang@rongyi.com>
2016-01-26 15:59:46 +0800

    merge develop

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/bdata/ShopService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/bdata/ShopService.java
@@ -1,10 +1,10 @@
 package com.rongyi.rss.bdata;
 
-import java.util.List;
-import java.util.Map;
-
 import com.rongyi.core.bean.ResponseVO;
 import com.rongyi.easy.bdata.entity.Shop;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * 店铺服务
@@ -22,23 +22,9 @@ public interface ShopService {
 	 * @param page			当前页
 	 * @param pageSize		分页条数
 	 * @return 带ShopVO 的ResponseVO
-	 * @throws Exception 
+	 * @throws Exception
 	 */
-	ResponseVO searchShops(String mallId, String zoneId, String keyword, String customCateId, int page, int pageSize) throws Exception;
-
-	/**
-	 * 店铺搜索
-	 * @param mallId		商场ID
-	 * @param keyword		关键词（商场名称）
-	 * @param customCateId	自定义分类ID
-	 * @param page			当前页
-	 * @param pageSize		分页条数
-	 * @return 带ShopVO 的ResponseVO
-	 * @throws Exception 
-	 */
-	ResponseVO searchShops(String mallId, String keyword, String customCateId, int page, int pageSize) throws Exception;
-
-	ResponseVO searchShops(String mallId, String keyword, String customCateId, String zondId, int page, int pageSize) throws Exception;
+	ResponseVO searchShops(String mallId, String keyword, String customCateId, String zoneId, int page, int pageSize) throws Exception;
 	
 	/**
 	 * 获取单个店铺详情
