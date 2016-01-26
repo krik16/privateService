f03b80c739be577694a88d38375173f47f00dacd
wangjianhua@rongyi.com <wangjianhua@rongyi.com>
2016-01-26 11:11:39 +0800

    1.保留之前查询店铺接口（无zoneid)

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/bdata/ShopService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/bdata/ShopService.java
@@ -25,7 +25,19 @@ public interface ShopService {
 	 * @throws Exception 
 	 */
 	ResponseVO searchShops(String mallId, String zoneId, String keyword, String customCateId, int page, int pageSize) throws Exception;
-	
+
+	/**
+	 * 店铺搜索
+	 * @param mallId		商场ID
+	 * @param keyword		关键词（商场名称）
+	 * @param customCateId	自定义分类ID
+	 * @param page			当前页
+	 * @param pageSize		分页条数
+	 * @return 带ShopVO 的ResponseVO
+	 * @throws Exception 
+	 */
+	ResponseVO searchShops(String mallId, String keyword, String customCateId, int page, int pageSize) throws Exception;
+
 	/**
 	 * 获取单个店铺详情
 	 * @param id 店铺id
