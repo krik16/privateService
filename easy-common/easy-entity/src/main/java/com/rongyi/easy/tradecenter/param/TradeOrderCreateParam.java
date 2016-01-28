af261622b443014c14a8cff88a884f21bd122878
kejun <kejun@rongyi.com>
2016-01-28 13:35:18 +0800

    优惠券下单增加微信标准版相关参数

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/tradecenter/param/TradeOrderCreateParam.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/tradecenter/param/TradeOrderCreateParam.java
@@ -144,6 +144,16 @@ public class TradeOrderCreateParam implements Serializable {
     private Date nextStatusTime;
 
     /**
+     * 微信标准版支付记录appid
+     */
+    private String weixinAppId;
+
+    /**
+     * 微信标准版支付渠道
+     */
+    private String weixinChannel;
+
+    /**
      * end
      */
 
@@ -423,6 +433,22 @@ public class TradeOrderCreateParam implements Serializable {
         this.idfa = idfa;
     }
 
+    public String getWeixinAppId() {
+        return weixinAppId;
+    }
+
+    public void setWeixinAppId(String weixinAppId) {
+        this.weixinAppId = weixinAppId;
+    }
+
+    public String getWeixinChannel() {
+        return weixinChannel;
+    }
+
+    public void setWeixinChannel(String weixinChannel) {
+        this.weixinChannel = weixinChannel;
+    }
+
     @Override
     public String toString() {
         return "TradeOrderCreateParam{" +
@@ -455,6 +481,8 @@ public class TradeOrderCreateParam implements Serializable {
                 ", statusHold=" + statusHold +
                 ", nextStatusTime=" + nextStatusTime +
                 ", platformRebateCode=" + platformRebateCode +
+                ", weixinAppId=" + weixinAppId +
+                ", weixinChannel=" + weixinChannel +
                 '}';
     }
 }
