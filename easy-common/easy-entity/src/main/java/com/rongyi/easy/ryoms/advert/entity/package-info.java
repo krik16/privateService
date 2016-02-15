83066a48865981d3934fa2e7e768e658d4c47ebf
shaozhou <shaozhou@rongyi.com>
2016-02-15 16:38:36 +0800

    广告一期新增package

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/ryoms/advert/entity/package-info.java
@@ -0,0 +1,15 @@
+/**   
+* @Title: package-info.java 
+* @Package com.rongyi.easy.ryoms.advert.entity 
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author shaozhou
+* @date 2016年2月15日 下午4:36:42 
+* @version V1.0   
+*/
+/** 
+* @ClassName: package-info 
+* @Description: TODO(这里用一句话描述这个类的作用) 
+* @author shaozhou 
+* @date 2016年2月15日 下午4:36:42 
+*/
+package com.rongyi.easy.ryoms.advert.entity;
