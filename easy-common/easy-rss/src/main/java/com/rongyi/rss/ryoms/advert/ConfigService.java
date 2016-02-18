e60a121e036d18fbe18ee0a7ca1e46ab908cb5b6
wangjianhua <wangjianhua@rongyi.com>
2016-02-18 18:41:13 +0800

    1. 添加参数配置对应实体类 2。添加城市对应实体类 3。添加操作记录实体类 4。定义参数配置接口（列表、新建、修改、详情、启用、停用、最新停用理由、获取城市列表等）

---
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/ryoms/advert/ConfigService.java
@@ -0,0 +1,102 @@
+/**   
+* @Title: ConfigService.java 
+* @Package com.rongyi.rss.ryoms.advert 
+* @Description: 参数配置管理接口
+* @author wangjh
+* @date 2016年2月15日 下午4:38:09 
+* @version V1.0   
+*/
+package com.rongyi.rss.ryoms.advert;
+
+import java.util.List;
+
+import com.rongyi.easy.ryoms.advert.entity.ConfigEntity;
+import com.rongyi.easy.ryoms.advert.param.ConfigSearchParam;
+import com.rongyi.easy.ryoms.advert.vo.CityVO;
+import com.rongyi.easy.ryoms.advert.vo.ConfigVO;
+import com.rongyi.easy.ryoms.advert.vo.ReasonVO;
+
+/**
+ * @ClassName: ConfigService
+ * @Description: 参数配置管理接口
+ * @author wangjh
+ * @date 2016年2月15日 下午4:38:09
+ */
+public interface ConfigService {
+
+	/**
+	 * 配置列表
+	 * 
+	 * @param param
+	 * @return
+	 */
+	public List<ConfigVO> list(ConfigSearchParam param);
+
+	/**
+	 * 配置列表数
+	 * 
+	 * @param param
+	 * @return
+	 */
+	public int listCnt(ConfigSearchParam param);
+
+	/**
+	 * 新增配置
+	 * @param entity
+	 * @return
+	 */
+	public int add(ConfigEntity entity);
+
+	/**
+	 * 修改配置
+	 * 
+	 * @param entity
+	 * @return
+	 */
+	public int save(ConfigEntity entity);
+
+	/**
+	 * 配置详情
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public ConfigVO info(int configId);
+
+	/**
+	 * 启用配置
+	 * 
+	 * @param id
+	 * @param userId
+	 * @return
+	 */
+	public int enable(int configId, int userId);
+
+	/**
+	 * 停用配置
+	 * 
+	 * @param id
+	 * @param userId
+	 * @param reason
+	 * @return
+	 */
+	public int disable(int configId, int userId, String reason);
+
+	/**
+	 * 停用理由列表(最新5条)
+	 * 
+	 * @param id
+	 * @param size
+	 * @return
+	 */
+	public List<ReasonVO> topDisableReasonList(int configId);
+	
+	/**
+	 * 停用理由列表
+	 * 
+	 * @param id
+	 * @param size
+	 * @return
+	 */
+	public List<CityVO> cityListByArea(int areaId);
+}
