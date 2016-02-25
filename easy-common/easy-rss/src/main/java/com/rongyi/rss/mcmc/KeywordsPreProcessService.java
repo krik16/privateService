a4815996da44dc3dc3c0402e50002414d2c8cb58
xieguoqiang <xieguoqiang@rongyi.com>
2016-02-25 17:53:38 +0800

    搜索优化 - 优惠搜索预处理。

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/mcmc/KeywordsPreProcessService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/mcmc/KeywordsPreProcessService.java
@@ -1,9 +1,12 @@
 package com.rongyi.rss.mcmc;
 
+import com.rongyi.easy.solr.param.ActivitySearchParam;
 import com.rongyi.easy.solr.param.CommoditySearchParam;
 
 public interface KeywordsPreProcessService {
 
     public CommoditySearchParam process(CommoditySearchParam commoditySearchParam);
+
+    public ActivitySearchParam process(ActivitySearchParam activitySearchParam);
 
 }
