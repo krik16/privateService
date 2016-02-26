f3383715c71039a9b2d0f4a13be23dfbb2f9e6dc
kejun <kejun@rongyi.com>
2016-02-26 13:45:36 +0800

    佣金controller

---
+++ easy-trade/easy-tms/src/main/java/com/rongyi/tms/web/controller/v2/SaleCommissionController.java
@@ -0,0 +1,10 @@
+package com.rongyi.tms.web.controller.v2;
+
+/**
+ * @Copyright (C), 上海容易网电子商务有限公司
+ * kejun
+ * 2016/2/26 13:44
+ * 新版本佣金controller
+ **/
+public class SaleCommissionController {
+}
