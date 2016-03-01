5f9883841c219e70c2f9e33f0436da853f611455
shaozhou <shaozhou@rongyi.com>
2016-03-01 17:35:05 +0800

    参数配置返回优化

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/ryoms/advert/entity/ConfigEntity.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/ryoms/advert/entity/ConfigEntity.java
@@ -1,91 +1,18 @@
 package com.rongyi.easy.ryoms.advert.entity;
 
+import java.io.Serializable;
+import java.util.Date;
+
 /**
  * @Description: 配置新增/修改保存的实类
  * @author wangjh
  * @date 2016年2月18日
  */
-public class ConfigEntity {
-	public int getConfigId() {
-		return configId;
-	}
-
-	public void setConfigId(int configId) {
-		this.configId = configId;
-	}
-
-	public String getMallName() {
-		return mallName;
-	}
-
-	public void setMallName(String mallName) {
-		this.mallName = mallName;
-	}
-
-	public String getMallLevel() {
-		return mallLevel;
-	}
-
-	public void setMallLevel(String mallLevel) {
-		this.mallLevel = mallLevel;
-	}
-
-	public String getCityMId() {
-		return cityMId;
-	}
-
-	public void setCityMId(String cityMId) {
-		this.cityMId = cityMId;
-	}
-
-	public int getPosition() {
-		return position;
-	}
-
-	public void setPosition(int position) {
-		this.position = position;
-	}
-
-	public int getCountTotal() {
-		return countTotal;
-	}
-
-	public void setCountTotal(int countTotal) {
-		this.countTotal = countTotal;
-	}
-
-	public int getCountRY() {
-		return countRY;
-	}
-
-	public void setCountRY(int countRy) {
-		this.countRY = countRy;
-	}
-
-	public int getCountMall() {
-		return countMall;
-	}
-
-	public void setCountMall(int countMall) {
-		this.countMall = countMall;
-	}
-
-	public int getUserId() {
-		return userId;
-	}
-
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
-
-	public String getMallMId() {
-		return mallMId;
-	}
-
-	public void setMallMId(String mallMId) {
-		this.mallMId = mallMId;
-	}
-
+public class ConfigEntity implements Serializable {
+	/** 
+	* @Fields serialVersionUID
+	*/ 
+	private static final long serialVersionUID = 2061650543938037443L;
 	// 编号
 	private int configId = 0;
 	// 商城Id
@@ -106,4 +33,192 @@ public class ConfigEntity {
 	private int countMall = 0;
 	// 用户Id
 	private int userId = 0;
+	private Integer create_by;
+	private Date create_at;
+	private Integer update_by;
+	private Date update_at;
+	/**
+	 * @return the configId
+	 */
+	public int getConfigId() {
+		return configId;
+	}
+	/**
+	 * @param configId the configId to set
+	 */
+	public void setConfigId(int configId) {
+		this.configId = configId;
+	}
+	/**
+	 * @return the mallMId
+	 */
+	public String getMallMId() {
+		return mallMId;
+	}
+	/**
+	 * @param mallMId the mallMId to set
+	 */
+	public void setMallMId(String mallMId) {
+		this.mallMId = mallMId;
+	}
+	/**
+	 * @return the mallName
+	 */
+	public String getMallName() {
+		return mallName;
+	}
+	/**
+	 * @param mallName the mallName to set
+	 */
+	public void setMallName(String mallName) {
+		this.mallName = mallName;
+	}
+	/**
+	 * @return the mallLevel
+	 */
+	public String getMallLevel() {
+		return mallLevel;
+	}
+	/**
+	 * @param mallLevel the mallLevel to set
+	 */
+	public void setMallLevel(String mallLevel) {
+		this.mallLevel = mallLevel;
+	}
+	/**
+	 * @return the cityMId
+	 */
+	public String getCityMId() {
+		return cityMId;
+	}
+	/**
+	 * @param cityMId the cityMId to set
+	 */
+	public void setCityMId(String cityMId) {
+		this.cityMId = cityMId;
+	}
+	/**
+	 * @return the position
+	 */
+	public int getPosition() {
+		return position;
+	}
+	/**
+	 * @param position the position to set
+	 */
+	public void setPosition(int position) {
+		this.position = position;
+	}
+	/**
+	 * @return the countTotal
+	 */
+	public int getCountTotal() {
+		return countTotal;
+	}
+	/**
+	 * @param countTotal the countTotal to set
+	 */
+	public void setCountTotal(int countTotal) {
+		this.countTotal = countTotal;
+	}
+	/**
+	 * @return the countRY
+	 */
+	public int getCountRY() {
+		return countRY;
+	}
+	/**
+	 * @param countRY the countRY to set
+	 */
+	public void setCountRY(int countRY) {
+		this.countRY = countRY;
+	}
+	/**
+	 * @return the countMall
+	 */
+	public int getCountMall() {
+		return countMall;
+	}
+	/**
+	 * @param countMall the countMall to set
+	 */
+	public void setCountMall(int countMall) {
+		this.countMall = countMall;
+	}
+	/**
+	 * @return the userId
+	 */
+	public int getUserId() {
+		return userId;
+	}
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	/**
+	 * @return the create_by
+	 */
+	public Integer getCreate_by() {
+		return create_by;
+	}
+	/**
+	 * @param create_by the create_by to set
+	 */
+	public void setCreate_by(Integer create_by) {
+		this.create_by = create_by;
+	}
+	/**
+	 * @return the create_at
+	 */
+	public Date getCreate_at() {
+		return create_at;
+	}
+	/**
+	 * @param create_at the create_at to set
+	 */
+	public void setCreate_at(Date create_at) {
+		this.create_at = create_at;
+	}
+	/**
+	 * @return the update_by
+	 */
+	public Integer getUpdate_by() {
+		return update_by;
+	}
+	/**
+	 * @param update_by the update_by to set
+	 */
+	public void setUpdate_by(Integer update_by) {
+		this.update_by = update_by;
+	}
+	/**
+	 * @return the update_at
+	 */
+	public Date getUpdate_at() {
+		return update_at;
+	}
+	/**
+	 * @param update_at the update_at to set
+	 */
+	public void setUpdate_at(Date update_at) {
+		this.update_at = update_at;
+	}
+	/** 
+	* @Title: toString 
+	* @Description: TODO(这里用一句话描述这个方法的作用) 
+	* @param @return    设定文件 
+	* @author shaozhou
+	* @date 2016年3月1日 下午5:21:44
+	* @throws 
+	*/
+	@Override
+	public String toString() {
+		return "ConfigEntity [configId=" + configId + ", mallMId=" + mallMId + ", mallName=" + mallName + ", mallLevel="
+				+ mallLevel + ", cityMId=" + cityMId + ", position=" + position + ", countTotal=" + countTotal
+				+ ", countRY=" + countRY + ", countMall=" + countMall + ", userId=" + userId + ", create_by="
+				+ create_by + ", create_at=" + create_at + ", update_by=" + update_by + ", update_at=" + update_at
+				+ "]";
+	}
 }
--- easy-common/easy-entity/src/main/java/com/rongyi/easy/ryoms/advert/vo/ConfigVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/ryoms/advert/vo/ConfigVO.java
@@ -1,6 +1,7 @@
 package com.rongyi.easy.ryoms.advert.vo;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @Description: 配置VO类
@@ -12,151 +13,6 @@ public class ConfigVO implements Serializable {
 	* @Fields serialVersionUID
 	*/ 
 	private static final long serialVersionUID = -1452982881105333559L;
-	/** 
-	* @Title: toString 
-	* @Description: TODO(这里用一句话描述这个方法的作用) 
-	* @param @return    设定文件 
-	* @author shaozhou
-	* @date 2016年2月29日 下午6:13:26
-	* @throws 
-	*/
-	@Override
-	public String toString() {
-		return "ConfigVO [configId=" + configId + ", mallMId=" + mallMId + ", mallName=" + mallName + ", mallLevel="
-				+ mallLevel + ", positionId=" + positionId + ", positionName=" + positionName + ", cityMId=" + cityMId
-				+ ", cityName=" + cityName + ", cityLevel=" + cityLevel + ", areaId=" + areaId + ", areaName="
-				+ areaName + ", countTotal=" + countTotal + ", countRY=" + countRY + ", countMall=" + countMall
-				+ ", statusId=" + statusId + ", statusName=" + statusName + "]";
-	}
-
-	public int getConfigId() {
-		return configId;
-	}
-
-	public void setConfigId(int configId) {
-		this.configId = configId;
-	}
-
-	public String getMallName() {
-		return mallName;
-	}
-
-	public void setMallName(String mallName) {
-		this.mallName = mallName;
-	}
-
-	public String getMallLevel() {
-		return mallLevel;
-	}
-
-	public void setMallLevel(String mallLevel) {
-		this.mallLevel = mallLevel;
-	}
-
-	public int getPositionId() {
-		return positionId;
-	}
-
-	public void setPositionId(int positionId) {
-		this.positionId = positionId;
-	}
-
-	public String getPositionName() {
-		return positionName;
-	}
-
-	public void setPositionName(String positionName) {
-		this.positionName = positionName;
-	}
-
-	public String getCityMId() {
-		return cityMId;
-	}
-
-	public void setCityMId(String cityMId) {
-		this.cityMId = cityMId;
-	}
-
-	public String getCityName() {
-		return cityName;
-	}
-
-	public void setCityName(String cityName) {
-		this.cityName = cityName;
-	}
-
-	public int getCityLevel() {
-		return cityLevel;
-	}
-
-	public void setCityLevel(int cityLevel) {
-		this.cityLevel = cityLevel;
-	}
-
-	public int getAreaId() {
-		return areaId;
-	}
-
-	public void setAreaId(int areaId) {
-		this.areaId = areaId;
-	}
-
-	public String getAreaName() {
-		return areaName;
-	}
-
-	public void setAreaName(String areaName) {
-		this.areaName = areaName;
-	}
-
-	public int getCountTotal() {
-		return countTotal;
-	}
-
-	public void setCountTotal(int countTotal) {
-		this.countTotal = countTotal;
-	}
-
-	public int getCountMall() {
-		return countMall;
-	}
-
-	public void setCountMall(int countMall) {
-		this.countMall = countMall;
-	}
-
-	public int getStatusId() {
-		return statusId;
-	}
-
-	public void setStatusId(int statusId) {
-		this.statusId = statusId;
-	}
-
-	public String getStatusName() {
-		return statusName;
-	}
-
-	public void setStatusName(String statusName) {
-		this.statusName = statusName;
-	}
-
-	public int getCountRY() {
-		return countRY;
-	}
-
-	public void setCountRY(int countRY) {
-		this.countRY = countRY;
-	}
-
-	public String getMallMId() {
-		return mallMId;
-	}
-
-	public void setMallMId(String mallMId) {
-		this.mallMId = mallMId;
-	}
-
 	// 编号
 	private int configId = 0;
 	// 商城Id
@@ -189,5 +45,319 @@ public class ConfigVO implements Serializable {
 	private int statusId = 0;
 	// 状态名称
 	private String statusName = "";
-
+	private Integer create_int;
+	private Date create_date;
+	private Integer update_int;
+	private Date update_date;
+	private String create_by;
+	private Long create_at;
+	private String update_by;
+	private Long update_at;
+	/**
+	 * @return the configId
+	 */
+	public int getConfigId() {
+		return configId;
+	}
+	/**
+	 * @param configId the configId to set
+	 */
+	public void setConfigId(int configId) {
+		this.configId = configId;
+	}
+	/**
+	 * @return the mallMId
+	 */
+	public String getMallMId() {
+		return mallMId;
+	}
+	/**
+	 * @param mallMId the mallMId to set
+	 */
+	public void setMallMId(String mallMId) {
+		this.mallMId = mallMId;
+	}
+	/**
+	 * @return the mallName
+	 */
+	public String getMallName() {
+		return mallName;
+	}
+	/**
+	 * @param mallName the mallName to set
+	 */
+	public void setMallName(String mallName) {
+		this.mallName = mallName;
+	}
+	/**
+	 * @return the mallLevel
+	 */
+	public String getMallLevel() {
+		return mallLevel;
+	}
+	/**
+	 * @param mallLevel the mallLevel to set
+	 */
+	public void setMallLevel(String mallLevel) {
+		this.mallLevel = mallLevel;
+	}
+	/**
+	 * @return the positionId
+	 */
+	public int getPositionId() {
+		return positionId;
+	}
+	/**
+	 * @param positionId the positionId to set
+	 */
+	public void setPositionId(int positionId) {
+		this.positionId = positionId;
+	}
+	/**
+	 * @return the positionName
+	 */
+	public String getPositionName() {
+		return positionName;
+	}
+	/**
+	 * @param positionName the positionName to set
+	 */
+	public void setPositionName(String positionName) {
+		this.positionName = positionName;
+	}
+	/**
+	 * @return the cityMId
+	 */
+	public String getCityMId() {
+		return cityMId;
+	}
+	/**
+	 * @param cityMId the cityMId to set
+	 */
+	public void setCityMId(String cityMId) {
+		this.cityMId = cityMId;
+	}
+	/**
+	 * @return the cityName
+	 */
+	public String getCityName() {
+		return cityName;
+	}
+	/**
+	 * @param cityName the cityName to set
+	 */
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	/**
+	 * @return the cityLevel
+	 */
+	public int getCityLevel() {
+		return cityLevel;
+	}
+	/**
+	 * @param cityLevel the cityLevel to set
+	 */
+	public void setCityLevel(int cityLevel) {
+		this.cityLevel = cityLevel;
+	}
+	/**
+	 * @return the areaId
+	 */
+	public int getAreaId() {
+		return areaId;
+	}
+	/**
+	 * @param areaId the areaId to set
+	 */
+	public void setAreaId(int areaId) {
+		this.areaId = areaId;
+	}
+	/**
+	 * @return the areaName
+	 */
+	public String getAreaName() {
+		return areaName;
+	}
+	/**
+	 * @param areaName the areaName to set
+	 */
+	public void setAreaName(String areaName) {
+		this.areaName = areaName;
+	}
+	/**
+	 * @return the countTotal
+	 */
+	public int getCountTotal() {
+		return countTotal;
+	}
+	/**
+	 * @param countTotal the countTotal to set
+	 */
+	public void setCountTotal(int countTotal) {
+		this.countTotal = countTotal;
+	}
+	/**
+	 * @return the countRY
+	 */
+	public int getCountRY() {
+		return countRY;
+	}
+	/**
+	 * @param countRY the countRY to set
+	 */
+	public void setCountRY(int countRY) {
+		this.countRY = countRY;
+	}
+	/**
+	 * @return the countMall
+	 */
+	public int getCountMall() {
+		return countMall;
+	}
+	/**
+	 * @param countMall the countMall to set
+	 */
+	public void setCountMall(int countMall) {
+		this.countMall = countMall;
+	}
+	/**
+	 * @return the statusId
+	 */
+	public int getStatusId() {
+		return statusId;
+	}
+	/**
+	 * @param statusId the statusId to set
+	 */
+	public void setStatusId(int statusId) {
+		this.statusId = statusId;
+	}
+	/**
+	 * @return the statusName
+	 */
+	public String getStatusName() {
+		return statusName;
+	}
+	/**
+	 * @param statusName the statusName to set
+	 */
+	public void setStatusName(String statusName) {
+		this.statusName = statusName;
+	}
+	/**
+	 * @return the create_int
+	 */
+	public Integer getCreate_int() {
+		return create_int;
+	}
+	/**
+	 * @param create_int the create_int to set
+	 */
+	public void setCreate_int(Integer create_int) {
+		this.create_int = create_int;
+	}
+	/**
+	 * @return the create_date
+	 */
+	public Date getCreate_date() {
+		return create_date;
+	}
+	/**
+	 * @param create_date the create_date to set
+	 */
+	public void setCreate_date(Date create_date) {
+		this.create_date = create_date;
+	}
+	/**
+	 * @return the update_int
+	 */
+	public Integer getUpdate_int() {
+		return update_int;
+	}
+	/**
+	 * @param update_int the update_int to set
+	 */
+	public void setUpdate_int(Integer update_int) {
+		this.update_int = update_int;
+	}
+	/**
+	 * @return the update_date
+	 */
+	public Date getUpdate_date() {
+		return update_date;
+	}
+	/**
+	 * @param update_date the update_date to set
+	 */
+	public void setUpdate_date(Date update_date) {
+		this.update_date = update_date;
+	}
+	/**
+	 * @return the create_by
+	 */
+	public String getCreate_by() {
+		return create_by;
+	}
+	/**
+	 * @param create_by the create_by to set
+	 */
+	public void setCreate_by(String create_by) {
+		this.create_by = create_by;
+	}
+	/**
+	 * @return the create_at
+	 */
+	public Long getCreate_at() {
+		return create_at;
+	}
+	/**
+	 * @param create_at the create_at to set
+	 */
+	public void setCreate_at(Long create_at) {
+		this.create_at = create_at;
+	}
+	/**
+	 * @return the update_by
+	 */
+	public String getUpdate_by() {
+		return update_by;
+	}
+	/**
+	 * @param update_by the update_by to set
+	 */
+	public void setUpdate_by(String update_by) {
+		this.update_by = update_by;
+	}
+	/**
+	 * @return the update_at
+	 */
+	public Long getUpdate_at() {
+		return update_at;
+	}
+	/**
+	 * @param update_at the update_at to set
+	 */
+	public void setUpdate_at(Long update_at) {
+		this.update_at = update_at;
+	}
+	/** 
+	* @Title: toString 
+	* @Description: TODO(这里用一句话描述这个方法的作用) 
+	* @param @return    设定文件 
+	* @author shaozhou
+	* @date 2016年3月1日 下午5:05:36
+	* @throws 
+	*/
+	@Override
+	public String toString() {
+		return "ConfigVO [configId=" + configId + ", mallMId=" + mallMId + ", mallName=" + mallName + ", mallLevel="
+				+ mallLevel + ", positionId=" + positionId + ", positionName=" + positionName + ", cityMId=" + cityMId
+				+ ", cityName=" + cityName + ", cityLevel=" + cityLevel + ", areaId=" + areaId + ", areaName="
+				+ areaName + ", countTotal=" + countTotal + ", countRY=" + countRY + ", countMall=" + countMall
+				+ ", statusId=" + statusId + ", statusName=" + statusName + ", create_int=" + create_int
+				+ ", create_date=" + create_date + ", update_int=" + update_int + ", update_date=" + update_date
+				+ ", create_by=" + create_by + ", create_at=" + create_at + ", update_by=" + update_by + ", update_at="
+				+ update_at + "]";
+	}
 }
