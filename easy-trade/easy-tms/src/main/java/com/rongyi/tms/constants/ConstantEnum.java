2fafb4efd9a8d0b7c968e9aa75ce0b11e6579b94
kejun <kejun@rongyi.com>
2016-03-03 11:33:55 +0800

    佣金权限

--- easy-trade/easy-tms/src/main/java/com/rongyi/tms/constants/ConstantEnum.java
+++ easy-trade/easy-tms/src/main/java/com/rongyi/tms/constants/ConstantEnum.java
@@ -133,6 +133,9 @@ public enum ConstantEnum {
     COMMISSION_CONFIG_CUST_VERIFY_0(0,"系统审核"),
     COMMISSION_CONFIG_CUST_VERIFY_1(1,"人工审核"),
 
+    SERCHER_TYPE_0(0,"客服请求"),
+    SERCHER_TYPE_1(1,"财务请求"),
+
     FIAL_USER_PARAMS_PAYMENT("1019401", "没有登录或登录信息错误"),//前台约定，没有登录统一后三位为401
     FIAL_NO_AUTHORITY_PAYMENT("1019403", "没有访问该资源的权限"),//前台约定，没有权限统一后三位为403
 
--- easy-trade/easy-tms/src/main/java/com/rongyi/tms/web/controller/v2/SalesCommissionControllerV2.java
+++ easy-trade/easy-tms/src/main/java/com/rongyi/tms/web/controller/v2/SalesCommissionControllerV2.java
@@ -2,6 +2,7 @@ package com.rongyi.tms.web.controller.v2;
 
 import com.rongyi.core.bean.ResponseData;
 import com.rongyi.easy.tms.vo.v2.SalesCommissionVO;
+import com.rongyi.tms.Exception.PermissionException;
 import com.rongyi.tms.constants.CodeEnum;
 import com.rongyi.tms.constants.Constant;
 import com.rongyi.tms.constants.ConstantEnum;
@@ -27,20 +28,25 @@ import java.util.Map;
  */
 @Controller
 @RequestMapping("/v2/salesCommission")
-public class SalesCommissionControllerV2 {
-    Logger logger = LoggerFactory.getLogger(SalesCommissionController.class);
+public class SalesCommissionControllerV2 extends BaseControllerV2{
+    private static final Logger LOGGER = LoggerFactory.getLogger(SalesCommissionController.class);
 
     @Autowired
     private SalesCommissionService salesCommissionService;
 
     @RequestMapping("/expandList")
     @ResponseBody
-    public ResponseData findExpandList(@RequestBody Map<String, Object> map) {
+    public ResponseData findExpandList(HttpServletRequest request,@RequestBody Map<String, Object> map) {
         ResponseData result;
         try {
-            logger.info("findExpandList begin paramMap={}", map);
+            LOGGER.info("findExpandList begin paramMap={}", map);
             if (!map.containsKey("currentPage") || !map.containsKey("searchType")){
                 return ResponseData.failure(Integer.valueOf(CodeEnum.ERROR_PARAM.getActionCode()),CodeEnum.ERROR_PARAM.getMessage());
+            }
+            if(ConstantEnum.SERCHER_TYPE_0.getCodeStr().equals(map.get("searchType").toString())){
+                permissionCheck(request,"CUSTOM_EXTRT_VIEW");
+            }else{
+                permissionCheck(request,"FNC_EXTRT_VIEW");
             }
             map.put("pageSize", Constant.PAGE.PAGESIZE);
             map.put("type", ConstantEnum.COMMISSION_TYPE_0.getCodeInt());
@@ -49,8 +55,12 @@ public class SalesCommissionControllerV2 {
             List<SalesCommissionVO> list = salesCommissionService.findCommissionList(map);
             int totalAccount = salesCommissionService.countCommission(map);
             result = ResponseData.success(list, currentPage,Constant.PAGE.PAGESIZE, totalAccount);
-            logger.info("findExpandList end");
-        } catch (Exception e) {
+            LOGGER.info("findExpandList end");
+        } catch (PermissionException e){
+            LOGGER.error(e.getMessage());
+            e.printStackTrace();
+            return ResponseData.failure(Integer.valueOf(e.getCode()), e.getMessage());
+        }catch (Exception e) {
             e.printStackTrace();
             result = ResponseData.failure(Integer.valueOf(CodeEnum.ERROR_SYSTEM.getActionCode()), CodeEnum.ERROR_SYSTEM.getMessage());
         }
@@ -59,12 +69,17 @@ public class SalesCommissionControllerV2 {
 
     @RequestMapping("/firstOrderList")
     @ResponseBody
-    public ResponseData findFirstOrderList(@RequestBody Map<String, Object> map) {
+    public ResponseData findFirstOrderList(HttpServletRequest request,@RequestBody Map<String, Object> map) {
         ResponseData result;
         try {
-            logger.info("findFirstOrderList begin paramMap={}", map);
+            LOGGER.info("findFirstOrderList begin paramMap={}", map);
             if (!map.containsKey("currentPage") || !map.containsKey("searchType")){
                 return ResponseData.failure(Integer.valueOf(CodeEnum.ERROR_PARAM.getActionCode()),CodeEnum.ERROR_PARAM.getMessage());
+            }
+            if(ConstantEnum.SERCHER_TYPE_0.getCodeStr().equals(map.get("searchType").toString())){
+                permissionCheck(request,"CUSTOM_FIRRT_VIEW");
+            }else{
+                permissionCheck(request,"FNC_FIRRT_VIEW");
             }
             map.put("pageSize", Constant.PAGE.PAGESIZE);
             map.put("type", ConstantEnum.COMMISSION_TYPE_1.getCodeInt());
@@ -73,8 +88,12 @@ public class SalesCommissionControllerV2 {
             List<SalesCommissionVO> list = salesCommissionService.findCommissionList(map);
             int totalAccount = salesCommissionService.countCommission(map);
             result = ResponseData.success(list, currentPage,Constant.PAGE.PAGESIZE, totalAccount);
-            logger.info("findFirstOrderList end");
-        } catch (Exception e) {
+            LOGGER.info("findFirstOrderList end");
+        }  catch (PermissionException e){
+            LOGGER.error(e.getMessage());
+            e.printStackTrace();
+            return ResponseData.failure(Integer.valueOf(e.getCode()), e.getMessage());
+        }catch (Exception e) {
             e.printStackTrace();
             result = ResponseData.failure(Integer.valueOf(CodeEnum.ERROR_SYSTEM.getActionCode()), CodeEnum.ERROR_SYSTEM.getMessage());
         }
@@ -87,13 +106,13 @@ public class SalesCommissionControllerV2 {
         ResponseData result;
         try {
             Integer id = Integer.valueOf(request.getParameter("id"));
-            logger.info("detail begin id={}", id);
+            LOGGER.info("detail begin id={}", id);
             if (id == null){
                 return ResponseData.failure(Integer.valueOf(CodeEnum.ERROR_PARAM.getActionCode()),CodeEnum.ERROR_PARAM.getMessage());
             }
             SalesCommissionVO salesCommissionVO = salesCommissionService.getCommissionDetail(id);
             result = ResponseData.success(salesCommissionVO);
-            logger.info("detail end");
+            LOGGER.info("detail end");
         } catch (Exception e) {
             e.printStackTrace();
             result = ResponseData.failure(Integer.valueOf(CodeEnum.ERROR_SYSTEM.getActionCode()), CodeEnum.ERROR_SYSTEM.getMessage());
@@ -103,19 +122,48 @@ public class SalesCommissionControllerV2 {
 
     @RequestMapping("/verify")
     @ResponseBody
-    public ResponseData verifyCommission(@RequestBody VerifyCommissionParam param) {
+    public ResponseData verifyCommission(HttpServletRequest request,@RequestBody VerifyCommissionParam param) {
         ResponseData result;
         try {
-            logger.info("verifyCommission begin param={}", param);
+            LOGGER.info("verifyCommission begin param={}", param);
             if (StringUtils.isBlank(param.getIds()) || param.getStatus()==null
                     || (param.getStatus()<0 && StringUtils.isBlank(param.getDesc()))){
                 return ResponseData.failure(Integer.valueOf(CodeEnum.ERROR_PARAM.getActionCode()),CodeEnum.ERROR_PARAM.getMessage());
             }
+            //推广返佣客服审核
+            if(ConstantEnum.COMMISSION_CONFIG_TYPE_0.getCodeInt().equals(param.getType()) &&
+                    (ConstantEnum.COMMISSION_STATUS_1_UNCHECK.getCodeInt().equals(param.getStatus()) ||
+                    ConstantEnum.COMMISSION_STATUS_2.getCodeInt().equals(param.getStatus()))){
+                permissionCheck(request,"CUSTOM_EXTRT_VFY");
+            //推广佣金财务审核
+            }else if(ConstantEnum.COMMISSION_CONFIG_TYPE_0.getCodeInt().equals(param.getType()) &&
+                    (ConstantEnum.COMMISSION_STATUS_2_UNCHECK.getCodeInt().equals(param.getStatus()) ||
+                            ConstantEnum.COMMISSION_STATUS_3.getCodeInt().equals(param.getStatus()))){
+                permissionCheck(request,"FNC_EXTRT_VFY");
+            //首单返佣客服审核
+            }else if(ConstantEnum.COMMISSION_CONFIG_TYPE_1.getCodeInt().equals(param.getType()) &&
+                    (ConstantEnum.COMMISSION_STATUS_1_UNCHECK.getCodeInt().equals(param.getStatus()) ||
+                            ConstantEnum.COMMISSION_STATUS_2.getCodeInt().equals(param.getStatus()))){
+                permissionCheck(request,"CUSTOM_FIRRT_VFY");
+             //首单返佣财务审核
+            }else if(ConstantEnum.COMMISSION_CONFIG_TYPE_1.getCodeInt().equals(param.getType()) &&
+                    (ConstantEnum.COMMISSION_STATUS_2_UNCHECK.getCodeInt().equals(param.getStatus()) ||
+                            ConstantEnum.COMMISSION_STATUS_3.getCodeInt().equals(param.getStatus()))){
+                permissionCheck(request,"FNC_FIRRT_VFY");
+            }else{
+                LOGGER.info("不存在该审核条件");
+                permissionCheck(request, "NO_PERMISSION");
+            }
+
             if (salesCommissionService.verifyCommission(param, "test"))
                 result = ResponseData.success();
             else
                 result = ResponseData.failure(Integer.valueOf(CodeEnum.ERROR_UPDATE.getActionCode()), CodeEnum.ERROR_UPDATE.getMessage());
-            logger.info("verifyCommission end");
+            LOGGER.info("verifyCommission end");
+        } catch (PermissionException e){
+            LOGGER.error(e.getMessage());
+            e.printStackTrace();
+            return ResponseData.failure(Integer.valueOf(e.getCode()), e.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
             result = ResponseData.failure(Integer.valueOf(CodeEnum.ERROR_SYSTEM.getActionCode()), CodeEnum.ERROR_SYSTEM.getMessage());
