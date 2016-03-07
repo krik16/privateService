5b91a8e7c21b3d8f7a71d2f341acb7dee17dfc83
yuzhijian <yuxia2217@163.com>
2016-03-07 17:29:08 +0800

    用户相关

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/dto/user/UserLoginDto.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/dto/user/UserLoginDto.java
@@ -15,5 +15,64 @@ public class UserLoginDto implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    private String phone; //登录账号
 
+    private String deviceId; //登录设备号
+
+    private String channel; //渠道号
+
+    private String jessionid; //sessionID
+
+    private Integer loginStatus; //登录状态
+
+    private String loginIp; //登录ip
+
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getChannel() {
+        return channel;
+    }
+
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+
+    public String getJessionid() {
+        return jessionid;
+    }
+
+    public void setJessionid(String jessionid) {
+        this.jessionid = jessionid;
+    }
+
+    public Integer getLoginStatus() {
+        return loginStatus;
+    }
+
+    public void setLoginStatus(Integer loginStatus) {
+        this.loginStatus = loginStatus;
+    }
+
+    public String getLoginIp() {
+        return loginIp;
+    }
+
+    public void setLoginIp(String loginIp) {
+        this.loginIp = loginIp;
+    }
 }
