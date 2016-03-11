f6c4c886f655512af74e7732b68ad5a4defa0d0b
袁波 <yuanbo@rongyi.com>
2016-03-11 11:26:03 +0800

    修改注释，卡券类型 活动券【3】

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/coupon/entity/Coupon.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/coupon/entity/Coupon.java
@@ -46,7 +46,7 @@ public class Coupon implements Serializable {
     private String name;
 
     /**
-     * 卡券类型:代金券[0], 抵扣券[1], 红包[2]
+     * 卡券类型:代金券[0], 抵扣券[1], 红包[2],活动券[3]
      */
     private Integer couponType;
 
