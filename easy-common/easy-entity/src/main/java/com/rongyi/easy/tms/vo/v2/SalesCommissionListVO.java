6024865f598fc312aa3895756df29b4a17dc480d
zhengyiliang <zhengyiliang@rongyi.com>
2016-03-15 16:32:58 +0800

    Serialized problem

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/tms/vo/v2/SalesCommissionListVO.java
@@ -0,0 +1,33 @@
+package com.rongyi.easy.tms.vo.v2;
+
+import java.io.Serializable;
+import java.util.List;
+
+/*
+ * Copyright (C),上海容易网电子商务有限公司
+ * Author: ZhengYl
+ * Description:  
+ * time:  2016/3/15
+ * History: 变更记录       
+ *
+ */
+
+public class SalesCommissionListVO implements Serializable {
+
+    List<SalesCommissionVO> salesCommissionVOs;
+
+    public List<SalesCommissionVO> getSalesCommissionVOs() {
+        return salesCommissionVOs;
+    }
+
+    public void setSalesCommissionVOs(List<SalesCommissionVO> salesCommissionVOs) {
+        this.salesCommissionVOs = salesCommissionVOs;
+    }
+
+    @Override
+    public String toString() {
+        return "SalesCommissionListVO{" +
+                "salesCommissionVOs=" + salesCommissionVOs +
+                '}';
+    }
+}
