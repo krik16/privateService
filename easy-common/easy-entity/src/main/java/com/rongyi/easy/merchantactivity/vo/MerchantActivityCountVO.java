76d591464872d4bd5de1b55245d9ce732c244ba8
lijing <lijing@rongyi.com>
2016-03-16 15:42:46 +0800

    摩店详情

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/merchantactivity/vo/MerchantActivityCountVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/merchantactivity/vo/MerchantActivityCountVO.java
@@ -1,5 +1,7 @@
 package com.rongyi.easy.merchantactivity.vo;
 
+import org.apache.commons.lang.builder.ToStringBuilder;
+
 import java.io.Serializable;
 
 /**
@@ -16,14 +18,92 @@ public class MerchantActivityCountVO implements Serializable {
     private Integer nowAddActivityCount;
     /**申请撤销中的活动数量*/
     private Integer revokingActivityCount;
-    /***/
+    /**自营活动(现在等于 所有活动数量)*/
     private Integer supportActivityCount;
-    /***/
+    /**参与活动*/
     private Integer joinActivityCount;
-    /***/
+    /**进行中活动*/
     private Integer conductingActivityCount;
-    /***/
+    /**结束活动*/
     private Integer endActivityCount;
-    /***/
+    /**下线活动*/
     private Integer OfflineActivityCount;
+
+    public Integer getAllActivityCount() {
+        return allActivityCount;
+    }
+
+    public void setAllActivityCount(Integer allActivityCount) {
+        this.allActivityCount = allActivityCount;
+    }
+
+    public Integer getNowAddActivityCount() {
+        return nowAddActivityCount;
+    }
+
+    public void setNowAddActivityCount(Integer nowAddActivityCount) {
+        this.nowAddActivityCount = nowAddActivityCount;
+    }
+
+    public Integer getRevokingActivityCount() {
+        return revokingActivityCount;
+    }
+
+    public void setRevokingActivityCount(Integer revokingActivityCount) {
+        this.revokingActivityCount = revokingActivityCount;
+    }
+
+    public Integer getSupportActivityCount() {
+        return supportActivityCount;
+    }
+
+    public void setSupportActivityCount(Integer supportActivityCount) {
+        this.supportActivityCount = supportActivityCount;
+    }
+
+    public Integer getJoinActivityCount() {
+        return joinActivityCount;
+    }
+
+    public void setJoinActivityCount(Integer joinActivityCount) {
+        this.joinActivityCount = joinActivityCount;
+    }
+
+    public Integer getConductingActivityCount() {
+        return conductingActivityCount;
+    }
+
+    public void setConductingActivityCount(Integer conductingActivityCount) {
+        this.conductingActivityCount = conductingActivityCount;
+    }
+
+    public Integer getEndActivityCount() {
+        return endActivityCount;
+    }
+
+    public void setEndActivityCount(Integer endActivityCount) {
+        this.endActivityCount = endActivityCount;
+    }
+
+    public Integer getOfflineActivityCount() {
+        return OfflineActivityCount;
+    }
+
+    public void setOfflineActivityCount(Integer offlineActivityCount) {
+        OfflineActivityCount = offlineActivityCount;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("allActivityCount", allActivityCount)
+                .append("nowAddActivityCount", nowAddActivityCount)
+                .append("revokingActivityCount", revokingActivityCount)
+                .append("supportActivityCount", supportActivityCount)
+                .append("joinActivityCount", joinActivityCount)
+                .append("conductingActivityCount", conductingActivityCount)
+                .append("endActivityCount", endActivityCount)
+                .append("OfflineActivityCount", OfflineActivityCount)
+                .toString();
+    }
 }
