abdfd2ce6377cc22d3bf2501960bd9f797c33037
zhangzhengping <zhangzhengping@rongyi.com>
2016-03-17 14:16:02 +0800

    增加参数

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/param/MyOrderParam.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/param/MyOrderParam.java
@@ -16,6 +16,8 @@ public class MyOrderParam implements Serializable{
 	private String currentPage;
 	
 	private String pageSize;
+	
+	private String memberId;//微信用户id
 
 	public String getJsessionid() {
 		return jsessionid;
@@ -48,13 +50,20 @@ public class MyOrderParam implements Serializable{
 	public void setStatus(String status) {
 		this.status = status;
 	}
+	
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
 
 	@Override
 	public String toString() {
-		return "MyOrderParam{" +
-				"status='" + status + '\'' +
-				", currentPage='" + currentPage + '\'' +
-				", pageSize='" + pageSize + '\'' +
-				'}';
+		return "MyOrderParam [jsessionid=" + jsessionid + ", status=" + status
+				+ ", currentPage=" + currentPage + ", pageSize=" + pageSize
+				+ ", memberId=" + memberId + "]";
 	}
+	
 }
