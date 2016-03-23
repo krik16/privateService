16b8fb98de5979407485a57685ed9dce8b207d34
wangzehui <wangzehui@rongyi.com>
2016-03-23 17:50:20 +0800

    闪购特卖集合修改 不一个一个修改

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/solr/CouponService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/solr/CouponService.java
@@ -1,6 +1,9 @@
 package com.rongyi.rss.solr;
 
+import com.rongyi.easy.mcmc.param.SaleParamSolr;
+
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by xuying on 2016/1/21.
@@ -10,4 +13,6 @@ public interface CouponService {
     public boolean cleanCouponSecKill(String secKillSign);
 
     public boolean updateCouponSecKill(String couponId,String secKillSign,Date activityStartTime,Date activityEndTime);
+
+    public boolean updateCouponSecKill(List<SaleParamSolr> coupons, String secKillSign);
 }
