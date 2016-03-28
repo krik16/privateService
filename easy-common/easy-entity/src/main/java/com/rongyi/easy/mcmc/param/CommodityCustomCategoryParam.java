203d1845e96303aa0acaac7c30d8722b32d47ea4
wangzehui <wangzehui@rongyi.com>
2016-03-28 16:17:58 +0800

    自定义类目

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/param/CommodityCustomCategoryParam.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/param/CommodityCustomCategoryParam.java
@@ -8,13 +8,17 @@
 */
 package com.rongyi.easy.mcmc.param;
 
-/** 
+import java.io.Serializable;
+
+/**
  * @ClassName: CommodityCategoryParam 
  * @Description: 查询商品自定义类目参数
  * @author wzh
  * @date 2015年9月23日 下午5:16:41
  */
-public class CommodityCustomCategoryParam {
+public class CommodityCustomCategoryParam implements Serializable{
+
+	//public static final String serialVersionUID="1L";
 	
 	private Integer id;//自定义分类id
 	
