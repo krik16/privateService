609e414fe929e7c2a8ae968394a562d68cb5e217
chenjun <chenjun@rongyi.com>
2016-03-28 11:15:47 +0800

    添加物流信息接口

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/msob/IExpressPushInfoService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/msob/IExpressPushInfoService.java
@@ -1,5 +1,17 @@
 package com.rongyi.rss.msob;
 
+import java.util.List;
+
+import com.rongyi.easy.rmmm.vo.ExpressDataVO;
+
 public interface IExpressPushInfoService {
+	
+	/**
+	 * 获取快递100推送的信息
+	 * @param logisticsBillId : 快递单号，expressOrderId 物流信息主键id
+	 * @return
+	 * @throws Exception
+	 */
+	public List<ExpressDataVO> getExpressDataVO(String logisticsBillId, String expressOrderId) throws Exception;
 
 }
