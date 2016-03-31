66557e64d726b3f4f1427c8f4ed228622d8f250b
wangzehui <wangzehui@rongyi.com>
2016-03-31 16:11:33 +0800

    修改toString方法

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/solr/param/ShopSearchParam.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/solr/param/ShopSearchParam.java
@@ -156,10 +156,25 @@ public class ShopSearchParam implements Serializable{
 
 	@Override
 	public String toString() {
-		return "ShopSearchParam [keyword=" + keyword + ", distance=" + distance + ", coord_x=" + coord_x + ", coord_y="
-				+ coord_y + ", cat_id=" + cat_id + ", custom_cat_id=" + custom_cat_id + ", city_name=" + city_name
-				+ ", sort=" + sort + ", from=" + from + ", size=" + size + ", brand_id=" + brand_id + ", zone_id="
-				+ zone_id + ", shop_id=" + shop_id + ", commodityRequired=" + commodityRequired + ", mall_id=" + mall_id
-				+ ", poi_type=" + poi_type + "]";
+		return "ShopSearchParam{" +
+				"brand_id='" + brand_id + '\'' +
+				", keyword='" + keyword + '\'' +
+				", distance=" + distance +
+				", coord_x='" + coord_x + '\'' +
+				", coord_y='" + coord_y + '\'' +
+				", cat_id='" + cat_id + '\'' +
+				", custom_cat_id='" + custom_cat_id + '\'' +
+				", city_name='" + city_name + '\'' +
+				", city_id='" + city_id + '\'' +
+				", sort='" + sort + '\'' +
+				", from=" + from +
+				", size=" + size +
+				", zone_id='" + zone_id + '\'' +
+				", shop_id='" + shop_id + '\'' +
+				", commodityRequired=" + commodityRequired +
+				", mall_id='" + mall_id + '\'' +
+				", poi_type='" + poi_type + '\'' +
+				", keywordList=" + keywordList +
+				'}';
 	}
 }
