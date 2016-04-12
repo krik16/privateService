2dc3a95567b8812c01063fe8c3a1119bd300be7d
yuzhijian <yuxia2217@163.com>
2016-04-12 18:04:07 +0800

    提交短信相关

--- easy-common/easy-core/src/main/java/com/rongyi/core/common/third/malllife/MallLifeThirdConfig.java
+++ easy-common/easy-core/src/main/java/com/rongyi/core/common/third/malllife/MallLifeThirdConfig.java
@@ -50,8 +50,6 @@ public class MallLifeThirdConfig {
         public static final String CHANNEL="HGDSLHJ";//哈根达斯—老虎机活动
         public static final String TOKEN="ec037d3911591c55b84b8e0918890989";
     }
-
-
     /**
      * TO_B业务
      * 微信-翻盘购
@@ -59,6 +57,21 @@ public class MallLifeThirdConfig {
     public abstract class TOB_WECHAT_CHANNEL{
         public static final String CHANNEL="WECHATFPG";//微信-翻盘购
         public static final String TOKEN="we037d3911591c55b84b8e09188909yu";
+    }
+
+
+    /**
+     * 短信接口公钥
+     */
+    public abstract class TOB_SMS_RSACODER{
+        public static final String PUBLIC_KEY_STR = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCOIfpdq2D+IlGxjSWJNIdis8E1xRNxA343wvZOWFMHotO55d973OD8TJBY8YMdI1SLj7B6RanAAxECA54vqTe/h9S95C1HVU1UlZKqTKWltatAK17zEiwZAC99BGRYY+Bz5mWfETa27RlOMHNkblyVF3KxJbJ2XS7BgYMgqFxSxwIDAQAB";
+        public static final String PRIVATE_KEY_STR = "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAI4h+l2rYP4iUbGNJYk0h2KzwTXFE3EDfjfC9k5YUwei07nl33vc4PxMkFjxgx0jVIuPsHpFqcADEQIDni+pN7+H1L3kLUdVTVSVkqpMpaW1q0ArXvMSLBkAL30EZFhj4HPmZZ8RNrbtGU4wc2RuXJUXcrElsnZdLsGBgyCoXFLHAgMBAAECgYBnmuUBupctKJ8cKaMe7Kdzj0xbgZi2XLF4vCkG2uNLQanMbIAHre5iev0NmJ7WcpaCVO9zcaxXRuTy7ug0HcmAO8HikL2HzV1l2nYwoQagoEvr/rBG7Mc2Ov4Wk4tAScmARo7U99pPZfZDUUTjzQHRJJ/D057vZvQOdWt5fiNhQQJBAOrqFaSKbMjO/TrDeqnSQzmDDSULGXfnn+XQFvGc6s7nMPJIJWYmvCfFnzJyzkyBC7d3GFAbZ56eXwKrZUIqCm8CQQCa4++BomwOeEd+U770pc4CbIL2viNl7s1U0xjl58vIqml446VRNcuRs7/ByjMicvblPYH3YOVtwaKZoFP3SUkpAkA91d5akIFwHmQz6q7IMuu1INT98j0C/fpuAOd5NAfu7JZKwEjv6bRPd8qe+e9p3fIamgPRWlVdQGzcBAfEcdKLAkBrOaofSwqVBc0wddc20+WGpgoPuFcXBLcepriQx+O6OcakSquySZRwfEgeZtT5Xy7uSaNEVs7dKXRf967I/PHpAkBsv8SUC63K/gwiRfeJLtwZofXPa3kGzv/zfGdkZsBC4SN9M5VKi9UvJac1638VWoPsYfO9Nik5mhu0mYHhOej6";
+    }
+
+
+    public abstract class TOB_SMS_CHANNEL{
+        public static final String CHANNEL="RONGYISMS";//微信-翻盘购
+        public static final String TOKEN="";
     }
 
 
