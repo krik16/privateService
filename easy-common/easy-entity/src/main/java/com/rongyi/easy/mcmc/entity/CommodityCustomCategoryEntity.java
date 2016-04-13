58518ad94b242d88acc73b212e1d629f02cc38b2
wangzehui <wangzehui@rongyi.com>
2016-04-13 17:54:01 +0800

    注释

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/entity/CommodityCustomCategoryEntity.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/entity/CommodityCustomCategoryEntity.java
@@ -3,33 +3,36 @@ package com.rongyi.easy.mcmc.entity;
 import java.io.Serializable;
 import java.util.Date;
 
+/**
+ * 商品自定义分类
+ */
 public class CommodityCustomCategoryEntity implements Serializable{
 
     public static final Long serialVersionUID=1L;
 
     private Integer id;
 
-    private String name;
+    private String name;//自定义类目名
 
-    private Integer type;
+    private Integer type;//分类级别 当前只有一级 0
 
-    private Integer onwnerId;
+    private Integer onwnerId;//自定义类目隶属者（店铺 分公司）mysql id
 
-    private String onwerMid;
+    private String onwerMid;//自定义类目隶属者（店铺 分公司） mongoid
 
-    private Integer ownerType;
+    private Integer ownerType;//身份类型:0集团管理员、1商场管理员、2品牌管理员、3分公司、4店长、5导购、6买手 目前只有分公司和店长
 
-    private Date createAt;
+    private Date createAt;//创建时间
 
-    private Integer createBy;
+    private Integer createBy;//创建者
 
-    private Date updateAt;
+    private Date updateAt;//修改时间
 
-    private Integer updateBy;
+    private Integer updateBy;//修改者
 
-    private Integer status;
+    private Integer status;//0表示正常1表示删除
 
-    private Integer parentId;
+    private Integer parentId;//父类id
 
     public Integer getId() {
         return id;
