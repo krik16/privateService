14c36f90f328af5a2ea04c2e351a7eab53cd1a68
chenjun <chenjun@rongyi.com>
2016-04-14 18:26:43 +0800

    是否订阅成功字段

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/vo/ExpressPushVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/vo/ExpressPushVO.java
@@ -8,6 +8,16 @@ public class ExpressPushVO  implements Serializable{
 	private String state;//已签收，在途中
 	
 	private List<ExpressDataVO> expressList;
+	
+	private boolean ifSupportByKuaidi100 = false;// true快递100订阅成功 false没成功
+
+	public boolean isIfSupportByKuaidi100() {
+		return ifSupportByKuaidi100;
+	}
+
+	public void setIfSupportByKuaidi100(boolean ifSupportByKuaidi100) {
+		this.ifSupportByKuaidi100 = ifSupportByKuaidi100;
+	}
 
 	public String getState() {
 		return state;
@@ -28,7 +38,7 @@ public class ExpressPushVO  implements Serializable{
 	@Override
 	public String toString() {
 		return "ExpressPushVO [state=" + state + ", expressList=" + expressList
-				+ "]";
+				+ ", ifSupportByKuaidi100=" + ifSupportByKuaidi100 + "]";
 	}
 
 }
