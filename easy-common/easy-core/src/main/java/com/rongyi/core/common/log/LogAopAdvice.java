4555ef383ab7b8561418f05a07a6ac5ad5a0df74
yujisun <yujisun@rongyi.com>
2016-04-18 13:38:36 +0800

    日志id添加

--- easy-common/easy-core/src/main/java/com/rongyi/core/common/log/LogAopAdvice.java
+++ easy-common/easy-core/src/main/java/com/rongyi/core/common/log/LogAopAdvice.java
@@ -3,6 +3,8 @@ package com.rongyi.core.common.log;
 //import com.alibaba.dubbo.rpc.RpcContext;
 import com.alibaba.dubbo.rpc.RpcContext;
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 
@@ -18,13 +20,18 @@ import java.util.UUID;
 
 public class LogAopAdvice
 {
+    private Logger logger = LoggerFactory.getLogger(LogAopAdvice.class);
+
     public void logIdInit() {
         String logId = RpcContext.getContext().getAttachment("logid");
+        logger.info("日志aop RpcContext logid={}",logId);
         if(StringUtils.isBlank(logId)){
             logId = MDC.get("logid");
+            logger.info("日志aop MDC logid={}",logId);
         }
         if(StringUtils.isBlank(logId)){
             logId = UUID.randomUUID().toString().substring(1,16);
+            logger.info("日志aop 生成logid={}",logId);
             MDC.put("logid",logId);
         }
 
