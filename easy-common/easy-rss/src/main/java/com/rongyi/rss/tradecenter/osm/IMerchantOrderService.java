5fd0047214fd19d633a7cde460c0362a561db86e
yujisun <yujisun@rongyi.com>
2016-04-21 15:25:55 +0800

    增加 订单查询接口

---
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/tradecenter/osm/IMerchantOrderService.java
@@ -0,0 +1,19 @@
+package com.rongyi.rss.tradecenter.osm;
+
+import com.rongyi.easy.bsoms.entity.SessionUserInfo;
+import com.rongyi.easy.tradecenter.param.OsmOrderQueryParam;
+
+import java.util.Map;
+
+public interface IMerchantOrderService
+{
+	
+	/**
+	 * 订单列表查询
+	 *
+	 * @param param 查询参数
+	 * @param userInfo 查询用户
+	 */
+	public Map<String,Object> queryOsmOrder(OsmOrderQueryParam param, SessionUserInfo userInfo);
+
+}
