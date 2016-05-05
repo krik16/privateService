5eabda85d3893353b1f4f5d1331f2430363d56ee
xuying <xuying@rongyi.com>
2016-05-05 11:16:19 +0800

    来源店铺只显示已添加商品的店铺名称

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/mcmc/ICommodityService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/mcmc/ICommodityService.java
@@ -13,23 +13,23 @@ import com.rongyi.easy.mcmc.Commodity;
 
 
 public interface ICommodityService {
-	
+
 	/**
 	 * 商品列表查询
 	 * @param paramMap
 	 * @return
 	 */
 	public List<Commodity> searchCommodity(Map<String,Object> paramMap,int pageSize);
-	
+
 	/**
 	 * 商品数量查询
 	 * @param paramMap
 	 * @return
 	 */
 	public Long searchCommodityCount(Map<String,Object> paramMap);
-	
+
 	public boolean updateOrDeleteCommodity(List<String> ids,Integer type,String reason,Integer userId);
-	
+
 	public String insertCommodity(Commodity commodity);
 	/**
 	 * 根据商品id查询商品信息
@@ -37,17 +37,17 @@ public interface ICommodityService {
 	 * @return
 	 */
 	public Commodity findCommodityById(String commodityId);
-	
+
 	public void updateCommoditySpecList(Commodity commodity);
-	
+
 	public List<Commodity> insertCommodityList(List<Commodity> commodityList);
-	
+
 	public List<Commodity> findByCode(String code);
-	
+
 	public void  deleteBySystemNumber(String systemNumber);
-	
+
 	public void updateSystemNumber(List<ObjectId> commodityIds,String systemNumber);
-	
+
 	public List<Commodity> findBySystemNumer(String systemNumber);
 
 	public CommodityFullVO getFullCommodity(String commodityId);
@@ -55,4 +55,6 @@ public interface ICommodityService {
 	public List<CommodityFullVO> getFullCommodityList(List<String> commodityIds);
 
 	public List<Commodity> findAllCommodity();
+
+	public List<Commodity> findCommodityByshopId(String shopMid);
 }
