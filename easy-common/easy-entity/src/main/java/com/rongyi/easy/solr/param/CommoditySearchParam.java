7e8a11e583a0f4a68f6e381bac150e03f2277885
yaoyiwei <yaoyiwei@rongyi.com>
2016-05-11 18:23:47 +0800

    提交代码： 修改saleId和flashSaleId类型

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/solr/param/CommoditySearchParam.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/solr/param/CommoditySearchParam.java
@@ -63,9 +63,9 @@ public class CommoditySearchParam  extends MalllifeBaseParam implements Serializ
 	/** 直播id */
 	private String liveId;
 
-	private Integer saleId; // 特卖ID
+	private String saleId; // 特卖ID
 
-	private Integer flashSaleId; // 闪购ID
+	private String flashSaleId; // 闪购ID
 
 	/** 商品类型 (买手 1， 全部商品 0, 特卖场 2, 闪购 3，导购商品 4)*/   //old
 	// 商品类型1：所有商品 2：导购商品 3：买手商品 4：闪购 5：卖场  6,直播     new
@@ -252,20 +252,20 @@ public class CommoditySearchParam  extends MalllifeBaseParam implements Serializ
 	}
 
 	@NeedCheck(getFieldName = "saleId")
-	public Integer getSaleId() {
+	public String getSaleId() {
 		return saleId;
 	}
 
-	public void setSaleId(Integer saleId) {
+	public void setSaleId(String saleId) {
 		this.saleId = saleId;
 	}
 
 	@NeedCheck(getFieldName = "flashSaleId")
-	public Integer getFlashSaleId() {
+	public String getFlashSaleId() {
 		return flashSaleId;
 	}
 
-	public void setFlashSaleId(Integer flashSaleId) {
+	public void setFlashSaleId(String flashSaleId) {
 		this.flashSaleId = flashSaleId;
 	}
 
