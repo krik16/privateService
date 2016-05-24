275a31cb47c32ee9bd5889b29dd7e74add2f8c09
yujisun <yujisun@rongyi.com>
2016-05-24 17:06:20 +0800

    校验邀请码 接口定义

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/rule/ICouponCheckService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/rule/ICouponCheckService.java
@@ -2,13 +2,12 @@ package com.rongyi.rss.rule;
 
 /**
  * Created by yujisun on 2016/5/20.
- * 购物车订单
  */
 public interface ICouponCheckService
 {
 
     /**
-     * 分页条件查询购物车订单
+     * 校验奖品券码
      *
      * @param couponCode 券码
      * @param mallId 验券商场id
@@ -16,8 +15,16 @@ public interface ICouponCheckService
      * @param useMode 校验方式
      * @param source 校验来源（PA/APP）
      * @param sellerId 校验人id
-     * @throws Exception
      */
     boolean checkCoupon(String couponCode, String mallId, String shopId, Long useMode, String source, Integer sellerId);
 
+    /**
+     * 校验活动邀请码
+     *
+     * @param inviteCode 邀请码
+     * @param activityId 活动id
+     * @param terminal 活动终端 1app 2微商城 3标准微信 4互动屏
+     * @return 是否成功
+     */
+    boolean checkCoupon(String inviteCode, String activityId, Integer terminal);
 }
