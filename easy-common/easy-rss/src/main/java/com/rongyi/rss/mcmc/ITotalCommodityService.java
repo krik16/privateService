3e5d56f6c6130c96c02bc6b8367f17c480ff935b
xuying <xuying@rongyi.com>
2016-05-30 20:04:42 +0800

    过滤没有商品的分类

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/mcmc/ITotalCommodityService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/mcmc/ITotalCommodityService.java
@@ -11,37 +11,39 @@ import com.rongyi.easy.mcmc.TotalCommodity;
 
 
 public interface ITotalCommodityService {
-	
+
 	public TotalCommodity  findByTotalCommodityId(String id);
-	
+
 	public void updateTotalCommodity(TotalCommodity toalCommodity);
-	
+
 	public boolean updateOrDeleteCommodity(List<ObjectId> ids,Integer type,String reason,Integer userId);
-	
+
 	public String insertTotalCommodity(TotalCommodity toalCommodity);
-	
+
 	/**
 	 * 商品列表查询
 	 * @param paramMap
 	 * @return
 	 */
 	public List<TotalCommodity> searchCommodity(Map<String,Object> paramMap,int pageSize);
-	
+
 	/**
 	 * 商品数量查询
 	 * @param paramMap
 	 * @return
 	 */
 	public Long searchCommodityCount(Map<String,Object> paramMap);
-	
+
 	public void insertTotalCommodityList(List<TotalCommodity> toalCommodities);
-	
+
 	public List<TotalCommodity>  findByTotalCommodityCode(List<String> codes,String brandMid);
-	
+
 	public Long searchCommodityByCodeAndBrand(String code,String brandMid);
 
 	public Map<String ,Object> findListForWechat(Map<String, Object> params);
 
 	public String updateCommodityWeAndTeStatus(String id, String type, String terminalType, String weAndTeStatus);
+
+	public List<TotalCommodity>  findCommodityByCategoryId(ObjectId categoryId);
 
 }
