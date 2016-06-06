a31d67f34edbcad279ea12a0bc17110ab351f5ab
kejun <kejun@rongyi.com>
2016-06-06 10:08:58 +0800

    支付签名获取及退款申请接口

---
+++ easy-common/easy-core/src/main/java/com/rongyi/core/Exception/ParamNullException.java
@@ -0,0 +1,17 @@
+package com.rongyi.core.Exception;
+
+/**
+ * Created by kejun on 2015/11/25.
+ */
+public class ParamNullException extends TradeException {
+
+    public ParamNullException(String code, String message)
+    {
+        super(message);
+        this.code = code;
+    }
+
+    public ParamNullException(){
+       throw new TradeException("-1","参数为空");
+   }
+}
