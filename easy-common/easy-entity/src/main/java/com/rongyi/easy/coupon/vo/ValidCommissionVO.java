36b1ee86894ec1409f6c715f2d3ed70ab357c6c1
袁波 <yuanbo@rongyi.com>
2016-06-07 18:35:16 +0800

    系统消息 商品下架消息接口

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/coupon/vo/ValidCommissionVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/coupon/vo/ValidCommissionVO.java
@@ -6,156 +6,170 @@ import java.util.List;
 
 /**
  * 核销佣金接口（新需求）
- * 
+ *
  * @author Ventures
- * 
  */
 public class ValidCommissionVO implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	private Integer totalPage;
-	private Integer pageSize;
-	private Integer currpage;
-	private Integer number;
-	private BigDecimal totalAmount;
-	private List<ValidDate> myValidList;
+    private static final long serialVersionUID = 1L;
+    private Integer totalPage;
+    private Integer pageSize;
+    private Integer currpage;
+    private Integer number;
+    private BigDecimal totalAmount;
+    private List<ValidDate> myValidList;
 
-	public Integer getTotalPage() {
-		return totalPage;
-	}
+    public Integer getTotalPage() {
+        return totalPage;
+    }
 
-	public void setTotalPage(Integer totalPage) {
-		this.totalPage = totalPage;
-	}
+    public void setTotalPage(Integer totalPage) {
+        this.totalPage = totalPage;
+    }
 
-	public Integer getPageSize() {
-		return pageSize;
-	}
+    public Integer getPageSize() {
+        return pageSize;
+    }
 
-	public void setPageSize(Integer pageSize) {
-		this.pageSize = pageSize;
-	}
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
 
-	public Integer getCurrpage() {
-		return currpage;
-	}
+    public Integer getCurrpage() {
+        return currpage;
+    }
 
-	public void setCurrpage(Integer currpage) {
-		this.currpage = currpage;
-	}
+    public void setCurrpage(Integer currpage) {
+        this.currpage = currpage;
+    }
 
-	public Integer getNumber() {
-		return number;
-	}
+    public Integer getNumber() {
+        return number;
+    }
 
-	public void setNumber(Integer number) {
-		this.number = number;
-	}
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
 
-	public BigDecimal getTotalAmount() {
-		return totalAmount;
-	}
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
 
-	public void setTotalAmount(BigDecimal totalAmount) {
-		this.totalAmount = totalAmount;
-	}
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
 
-	public List<ValidDate> getMyValidList() {
-		return myValidList;
-	}
+    public List<ValidDate> getMyValidList() {
+        return myValidList;
+    }
 
-	public void setMyValidList(List<ValidDate> myValidList) {
-		this.myValidList = myValidList;
-	}
+    public void setMyValidList(List<ValidDate> myValidList) {
+        this.myValidList = myValidList;
+    }
 
-	public static class ValidDate implements Serializable {
-		private static final long serialVersionUID = 1L;
-		private String date;
-		private List<ValidCoupon> list;
+    public static class ValidDate implements Serializable {
+        private static final long serialVersionUID = 1L;
+        private String date;
+        private Integer count;
+        private List<ValidCoupon> list;
 
-		public String getDate() {
-			return date;
-		}
-		public void setDate(String date) {
-			this.date = date;
-		}
-		public List<ValidCoupon> getList() {
-			return list;
-		}
-		public void setList(List<ValidCoupon> list) {
-			this.list = list;
-		}
+        public String getDate() {
+            return date;
+        }
 
-		@Override
-		public String toString() {
-			return "ValidDate [date=" + date + ", list=" + list + "]";
-		}
+        public void setDate(String date) {
+            this.date = date;
+        }
 
-		public static class ValidCoupon implements Serializable {
+        public List<ValidCoupon> getList() {
+            return list;
+        }
 
-			private static final long serialVersionUID = 1L;
-			private String title;
-			private String couponCode;
-			private String phone;
-			private String time;
-			private BigDecimal currentPrice;
+        public void setList(List<ValidCoupon> list) {
+            this.list = list;
+        }
 
-			public String getTitle() {
-				return title;
-			}
+        public Integer getCount() {
+            return count;
+        }
 
-			public void setTitle(String title) {
-				this.title = title;
-			}
+        public void setCount(Integer count) {
+            this.count = count;
+        }
 
-			public String getCouponCode() {
-				return couponCode;
-			}
+        @Override
+        public String toString() {
+            final StringBuffer sb = new StringBuffer("ValidDate{");
+            sb.append("date='").append(date).append('\'');
+            sb.append(", count=").append(count);
+            sb.append(", list=").append(list);
+            sb.append('}');
+            return sb.toString();
+        }
 
-			public void setCouponCode(String couponCode) {
-				this.couponCode = couponCode;
-			}
+        public static class ValidCoupon implements Serializable {
 
-			public String getPhone() {
-				return phone;
-			}
+            private static final long serialVersionUID = 1L;
+            private String title;
+            private String couponCode;
+            private String phone;
+            private String time;
+            private BigDecimal currentPrice;
 
-			public void setPhone(String phone) {
-				this.phone = phone;
-			}
+            public String getTitle() {
+                return title;
+            }
 
-			public String getTime() {
-				return time;
-			}
+            public void setTitle(String title) {
+                this.title = title;
+            }
 
-			public void setTime(String time) {
-				this.time = time;
-			}
+            public String getCouponCode() {
+                return couponCode;
+            }
 
-			public BigDecimal getCurrentPrice() {
-				return currentPrice;
-			}
+            public void setCouponCode(String couponCode) {
+                this.couponCode = couponCode;
+            }
 
-			public void setCurrentPrice(BigDecimal currentPrice) {
-				this.currentPrice = currentPrice;
-			}
+            public String getPhone() {
+                return phone;
+            }
 
-			@Override
-			public String toString() {
-				return "ValidCoupon [title=" + title + ", couponCode="
-						+ couponCode + ", phone=" + phone + ", time=" + time
-						+ ", currentPrice=" + currentPrice + "]";
-			}
+            public void setPhone(String phone) {
+                this.phone = phone;
+            }
 
-		}
-	}
+            public String getTime() {
+                return time;
+            }
 
-	@Override
-	public String toString() {
-		return "ValidCommissionVO [totalPage=" + totalPage + ", pageSize="
-				+ pageSize + ", currpage=" + currpage + ", number=" + number
-				+ ", totalAmount=" + totalAmount + ", myValidList="
-				+ myValidList + "]";
-	}
+            public void setTime(String time) {
+                this.time = time;
+            }
+
+            public BigDecimal getCurrentPrice() {
+                return currentPrice;
+            }
+
+            public void setCurrentPrice(BigDecimal currentPrice) {
+                this.currentPrice = currentPrice;
+            }
+
+            @Override
+            public String toString() {
+                return "ValidCoupon [title=" + title + ", couponCode=" + couponCode + ", phone="
+                    + phone + ", time=" + time + ", currentPrice=" + currentPrice + "]";
+            }
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ValidCommissionVO [totalPage=" + totalPage + ", pageSize=" + pageSize
+            + ", currpage=" + currpage + ", number=" + number + ", totalAmount=" + totalAmount
+            + ", myValidList=" + myValidList + "]";
+    }
 
 }
