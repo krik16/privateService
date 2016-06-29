d66344b4f1ea14e41fcb4348dce38b26e8129316
shaozhou <shaozhou@.rongyi.com>
2016-06-29 15:43:39 +0800

    新增实体

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/code/HannelsSaveParam.java
@@ -0,0 +1,16 @@
+package com.rongyi.easy.code;
+
+import java.io.Serializable;
+
+/**
+ * Created by shaozhou on 2016/6/29.
+ */
+public class HannelsSaveParam implements Serializable {
+    private String id;
+    private String type;
+    private String name;
+    private String couponIds;
+    private String validStartAt;
+    private String validEndAt;
+    private String channelId;
+}
