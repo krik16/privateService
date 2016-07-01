483a3039bdb28dab55dfb4006c369ef72e6412b0
zhangzhengping <zhangzhengping@rongyi.com>
2016-07-01 13:08:32 +0800

    店铺分享详情页

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/malllife/vo/h5/MallDetailVO.java
@@ -0,0 +1,16 @@
+package com.rongyi.easy.malllife.vo.h5;
+
+import java.io.Serializable;
+/**
+ * 商场分享详情
+ * @author user
+ *
+ */
+public class MallDetailVO implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
