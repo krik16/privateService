3a0732e0cadf6ba2c870482fcc1cca191e72afbc
shaozhou <shaozhou@.rongyi.com>
2016-07-04 20:07:37 +0800

    新增审核失败或作废日志

---
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/code/HannelsCheckLogService.java
@@ -0,0 +1,10 @@
+package com.rongyi.rss.code;
+
+import com.rongyi.easy.code.HannelsCheckLogParam;
+
+/**
+ * Created by shaozhou on 2016/7/4.
+ */
+public interface HannelsCheckLogService {
+    int insertLog(HannelsCheckLogParam param);
+}
