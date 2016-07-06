4f7475347430bc6f6a7eab5092790e77ba1a13d2
kejun <kejun@rongyi.com>
2016-07-06 11:10:11 +0800

    增加dubbo提供者

--- easy-trade/easy-tms/src/main/java/com/rongyi/tms/service/impl/CommissionOverLimitCheckTrigger.java
+++ easy-trade/easy-tms/src/main/java/com/rongyi/tms/service/impl/CommissionOverLimitCheckTrigger.java
@@ -3,13 +3,11 @@ package com.rongyi.tms.service.impl;
 import com.rongyi.rss.service.trigger.ICommissionOverLimitCheckTrigger;
 import com.rongyi.tms.service.SalesCommissionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 
 /**
  * kejun
  * 2016/6/30 18:01
  **/
-@Repository
 public class CommissionOverLimitCheckTrigger implements ICommissionOverLimitCheckTrigger{
 
     @Autowired
