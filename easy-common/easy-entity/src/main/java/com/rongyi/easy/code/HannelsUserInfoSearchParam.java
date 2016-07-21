9d3a08d24cf12525ee5276570544d4d127c5ee69
shaozhou <shaozhou@.rongyi.com>
2016-07-22 00:15:18 +0800

    hannels_user code commit2 shaozhou

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/code/HannelsUserInfoSearchParam.java
@@ -0,0 +1,105 @@
+package com.rongyi.easy.code;
+
+import java.io.Serializable;
+
+/**
+ * Created by shaozhou on 2016/7/21.
+ */
+public class HannelsUserInfoSearchParam implements Serializable {
+    private String userAccount;//账号
+    private String userNickName;//用户昵称
+    private String userPhone;//用户手机号码
+    private Integer identity;//身份 -1 全部 4:店长；5：导购
+    private String mallId;//商场id
+    private String shopId;//店铺id
+    private int currentPage;//当前页（从1开始）
+    private int pageSize;//每页显示数量（默认10）
+    private int startIndex = 0;
+
+    public String getUserAccount() {
+        return userAccount;
+    }
+
+    public void setUserAccount(String userAccount) {
+        this.userAccount = userAccount;
+    }
+
+    public String getUserNickName() {
+        return userNickName;
+    }
+
+    public void setUserNickName(String userNickName) {
+        this.userNickName = userNickName;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public Integer getIdentity() {
+        return identity;
+    }
+
+    public void setIdentity(Integer identity) {
+        this.identity = identity;
+    }
+
+    public String getMallId() {
+        return mallId;
+    }
+
+    public void setMallId(String mallId) {
+        this.mallId = mallId;
+    }
+
+    public String getShopId() {
+        return shopId;
+    }
+
+    public void setShopId(String shopId) {
+        this.shopId = shopId;
+    }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getStartIndex() {
+        return startIndex;
+    }
+
+    public void setStartIndex(int startIndex) {
+        this.startIndex = startIndex;
+    }
+
+    @Override
+    public String toString() {
+        return "HannelsUserInfoSearchParam{" +
+                "userAccount='" + userAccount + '\'' +
+                ", userNickName='" + userNickName + '\'' +
+                ", userPhone='" + userPhone + '\'' +
+                ", identity=" + identity +
+                ", mallId='" + mallId + '\'' +
+                ", shopId='" + shopId + '\'' +
+                ", currentPage=" + currentPage +
+                ", pageSize=" + pageSize +
+                ", startIndex=" + startIndex +
+                '}';
+    }
+}
