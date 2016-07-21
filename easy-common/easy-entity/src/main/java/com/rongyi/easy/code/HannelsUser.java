b2894c83582dfcd904f13bf912b4d8c8ac2c5ea1
袁波 <yuanbo@rongyi.com>
2016-07-21 18:01:26 +0800

    update

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/code/HannelsUser.java
@@ -0,0 +1,63 @@
+package com.rongyi.easy.code;
+
+public class HannelsUser {
+    private Integer id;
+
+    private Integer hannelsId;
+
+    private Integer userId;
+
+    private String userAccount;
+
+    private String userPhone;
+
+    private Integer identity;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getHannelsId() {
+        return hannelsId;
+    }
+
+    public void setHannelsId(Integer hannelsId) {
+        this.hannelsId = hannelsId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserAccount() {
+        return userAccount;
+    }
+
+    public void setUserAccount(String userAccount) {
+        this.userAccount = userAccount;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public Integer getIdentity() {
+        return identity;
+    }
+
+    public void setIdentity(Integer identity) {
+        this.identity = identity;
+    }
+}
