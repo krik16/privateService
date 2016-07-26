792170f329809212825ce483c31451fae8772c2b
zhangzhengping <zhangzhengping@rongyi.com>
2016-07-26 17:49:46 +0800

    活动专场

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/malllife/param/specialActivity/SpecialActivityParam.java
@@ -0,0 +1,19 @@
+package com.rongyi.easy.malllife.param.specialActivity;
+
+import java.io.Serializable;
+
+import com.rongyi.easy.malllife.param.MalllifeBaseParam;
+
+/**
+ * 专场活动参数
+ * @author user
+ *
+ */
+public class SpecialActivityParam extends MalllifeBaseParam implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
