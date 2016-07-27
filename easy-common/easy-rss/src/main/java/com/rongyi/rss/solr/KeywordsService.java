1aabdfe863a8443e2e52140aacdefcc9da4eb636
wangjianhua <wangjianhua@rongyi.com>
2016-07-27 18:11:22 +0800

    1。添加接口，实体

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/solr/KeywordsService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/solr/KeywordsService.java
@@ -1,12 +1,12 @@
-/** 
-* @Title: KeywordsService.java 
-* @Package com.rongyi.rss.solr 
-* @Description: 用户关键词联想
-* @author 郑亦梁  zhengyiliang@rongyi.com
-* @date 2015年10月14日 下午1:36:02 
-* @version V1.0   
-* Copyright (C),上海容易网电子商务有限公司
-*/
+/**
+ * @Title: KeywordsService.java
+ * @Package com.rongyi.rss.solr
+ * @Description: 用户关键词联想
+ * @author 郑亦梁  zhengyiliang@rongyi.com
+ * @date 2015年10月14日 下午1:36:02
+ * @version V1.0
+ * Copyright (C),上海容易网电子商务有限公司
+ */
 package com.rongyi.rss.solr;
 
 import com.rongyi.core.framework.exception.RYServiceException;
@@ -16,37 +16,25 @@ import com.rongyi.easy.rmmm.entity.KeywordSearchHistory;
 import java.util.List;
 
 /**
- * @author ZhengYl
+ * 用户关键词搜索
  *
+ * @author wangjh7
+ * @date 2016-07-22
  */
 public interface KeywordsService {
 
-	/**
-	 * 定时同步用户关键词历史记录
-	 * 
-	 * @author ZhengYl
-	 * @date 2015年10月26日 上午10:24:12 
-	 * @param scope 1:commodity 2:poi 3:activity
-	 * @param hours 同步前N小时的记录，默认为1
-	 */
-	public void SyncSearchKeywords(String scope, Integer hours);
-	
-	/**
-	 * 关键词联想主入口
-	 * 
-	 * @author ZhengYl
-	 * @date 2015年10月14日 下午1:38:45 
-	 * @param type   1:commodity 2:poi 3:activity
-	 * @param prefix 用户输入的前缀内容
-	 * @return 联想词列表
-	 */
-	public List<String> GetSuggestKeywords(String type, String prefix);
+    /**
+     * 保存用户关键词历史记录
+     *
+     * @param history
+     */
+    public boolean saveSearchKeywords(KeywordSearchHistory history) throws RYServiceException;
 
-	/**
-	 * 搜索关联联想词
-	 *
-	 * @param param
-	 * @return 返回LIST(com.rongyi.easy.solr.result.KeywordHistoryEntity)
-	 */
-	public List<KeywordSearchHistory> searchKeywordHistory(KeywordSearchParam param) throws RYServiceException;
+    /**
+     * 搜索关联联想词
+     *
+     * @param param
+     * @return 返回LIST(com.rongyi.easy.solr.result.KeywordHistoryEntity)
+     */
+    public List<KeywordSearchHistory> searchKeywordHistory(KeywordSearchParam param) throws RYServiceException;
 }
