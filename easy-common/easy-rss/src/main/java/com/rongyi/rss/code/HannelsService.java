dcca5e4a38f21dcff1c2d92d68b02f7239acccd8
袁波 <yuanbo@rongyi.com>
2016-07-29 17:24:05 +0800

    验码接口修改

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/code/HannelsService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/code/HannelsService.java
@@ -100,4 +100,12 @@ public interface HannelsService {
      * @return
      */
     List<Hannels> getHannelsByUserId(Integer userId,Integer hannelsType);
+
+    /**
+     * 根据商场和店铺id 和 hannelsType ,查询该用户关联的且当前有效的hannels
+     * @param assignId
+     * @param hannelsType
+     * @return
+     */
+    List<Hannels> getHannelsByAssignId(String assignId,Integer hannelsType);
 }
