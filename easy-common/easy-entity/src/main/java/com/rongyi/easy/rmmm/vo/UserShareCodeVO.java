3271d99ee2425795a6efdb9ea799adf6df0ea244
zhangzhengping <zhangzhengping@rongyi.com>
2016-08-08 18:51:35 +0800

    邀请分享

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/vo/UserShareCodeVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/vo/UserShareCodeVO.java
@@ -11,7 +11,7 @@ public class UserShareCodeVO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private String userLogo;//用户头像
 	private String inviteCode;//邀请码
-	private String inviteType;//被邀请人对象  1容易逛  2买手  3导购  4摩店全部
+	private List<String> inviteTypeList;//被邀请人对象  1容易逛  2买手  3导购  4摩店全部
 	public String getUserLogo() {
 		return userLogo;
 	}
@@ -24,13 +24,11 @@ public class UserShareCodeVO implements Serializable{
 	public void setInviteCode(String inviteCode) {
 		this.inviteCode = inviteCode;
 	}
-	public String getInviteType() {
-		return inviteType;
+	public List<String> getInviteTypeList() {
+		return inviteTypeList;
 	}
-	public void setInviteType(String inviteType) {
-		this.inviteType = inviteType;
+	public void setInviteTypeList(List<String> inviteTypeList) {
+		this.inviteTypeList = inviteTypeList;
 	}
-	
-	
 	
 }
