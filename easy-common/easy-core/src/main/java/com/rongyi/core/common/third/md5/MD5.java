aeb56053f0ab30a245155131fb8861016c1e7ba0
wangjianhua <wangjianhua@rongyi.com>
2016-08-10 10:04:38 +0800

    1。修改MD5加密默认编码

--- easy-common/easy-core/src/main/java/com/rongyi/core/common/third/md5/MD5.java
+++ easy-common/easy-core/src/main/java/com/rongyi/core/common/third/md5/MD5.java
@@ -8,6 +8,7 @@ public class MD5 {
     // 获得MD5摘要算法的 MessageDigest 对象
     private static MessageDigest _mdInst = null;
     private static char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+    private final static Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
 
     private static MessageDigest getMdInst() {
         if (_mdInst == null) {
@@ -21,26 +22,7 @@ public class MD5 {
     }
 
     public static String encode(String s) {
-        try {
-            byte[] btInput = s.getBytes();
-            // 使用指定的字节更新摘要
-            getMdInst().update(btInput);
-            // 获得密文
-            byte[] md = getMdInst().digest();
-            // 把密文转换成十六进制的字符串形式
-            int j = md.length;
-            char str[] = new char[j * 2];
-            int k = 0;
-            for (int i = 0; i < j; i++) {
-                byte byte0 = md[i];
-                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
-                str[k++] = hexDigits[byte0 & 0xf];
-            }
-            return new String(str);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+       return encode(s,DEFAULT_CHARSET);
     }
 
     /**
@@ -74,11 +56,5 @@ public class MD5 {
     }
 
     public static void main(String[] arg) {
-//        String s = "channeld=21,appKey=0000000100000153a76b48910000000d,orderId=080943506176163559,count=1,couponType=6004,timeStamp=1470731769977";
-//        System.out.println("默认："+encode(s).toUpperCase());
-//
-//        System.out.println("UTF-8："+encode(s,Charset.forName("UTF-8")).toUpperCase());
-//
-//        System.out.println("GBK："+encode(s,Charset.forName("gb2312")).toUpperCase());
     }
 }
