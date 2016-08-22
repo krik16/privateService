839a70c6c033eb914edafdc6591d5949175feef0
yangyang <yangyang@rongyi.com>
2016-08-22 14:47:36 +0800

    商品查询修改

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/Commodity.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/Commodity.java
@@ -379,9 +379,6 @@ public class Commodity implements  Serializable,Cloneable{
 		this.stockStatus = stockStatus;
 	}
 	public Double getDiscount() {
-		if (discount != null){
-			return discount;
-		}
 		try {
 			if(StringUtils.isNotBlank(this.currentPrice) && StringUtils.isNotBlank(this.originalPrice)) {
 				BigDecimal currentPrice = new BigDecimal(this.currentPrice);
