09487f3258e56e145cf1a3aac0e3fca2243cf552
shaozhou <shaozhou@.rongyi.com>
2016-08-25 20:12:44 +0800

    领取红包接口

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/msgcenter/OldPushNewParam.java
@@ -0,0 +1,53 @@
+package com.rongyi.easy.msgcenter;
+
+import java.io.Serializable;
+
+/**
+ * Created by shaozhou on 2016/8/25.
+ */
+public class OldPushNewParam implements Serializable {
+    private String inviteCode;//老用户邀请码
+    private Integer inviteType;//邀请类型 1注册 2注册并交易
+    private Integer userId;//被邀请用户id
+    private String userPhone;//被邀请用户手机号码
+
+    public String getInviteCode() {
+        return inviteCode;
+    }
+
+    public void setInviteCode(String inviteCode) {
+        this.inviteCode = inviteCode;
+    }
+
+    public Integer getInviteType() {
+        return inviteType;
+    }
+
+    public void setInviteType(Integer inviteType) {
+        this.inviteType = inviteType;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+    @Override
+    public String toString() {
+        return "OldPushNewParam{" +
+                "inviteCode='" + inviteCode + '\'' +
+                ", inviteType=" + inviteType +
+                ", userId=" + userId +
+                '}';
+    }
+}
