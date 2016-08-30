9933763bacfcad2c338a7012fed50656ed991864
袁波 <yuanbo@rongyi.com>
2016-08-30 15:59:19 +0800

    心愿单回复接口

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/msgcenter/SysMsgService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/msgcenter/SysMsgService.java
@@ -1,6 +1,7 @@
 package com.rongyi.rss.msgcenter;
 
 import com.rongyi.easy.msgcenter.MessageDTO;
+import com.rongyi.easy.msgcenter.UserMsgDTO;
 
 /**
  * Description:系统消息对外接口
@@ -10,5 +11,16 @@ import com.rongyi.easy.msgcenter.MessageDTO;
  * Project:easy-mobile
  */
 public interface SysMsgService {
+    /**
+     * 商品下架系统消息接口
+     * @param messageDTO
+     */
     public void createSysMsg(MessageDTO messageDTO);
+
+    /**
+     * 心愿单回复创建一条系统消息
+     * @param userMsgDTO
+     */
+    public void createUserSysMsg(UserMsgDTO userMsgDTO);
+
 }
