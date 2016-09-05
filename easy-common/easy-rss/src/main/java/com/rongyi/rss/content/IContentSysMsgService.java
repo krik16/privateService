d8017f50ab6f81d87d22e9d1d8be21bfcbe37931
袁波 <yuanbo@rongyi.com>
2016-09-05 19:17:31 +0800

    系统消息

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/content/IContentSysMsgService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/content/IContentSysMsgService.java
@@ -4,71 +4,94 @@ import java.util.List;
 import java.util.Map;
 
 import com.rongyi.easy.content.entity.ContentSysMsg;
+import com.rongyi.easy.rmmm.entity.ContentUserMsg;
 
 /**
  * 废弃
  */
 @Deprecated
 public interface IContentSysMsgService {
-	
-	/**
-	 * 根据id查系统消息信息
-	 * @param id
-	 * @return
-	 * @throws Exception
-	 */
-	public ContentSysMsg getMsgInfoById(Integer id) throws Exception;
-	/**
-	 * 获取所有的系统消息信息
-	 * @return
-	 * @throws Exception
-	 */
-	public List<ContentSysMsg> getAllMsgInfo(Map<String, Object> map) throws Exception;
-	/**
-	 * 获取所有系统消息列表数量
-	 * @return
-	 * @throws Exception
-	 */
-	public Integer getTotalRecord() throws Exception;
-	/**
-	 * 根据标题和状态模糊查询获取系统消息列表
-	 * @return
-	 * @throws Exception
-	 */
-	public List<ContentSysMsg> getMsgInfoByName(Map<String, Object> paraMap) throws Exception;
-	/**
-	 * 根据标题和状态模糊查询获取列表总数量
-	 * @return
-	 * @throws Exception
-	 */
-	public Integer getTotalRecordByName(Map<String, Object> paraMap) throws Exception;
-	/**
-	 * 根据标题和状态模糊查询获取系统消息列表
-	 * @return
-	 * @throws Exception
-	 */
-	public List<ContentSysMsg> getMsgInfoByNameAndStatus(Map<String, Object> paraMap) throws Exception;
-	/**
-	 * 根据标题和状态模糊查询获取列表总数量
-	 * @return
-	 * @throws Exception
-	 */
-	public Integer getTotalRecordByNameAndStatus(Map<String, Object> paraMap) throws Exception;
-	/**
-	 * 修改帮助中心信息
-	 * @throws Exception
-	 */
-	public void updateMsgInfoById(Map<String, Object> resultMap) throws Exception;
-	/**
-	 * 新建帮助中心信息
-	 * @param user
-	 * @throws Exception
-	 */
-	public void insertMsgInfo(ContentSysMsg entity) throws Exception;
-	/**
-	 * 删除该条信息（根据id将对应的信息变成禁用态）
-	 * @param id
-	 * @throws Exception
-	 */
-	public void deleteMsgInfo(Integer id) throws Exception;
+
+    /**
+     * 根据id查系统消息信息
+     *
+     * @param id
+     * @return
+     * @throws Exception
+     */
+    public ContentSysMsg getMsgInfoById(Integer id) throws Exception;
+
+    /**
+     * 获取所有的系统消息信息
+     *
+     * @return
+     * @throws Exception
+     */
+    public List<ContentSysMsg> getAllMsgInfo(Map<String, Object> map) throws Exception;
+
+    /**
+     * 获取所有系统消息列表数量
+     *
+     * @return
+     * @throws Exception
+     */
+    public Integer getTotalRecord() throws Exception;
+
+    /**
+     * 根据标题和状态模糊查询获取系统消息列表
+     *
+     * @return
+     * @throws Exception
+     */
+    public List<ContentSysMsg> getMsgInfoByName(Map<String, Object> paraMap) throws Exception;
+
+    /**
+     * 根据标题和状态模糊查询获取列表总数量
+     *
+     * @return
+     * @throws Exception
+     */
+    public Integer getTotalRecordByName(Map<String, Object> paraMap) throws Exception;
+
+    /**
+     * 根据标题和状态模糊查询获取系统消息列表
+     *
+     * @return
+     * @throws Exception
+     */
+    public List<ContentSysMsg> getMsgInfoByNameAndStatus(Map<String, Object> paraMap) throws Exception;
+
+    /**
+     * 根据标题和状态模糊查询获取列表总数量
+     *
+     * @return
+     * @throws Exception
+     */
+    public Integer getTotalRecordByNameAndStatus(Map<String, Object> paraMap) throws Exception;
+
+    /**
+     * 修改帮助中心信息
+     *
+     * @throws Exception
+     */
+    public void updateMsgInfoById(Map<String, Object> resultMap) throws Exception;
+
+    /**
+     * 新建帮助中心信息
+     *
+     * @param user
+     * @throws Exception
+     */
+    public void insertMsgInfo(ContentSysMsg entity) throws Exception;
+
+    /**
+     * 删除该条信息（根据id将对应的信息变成禁用态）
+     *
+     * @param id
+     * @throws Exception
+     */
+    public void deleteMsgInfo(Integer id) throws Exception;
+
+    public List<ContentUserMsg> findListByMsgId(Integer msgId);
+
 }
