b64cef712bc4072b6d559f9269f15a048cf1f8c1
袁波 <yuanbo@rongyi.com>
2016-09-18 16:43:24 +0800

    新的验码记录

---
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/code/ValidRecordService.java
@@ -0,0 +1,21 @@
+package com.rongyi.rss.code;
+
+import com.rongyi.easy.coupon.entity.Coupon;
+
+import java.util.List;
+
+/**
+ * Description:验码记录
+ * Author: 袁波
+ * DATE: 2016/9/18 16:03
+ * Package:com.rongyi.rss.code
+ * Project:easy-market
+ */
+public interface ValidRecordService {
+
+    public int createValidRecord(List<String> codes, Coupon coupon, Byte couponType, Integer validUserId, String validUserName, String mallId,String shopId) throws Exception;
+
+    public int createValidRecord(String code, String couponName, Byte couponType, Integer validUserId, String validUserName, String mallId,String shopId) throws Exception;
+
+    public int createValidRecord(String code, Coupon coupon, Byte couponType, Integer validUserId, String validUserName, String mallId,String shopId) throws Exception;
+}
