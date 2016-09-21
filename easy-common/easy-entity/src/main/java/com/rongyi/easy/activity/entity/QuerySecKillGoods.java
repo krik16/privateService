619085f7a80ca5a2555c5eaac59ff9476f8a8bb7
yangyang <yangyang@rongyi.com>
2016-09-21 11:45:15 +0800

    商品查询修改

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/activity/entity/QuerySecKillGoods.java
@@ -0,0 +1,80 @@
+package com.rongyi.easy.activity.entity;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+import java.io.Serializable;
+
+/**
+ * Created by yangyang on 2016/9/21.
+ */
+public class QuerySecKillGoods implements Serializable {
+
+    private Integer activityId;
+    private String goodsCode;//商品编码
+    private String name;//商品名称
+    private String shopName;//店铺名称
+    private Integer currentPage;
+    private Integer pageSize;
+
+
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("activityId", activityId)
+                .append("goodsCode", goodsCode)
+                .append("name", name)
+                .append("shopName", shopName)
+                .append("currentPage", currentPage)
+                .append("pageSize", pageSize)
+                .toString();
+    }
+
+    public Integer getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(Integer currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public Integer getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(Integer activityId) {
+        this.activityId = activityId;
+    }
+
+    public String getGoodsCode() {
+        return goodsCode;
+    }
+
+    public void setGoodsCode(String goodsCode) {
+        this.goodsCode = goodsCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+}
