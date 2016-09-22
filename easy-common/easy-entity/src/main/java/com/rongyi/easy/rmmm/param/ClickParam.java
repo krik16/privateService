91bf338a69622ab3d8d4bfd125fd7740d6237248
chenjun <chenjun@rongyi.com>
2016-09-22 16:27:56 +0800

    点赞

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/param/ClickParam.java
@@ -0,0 +1,60 @@
+package com.rongyi.easy.rmmm.param;
+
+import java.io.Serializable;
+
+import com.rongyi.easy.malllife.param.MalllifeBaseParam;
+
+public class ClickParam extends MalllifeBaseParam implements Serializable{
+	
+	/** 点赞活动id */
+    private String id;
+
+    /** 类型 */
+    private String type;
+
+    /** 0:点赞 1:取消点赞 */
+    private Integer status;
+
+    /** 设备号 */
+    private String devId;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getDevId() {
+		return devId;
+	}
+
+	public void setDevId(String devId) {
+		this.devId = devId;
+	}
+
+	@Override
+	public String toString() {
+		return "ClickParam [id=" + id + ", type=" + type + ", status=" + status
+				+ ", devId=" + devId 
+				+ "]";
+	}
+
+}
