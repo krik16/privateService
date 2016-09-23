0f694423c4bcda6e8e34109cef5712a2ac7bfc35
yujisun <yujisun@rongyi.com>
2016-09-23 14:55:26 +0800

    下单接口增加文章id和文章类型字段

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/osm/entity/OrderDetailModelEntity.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/osm/entity/OrderDetailModelEntity.java
@@ -38,7 +38,7 @@ public class OrderDetailModelEntity {
     private Integer articleType;
 
     //文章id
-    private Integer articleId;
+    private String articleId;
 
     /**
      * 主键id
@@ -208,11 +208,11 @@ public class OrderDetailModelEntity {
         this.articleType = articleType;
     }
 
-    public Integer getArticleId() {
+    public String getArticleId() {
         return articleId;
     }
 
-    public void setArticleId(Integer articleId) {
+    public void setArticleId(String articleId) {
         this.articleId = articleId;
     }
 }
