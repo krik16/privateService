bf7d5b476957e3d864ace04b30c2e79777905b4c
shaozhou <shaozhou@.rongyi.com>
2016-10-09 20:27:52 +0800

    一分钱红包

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/coupon/vo/YifenqianRedenvelopVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/coupon/vo/YifenqianRedenvelopVO.java
@@ -7,8 +7,8 @@ import java.util.List;
  * Created by shaozhou on 2016/10/9.
  */
 public class YifenqianRedenvelopVO implements Serializable {
-    private List<String> commodityIds;
-    private Integer status;
+    private List<String> commodityIds;//促销券关联的商品ids
+    private Integer status;//1 生成红包成功，2生成红包失败，3已经领取过一分钱红包
 
     public List<String> getCommodityIds() {
         return commodityIds;
