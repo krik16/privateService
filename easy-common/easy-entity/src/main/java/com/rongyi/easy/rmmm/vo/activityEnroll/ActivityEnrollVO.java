063a198ccbba29e939c20275b9f9df7d66ebac90
zhangzhengping <zhangzhengping@rongyi.com>
2016-10-09 16:05:13 +0800

    活动报名

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/vo/activityEnroll/ActivityEnrollVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/vo/activityEnroll/ActivityEnrollVO.java
@@ -17,7 +17,12 @@ public class ActivityEnrollVO implements Serializable{
 	private String description;//描述
 	private String startAt;//开始时间
 	private String endAt;//结束时间
+	private String createAt;//创建时间
 	private Integer status;//活动状态
+	/**
+	 * 活动类型 0.商品类活动，1卡券类活动，2.抽奖类活动 3.卡券及商品类活动，4.签到送积分，5，特卖7秒杀' 7'10月版本新增的秒杀'
+	 */
+	private Integer activityType;
 	public String getActivityId() {
 		return activityId;
 	}
@@ -48,17 +53,30 @@ public class ActivityEnrollVO implements Serializable{
 	public void setEndAt(String endAt) {
 		this.endAt = endAt;
 	}
+	public String getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(String createAt) {
+		this.createAt = createAt;
+	}
 	public Integer getStatus() {
 		return status;
 	}
 	public void setStatus(Integer status) {
 		this.status = status;
 	}
+	public Integer getActivityType() {
+		return activityType;
+	}
+	public void setActivityType(Integer activityType) {
+		this.activityType = activityType;
+	}
 	@Override
 	public String toString() {
 		return "ActivityEnrollVO [activityId=" + activityId + ", activityName="
 				+ activityName + ", description=" + description + ", startAt="
-				+ startAt + ", endAt=" + endAt + ", status=" + status + "]";
+				+ startAt + ", endAt=" + endAt + ", createAt=" + createAt
+				+ ", status=" + status + ", activityType=" + activityType + "]";
 	}
 	
 }
