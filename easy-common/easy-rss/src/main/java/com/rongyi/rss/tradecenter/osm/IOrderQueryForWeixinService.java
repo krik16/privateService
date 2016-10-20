823b1359438cda1228606d29bfe76547de9aa955
yujisun <yujisun@rongyi.com>
2016-10-20 11:25:47 +0800

    增加拼团订单统计类

---
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/tradecenter/osm/IOrderQueryForWeixinService.java
@@ -0,0 +1,28 @@
+/**
+ *
+ */
+package com.rongyi.rss.tradecenter.osm;
+
+import com.rongyi.easy.tradecenter.vo.PintuanOrderCountVO;
+
+import java.util.List;
+
+/**
+ * Copyright (C),上海容易网电子商务有限公司
+ * author yujisun
+ * Description ：
+ * time ：2016年10月20日
+ * History: 变更记录
+ * <author>           <time>             <version>        <desc>
+ */
+public interface IOrderQueryForWeixinService {
+
+	/**
+	 * 拼团统计
+	 * @param  activityId 活动id（必填）
+	 * @param  commodityId 商品id（空为全部）
+	 * @param  countType 统计维度（0全部 1当天 2昨天）
+	 * @throws Exception
+	 */
+	PintuanOrderCountVO pintuanOrderCount(Integer activityId,List<String> commodityId, Integer countType) throws Exception;
+}
