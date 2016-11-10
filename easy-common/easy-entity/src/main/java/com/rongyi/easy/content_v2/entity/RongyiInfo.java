3be8f0cc4e508dbc0926459dea9905c5123d032d
yangyang <yangyang@rongyi.com>
2016-11-10 15:32:19 +0800

    rongyiInfo service.dao

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/content_v2/entity/RongyiInfo.java
@@ -0,0 +1,178 @@
+package com.rongyi.easy.content_v2.entity;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by yangyang on 2016/11/10.
+ */
+public class RongyiInfo implements Serializable {
+    private Integer id;
+
+    private String title;
+
+    private String subTitle;
+
+    private String source;
+
+    private String outerLink;
+
+    private Date publishBeginAt;
+
+    private String coverUrl;
+
+    private String intro;
+
+    private Byte status;
+
+    private String createUser;
+
+    private Date createAt;
+
+    private String updateUser;
+
+    private Date updateAt;
+
+    private String infoContent;
+
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("id", id)
+                .append("subTitle", subTitle)
+                .append("source", source)
+                .append("title", title)
+                .append("outerLink", outerLink)
+                .append("publishBeginAt", publishBeginAt)
+                .append("coverUrl", coverUrl)
+                .append("intro", intro)
+                .append("status", status)
+                .append("createUser", createUser)
+                .append("status", status)
+                .append("createUser", createUser)
+                .append("createAt", createAt)
+                .append("updateUser", updateUser)
+                .append("updateAt", updateAt)
+                .append("infoContent", infoContent)
+                .toString();
+    }
+
+
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubTitle() {
+        return subTitle;
+    }
+
+    public void setSubTitle(String subTitle) {
+        this.subTitle = subTitle;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getOuterLink() {
+        return outerLink;
+    }
+
+    public void setOuterLink(String outerLink) {
+        this.outerLink = outerLink;
+    }
+
+    public Date getPublishBeginAt() {
+        return publishBeginAt;
+    }
+
+    public void setPublishBeginAt(Date publishBeginAt) {
+        this.publishBeginAt = publishBeginAt;
+    }
+
+    public String getCoverUrl() {
+        return coverUrl;
+    }
+
+    public void setCoverUrl(String coverUrl) {
+        this.coverUrl = coverUrl;
+    }
+
+    public String getIntro() {
+        return intro;
+    }
+
+    public void setIntro(String intro) {
+        this.intro = intro;
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public String getInfoContent() {
+        return infoContent;
+    }
+
+    public void setInfoContent(String infoContent) {
+        this.infoContent = infoContent;
+    }
+}
---
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/content/RongyiInfoService.java
@@ -0,0 +1,21 @@
+package com.rongyi.rss.content;
+
+import com.rongyi.easy.content_v2.entity.RongyiInfo;
+
+import java.util.List;
+
+/**
+ * Created by yangyang on 2016/11/10.
+ */
+public interface RongyiInfoService {
+
+    public boolean saveOrUpdateRongyiInfo(RongyiInfo rongyiInfo);
+
+    public RongyiInfo selectById(Integer id);
+
+    public boolean updateStatus(RongyiInfo rongyiInfo);
+
+    public List<RongyiInfo> getRongyiInfoByPage(Integer currentPage,Integer pageSize);
+
+    public Integer countRongyiInfoList();
+}
