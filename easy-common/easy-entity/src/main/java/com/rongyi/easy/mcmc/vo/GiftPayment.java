a3c420b6248a64af5181005e8d88e4ec0ae32a10
yaoyiwei <yaoyiwei@rongyi.com>
2016-11-14 10:39:14 +0800

    提交

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/vo/GiftPayment.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/vo/GiftPayment.java
@@ -8,14 +8,19 @@ import java.io.Serializable;
 import java.util.Date;
 
 /**
- * Created by yaoyiwei on 2016/11/10.
+ * 礼品支付规则类
+ *
+ * @author yaoyiwei
+ * @date 2016-11-11
+ * @version 1.0
+ *
  */
 @Entity(value="mcmc_gift_payment",noClassnameStored=true)
 public class GiftPayment implements Serializable, Cloneable {
     @Id
     private ObjectId id;
     // 礼品id
-    private ObjectId commodityId;
+    private String commodityId;
     // 支付方式（1.兑换 2.换购）
     private Integer type;
     // 用户等级
@@ -28,11 +33,94 @@ public class GiftPayment implements Serializable, Cloneable {
     private Integer isEffective;
     // 创建时间
     private Date createAt;
+    // mysql id
+    private String mysqlId;
 
 
+    public ObjectId getId() {
+        return id;
+    }
 
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
 
+    public String getCommodityId() {
+        return commodityId;
+    }
 
+    public void setCommodityId(String commodityId) {
+        this.commodityId = commodityId;
+    }
 
+    public Integer getType() {
+        return type;
+    }
 
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public Integer getPoint() {
+        return point;
+    }
+
+    public void setPoint(Integer point) {
+        this.point = point;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    public Integer getIsEffective() {
+        return isEffective;
+    }
+
+    public void setIsEffective(Integer isEffective) {
+        this.isEffective = isEffective;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public String getMysqlId() {
+        return mysqlId;
+    }
+
+    public void setMysqlId(String mysqlId) {
+        this.mysqlId = mysqlId;
+    }
+
+    @Override
+    public String toString() {
+        return "GiftPayment{" +
+                "id=" + id +
+                ", commodityId='" + commodityId + '\'' +
+                ", type=" + type +
+                ", level='" + level + '\'' +
+                ", point=" + point +
+                ", money=" + money +
+                ", isEffective=" + isEffective +
+                ", createAt=" + createAt +
+                ", mysqlId='" + mysqlId + '\'' +
+                '}';
+    }
 }
--- easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/vo/GiftTag.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/vo/GiftTag.java
@@ -9,19 +9,74 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * Created by yaoyiwei on 2016/11/10.
+ * 礼品标签类
+ *
+ * @author yaoyiwei
+ * @date 2016-11-10
+ * @version 1.0
+ *
  */
 @Entity(value="mcmc_gift_tag", noClassnameStored=true)
 public class GiftTag implements Serializable, Cloneable {
     @Id
     private ObjectId id;
-    // 所属ids
-    private List<Long> mappingIds;
+    // 所属id
+    private String mappingId;
     // 名称
     private String name;
     // 创建时间
     private Date createAt;
+    // mysql id
+    private String mysqlId;
 
+    public ObjectId getId() {
+        return id;
+    }
 
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
 
+    public String getMappingId() {
+        return mappingId;
+    }
+
+    public void setMappingId(String mappingId) {
+        this.mappingId = mappingId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public String getMysqlId() {
+        return mysqlId;
+    }
+
+    public void setMysqlId(String mysqlId) {
+        this.mysqlId = mysqlId;
+    }
+
+    @Override
+    public String toString() {
+        return "GiftTag{" +
+                "id=" + id +
+                ", mappingId=" + mappingId +
+                ", name='" + name + '\'' +
+                ", createAt=" + createAt +
+                ", mysqlId=" + mysqlId +
+                '}';
+    }
 }
