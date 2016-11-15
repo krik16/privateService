835a27d4077bd867adcbc3507e711534fb6fe7d9
yuzhijian <yuxia2217@163.com>
2016-11-15 13:26:35 +0800

    修改mongodb

--- easy-common/easy-core/src/main/java/com/rongyi/core/framework/morphia/bean/MongoFactoryBean.java
+++ easy-common/easy-core/src/main/java/com/rongyi/core/framework/morphia/bean/MongoFactoryBean.java
@@ -74,6 +74,17 @@ public class MongoFactoryBean extends AbstractFactoryBean<MongoClient> {
 //        this.logger.info("[Mongo Client] username: " + this.userName + ", password: " + this.password + ", userdb: " + this.database);
         MongoCredential credential = MongoCredential.createCredential(this.userName, this.database, this.password.toCharArray());
 
+        if(null!=serverList&&1==serverList.size()) {
+            if (null!=this.mongoOptions ) {
+                this.logger.info("[Mongo Client] " + this.showOptions(this.mongoOptions));
+                mongo = new MongoClient(serverList.get(0), Arrays.asList(credential), this.mongoOptions);
+            }else {
+                this.logger.info("[Mongo Client] mongoOptions is empty!");
+                mongo = new MongoClient(serverList.get(0), Arrays.asList(credential));
+            }
+
+        }else {
+
         for (ServerAddress serverAddress : serverList) {
             this.logger.info("[Mongo Client] server: " + serverAddress.getHost() + ":" + serverAddress.getPort());
         }
@@ -86,6 +97,7 @@ public class MongoFactoryBean extends AbstractFactoryBean<MongoClient> {
             this.logger.info("[Mongo Client] mongoOptions is empty!");
             mongo = new MongoClient(serverList, Arrays.asList(credential));
         }
+    }
         return mongo;
     }
 
