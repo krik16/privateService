3a0972e2e6a027ef3858dea21d0e287312e42593
pingkang <pingkang@rongyi.com>
2016-11-16 16:55:27 +0800

    修改行业详情返回json格式

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/ryoms/merchant/vo/IndustryVersionVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/ryoms/merchant/vo/IndustryVersionVO.java
@@ -2,6 +2,7 @@ package com.rongyi.easy.ryoms.merchant.vo;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 import com.rongyi.easy.bsoms.entity.BIndustryVersion;
 
@@ -9,15 +10,17 @@ public class IndustryVersionVO extends BIndustryVersion implements Serializable 
 
 	private static final long serialVersionUID = 8149365561835250043L;
 	
-	private List<AuthorityVO> authorityVOs;
+	private List<Map> auths;
 
-	public List<AuthorityVO> getAuthorityVOs() {
-		return authorityVOs;
+	public List<Map> getAuths() {
+		return auths;
 	}
 
-	public void setAuthorityVOs(List<AuthorityVO> authorityVOs) {
-		this.authorityVOs = authorityVOs;
+	public void setAuths(List<Map> auths) {
+		this.auths = auths;
 	}
 
+	
+	
 	
 }
