082406a969ac133976eccce5ead851f5b6b24de7
kejun <kejun@rongyi.com>
2016-11-22 14:11:52 +0800

    支付宝网页支付结果页面修改

--- easy-trade/easy-rpb/src/main/java/com/rongyi/rpb/constants/ConstantUtil.java
+++ easy-trade/easy-rpb/src/main/java/com/rongyi/rpb/constants/ConstantUtil.java
@@ -82,7 +82,7 @@ public interface ConstantUtil {
 
 	interface PayNetAdress {
 		// 支付宝网页支付主动通知商户网站里指定的页面http路径
-		String CALL_BACK_URL_WEB = "http://" + NOTIFY_URL.innernetAddress + "/v5/WebPageAlipay/call_back.htm?version=730&client=iOS";
+		String CALL_BACK_URL_WEB = "http://" + NOTIFY_URL.innernetAddress + "/v5/WebPageAlipay/call_back.htm";
 		String NOTIFY_URL_ZHIFUBAO_APP = "http://" + NOTIFY_URL.outernetAddress + "/v5/WebPageAlipay/app/notify_url.htm";
 		String NOTIFY_URL_ZHIFUBAO_WEB = "http://" + NOTIFY_URL.outernetAddress + "/v5/WebPageAlipay/notify_url.htm";
 		String MERCHANT_URL_WEB = "http://" + NOTIFY_URL.innernetAddress + "/v5/WebPageAlipay/merchant_url.htm";
