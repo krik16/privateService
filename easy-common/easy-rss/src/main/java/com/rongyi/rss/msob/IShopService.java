a7b50cbbac8a4d0bdd7fc1abdda2a9217db34517
sujuan <sujuan@rongyi.com>
2016-11-28 17:13:12 +0800

    增加通过商户ID查询店铺列表的接口

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/msob/IShopService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/msob/IShopService.java
@@ -234,5 +234,13 @@ public interface IShopService{
      * @throws Exception
      */
     public BrandShopListVO getBrandShopListByBrandId(BrandParam param) throws Exception;
+
+	/**
+	 * 通过上级商户ID查询店铺列表
+	 * @param type 0集团 1商场 4店铺
+	 * @param merId 商户ID
+	 * @return 店铺列表
+	 */
+	List<ShopInfoEntity> getShopInfoListBySuperiorId(Integer type ,Integer merId);
 }
 
