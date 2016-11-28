fa496109ad397ef9dfaa6d79666a31c5472ca6a6
袁波 <yuanbo@rongyi.com>
2016-11-28 10:44:08 +0800

    版本号

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/coupon/merchant/TradeCouponService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/coupon/merchant/TradeCouponService.java
@@ -1,16 +1,19 @@
 package com.rongyi.rss.coupon.merchant;
 
+import com.rongyi.easy.coupon.entity.Coupon;
 import com.rongyi.easy.coupon.vo.CouponGeneralVO;
 
 import java.util.List;
 
 /**
- * Description: 给交易使用的接口
- * Author: 袁波
+ * Description: 对外dubbo 接口
+ * Author: yb
  * DATE: 2016/6/28 16:19
  * Package:com.rongyi.rss.coupon.merchant
  * Project:easy-market
  */
 public interface TradeCouponService {
     public List<CouponGeneralVO> jugeGeneralByIds(List<String> couponIds);
+
+    public List<Coupon> findByShopId(String shopId);
 }
