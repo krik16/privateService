308ec2706ec61d661824d98f221ce1ffcabec019
zhangzhengping <zhangzhengping@rongyi.com>
2016-11-29 18:40:09 +0800

    submit

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/bdata/MerchantService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/bdata/MerchantService.java
@@ -6,5 +6,19 @@ import com.rongyi.easy.bdata.vo.MallShopVO;
 
 public interface MerchantService {
 
+	/**
+	 * 根据类型和id查询下属商场/店铺列表
+	 * @param type  (0) 集团，(1)商场，(4,5)店铺
+	 * @param id
+	 * @return
+	 */
 	public List<MallShopVO> getMallShopVO(Integer type,String id);
+	
+	/**
+	 * 根据类型和id查询下属商场/店铺列表
+	 * @param type  (0) 集团，(1)商场，(4,5)店铺
+	 * @param id
+	 * @return
+	 */
+	public List<String> getMallShopIds(Integer type,String id);
 }
