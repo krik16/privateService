958787c2469c769ab16ba42abd5c5fb0e0b8c6a5
wangjianhua <wangjianhua@rongyi.com>
2016-11-29 16:42:31 +0800

    修改收货人返回的对象信息

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/osm/entity/OsmOrderDetailForUserCenterEntity.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/osm/entity/OsmOrderDetailForUserCenterEntity.java
@@ -17,12 +17,8 @@ public class OsmOrderDetailForUserCenterEntity extends OsmOrderForUserCenterEnti
     private String commodityLogo;
     // 快递方式 1：自提 2：快递
     private int deliveryType;
-    // 收货人手机
-    private String receiverPhone;
-    // 收货人地址（包含省市区）
-    private String receiverAddress;
-    // 收货人姓名
-    private String receiverName;
+    // 快递信息
+    private OrderFormExtraEntity orderFormExtraEntity;
     // 对应事件
     private List<OrderEventEntity> eventList;
     // 券码
@@ -68,36 +64,20 @@ public class OsmOrderDetailForUserCenterEntity extends OsmOrderForUserCenterEnti
         this.eventList = eventList;
     }
 
-    public String getReceiverAddress() {
-        return receiverAddress;
-    }
-
-    public void setReceiverAddress(String receiverAddress) {
-        this.receiverAddress = receiverAddress;
-    }
-
-    public String getReceiverName() {
-        return receiverName;
-    }
-
-    public void setReceiverName(String receiverName) {
-        this.receiverName = receiverName;
-    }
-
-    public String getReceiverPhone() {
-        return receiverPhone;
-    }
-
-    public void setReceiverPhone(String receiverPhone) {
-        this.receiverPhone = receiverPhone;
-    }
-
     public List<String> getCommodityCodeList() {
         return commodityCodeList;
     }
 
     public void setCommodityCodeList(List<String> commodityCodeList) {
         this.commodityCodeList = commodityCodeList;
+    }
+
+    public OrderFormExtraEntity getOrderFormExtraEntity() {
+        return orderFormExtraEntity;
+    }
+
+    public void setOrderFormExtraEntity(OrderFormExtraEntity orderFormExtraEntity) {
+        this.orderFormExtraEntity = orderFormExtraEntity;
     }
 
     @Override
@@ -108,9 +88,7 @@ public class OsmOrderDetailForUserCenterEntity extends OsmOrderForUserCenterEnti
                 ", commoditySpecList=" + commoditySpecList +
                 ", commodityLogo='" + commodityLogo + '\'' +
                 ", deliveryType=" + deliveryType +
-                ", receiverPhone='" + receiverPhone + '\'' +
-                ", receiverAddress='" + receiverAddress + '\'' +
-                ", receiverName='" + receiverName + '\'' +
+                ", orderFormExtraEntity=" + orderFormExtraEntity +
                 ", eventList=" + eventList +
                 "} " + super.toString();
     }
