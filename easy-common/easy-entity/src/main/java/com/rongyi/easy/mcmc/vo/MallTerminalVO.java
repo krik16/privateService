11f9b79c32ee263da0f6e58d6a78026e32241bdb
xuying <xuying@rongyi.com>
2016-11-30 19:01:02 +0800

    发布商品终端返回list

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/vo/MallTerminalVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/vo/MallTerminalVO.java
@@ -1,19 +1,27 @@
 package com.rongyi.easy.mcmc.vo;
 
+import com.rongyi.easy.ryoms.entity.WechatInfoVo;
+
 import java.io.Serializable;
+import java.util.List;
 
 public class MallTerminalVO implements Serializable {
 	private static final long serialVersionUID = -7647318295364366168L;
-	
-	// 1 表示绑定 0 表示未绑定 
-	private Integer wechat;	 		// 是否绑定微信终端
+
+	// 1 表示绑定 0 表示未绑定
+	//private Integer wechat;	 		// 是否绑定微信终端
+
+	private List<WechatInfoVo> wechatInfoVoList;
 	private Integer mallTerminal;	// 商场终端
-	public Integer getWechat() {
-		return wechat;
+
+	public List<WechatInfoVo> getWechatInfoVoList() {
+		return wechatInfoVoList;
 	}
-	public void setWechat(Integer wechat) {
-		this.wechat = wechat;
+
+	public void setWechatInfoVoList(List<WechatInfoVo> wechatInfoVoList) {
+		this.wechatInfoVoList = wechatInfoVoList;
 	}
+
 	public Integer getMallTerminal() {
 		return mallTerminal;
 	}
@@ -22,7 +30,7 @@ public class MallTerminalVO implements Serializable {
 	}
 	@Override
 	public String toString() {
-		return "MallTerminalVO [wechat=" + wechat + ", mallTerminal=" + mallTerminal + "]";
+		return "MallTerminalVO [wechatInfoVoList=" + wechatInfoVoList + ", mallTerminal=" + mallTerminal + "]";
 	}
-	
+
 }
