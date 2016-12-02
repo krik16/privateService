4460797186b856d1ca939ef0788fc396572721eb
yangyang <yangyang@rongyi.com>
2016-12-02 14:47:30 +0800

    super pin tuan h5

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/activitymanage/vo/groupBuy/ControllerViewDownGroupVo.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/activitymanage/vo/groupBuy/ControllerViewDownGroupVo.java
@@ -15,7 +15,25 @@ public class ControllerViewDownGroupVo implements Serializable{
     private Date createAt;
     private Integer state;
     private String goodName;
+    private String userName;
+    private String userPhone;
 
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
 
     public Integer getGroupId() {
         return groupId;
