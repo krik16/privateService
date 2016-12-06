acc3cbc7db95153d686f309fbe82d327c946d0e4
zhangzhengping <zhangzhengping@rongyi.com>
2016-12-06 19:18:34 +0800

    submit

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/bdata/vo/MallShopVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/bdata/vo/MallShopVO.java
@@ -12,7 +12,10 @@ public class MallShopVO implements Serializable{
 	private String name;//名称
 	private Integer type;//类型    1:商场,2:店铺
 	private String address;//地址
-	
+	private String provinceName;//省名称
+	private String cityName;//市名称
+	private String districtName;//区名称
+	private String businessDistrictName;//
 	public String getAddress() {
 		return address;
 	}
@@ -37,11 +40,36 @@ public class MallShopVO implements Serializable{
 	public void setType(Integer type) {
 		this.type = type;
 	}
+	public String getProvinceName() {
+		return provinceName;
+	}
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	public String getDistrictName() {
+		return districtName;
+	}
+	public void setDistrictName(String districtName) {
+		this.districtName = districtName;
+	}
+	public String getBusinessDistrictName() {
+		return businessDistrictName;
+	}
+	public void setBusinessDistrictName(String businessDistrictName) {
+		this.businessDistrictName = businessDistrictName;
+	}
 	@Override
 	public String toString() {
 		return "MallShopVO [id=" + id + ", name=" + name + ", type=" + type
-				+ ", address=" + address + "]";
+				+ ", address=" + address + ", provinceName=" + provinceName
+				+ ", cityName=" + cityName + ", districtName=" + districtName
+				+ ", businessDistrictName=" + businessDistrictName + "]";
 	}
-	
 	
 }
