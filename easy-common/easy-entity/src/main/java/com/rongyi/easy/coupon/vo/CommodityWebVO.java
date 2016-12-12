e2bf9cdb2881d4cbe713d8554135d288eee20fa3
yangyang <yangyang@rongyi.com>
2016-12-12 16:26:22 +0800

    spec search  topcommodity list

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/coupon/vo/CommodityWebVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/coupon/vo/CommodityWebVO.java
@@ -26,16 +26,8 @@ public class CommodityWebVO implements Serializable{
     private String shopName;
     private String detailPic;
     private String discount;
-    private Integer isChoose;//是否可选
 
 
-    public Integer getIsChoose() {
-        return isChoose;
-    }
-
-    public void setIsChoose(Integer isChoose) {
-        this.isChoose = isChoose;
-    }
 
     public String getDiscount() {
         return discount;
@@ -139,7 +131,6 @@ public class CommodityWebVO implements Serializable{
                 .append("shopName", shopName)
                 .append("detailPic", detailPic)
                 .append("discount", discount)
-                .append("isChoose", isChoose)
                 .toString();
     }
 }
