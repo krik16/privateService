7f8645b0c54affd47c60d21f1c95ea40878f1661
yangyang <yangyang@rongyi.com>
2016-12-14 19:27:43 +0800

    spec search  topcommodity list

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/activitymanage/vo/groupBuy/GroupSearch.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/activitymanage/vo/groupBuy/GroupSearch.java
@@ -84,6 +84,26 @@ public class GroupSearch implements Serializable {
 
     private String goodName;
     private Integer groupRole;
+    private String userPhone;
+
+    private String userName;
+
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
     public Integer getGroupRole() {
         return groupRole;
