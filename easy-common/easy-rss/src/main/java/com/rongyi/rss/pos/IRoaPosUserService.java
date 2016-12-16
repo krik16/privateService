cbe7d9accff30e73eb614dbd9c89beb043407df4
sujuan <sujuan@rongyi.com>
2016-12-16 18:00:33 +0800

    用户停用 pos用户登出

---
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/pos/IRoaPosUserService.java
@@ -0,0 +1,11 @@
+package com.rongyi.rss.pos;
+
+import java.util.List;
+
+/**
+ *
+ * Created by sujuan on 2016/12/16.
+ */
+public interface IRoaPosUserService {
+    void exitLogin(List<Integer> userIds);
+}
