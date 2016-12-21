67f2a9c769d2f1eccc8ed7ca5c4a90543356821c
chenjun <chenjun@rongyi.com>
2016-12-21 14:13:09 +0800

    trade-push 去掉trade-core依赖

--- easy-common/easy-core/src/main/java/com/rongyi/core/constant/Constants.java
+++ easy-common/easy-core/src/main/java/com/rongyi/core/constant/Constants.java
@@ -938,4 +938,28 @@ public interface Constants {
     	String own_info = "own_info";
     	String extension_info = "extension_info";
     }
+
+    /**
+     * 分销推送的订单状态
+     * @author user
+     *
+     */
+    interface OrderPushStatus{
+        /** 支付成功 */
+        String TO_SHIPPED = "1";
+        /** 订单确认收货状态 */
+        String CONFIRM_RECEIPT = "2";
+        /** 订单取消和退款 */
+        String CANCEL = "3";
+    }
+
+    /**
+     * 推送类型
+     * @author user
+     *
+     */
+    interface OrderPushSource{
+        /** 商品分销推送 */
+        String COMMODITY_FENXIAO = "COMMODITY_FENXIAO";
+    }
 }
