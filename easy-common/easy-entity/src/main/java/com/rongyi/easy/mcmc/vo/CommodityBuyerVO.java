3f61225efa668920f24aaef18017ed075fea257a
yaoyiwei <yaoyiwei@rongyi.com>
2017-01-05 11:04:38 +0800

    微信查询接口修改

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/vo/CommodityBuyerVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/vo/CommodityBuyerVO.java
@@ -90,9 +90,17 @@ public class CommodityBuyerVO implements Serializable {
     private Long registerAt;//上架时间
 	private Long soldOutAt;//下架时间
     private Integer shelvesType;//1:立即上架，手动下架,2:定时上下架
+    private List<String> categoryNames;
 
+    public List<String> getCategoryNames() {
+        return categoryNames;
+    }
 
-	public Long getRegisterAt() {
+    public void setCategoryNames(List<String> categoryNames) {
+        this.categoryNames = categoryNames;
+    }
+
+    public Long getRegisterAt() {
 		return registerAt;
 	}
 
