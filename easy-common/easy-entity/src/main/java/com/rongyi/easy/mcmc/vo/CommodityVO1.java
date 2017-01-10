cb987c69b31afe6d7fc3308298bd905821a057b7
xuying <xuying@rongyi.com>
2017-01-10 15:01:05 +0800

    商品列表页面修改

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/vo/CommodityVO1.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/vo/CommodityVO1.java
@@ -33,6 +33,11 @@ public class CommodityVO1 implements  Serializable {
 	private String terminalTypeDesc = CommodityTerminalType.CommodityTerminalCommon.getName(terminalType);
 	private Date soldOutAt;
 	private String merchantId;
+	private Date registerAt;
+	private String commodityModelNo;
+	private String pic;
+	private String commoditycategoryNames;
+	private String createBy;
 
 	public String getId() {
 		return id;
@@ -150,5 +155,46 @@ public class CommodityVO1 implements  Serializable {
 
 	public void setMerchantId(String merchantId) {
 		this.merchantId = merchantId;
+	}
+
+
+	public Date getRegisterAt() {
+		return registerAt;
+	}
+
+	public void setRegisterAt(Date registerAt) {
+		this.registerAt = registerAt;
+	}
+
+	public String getCommodityModelNo() {
+		return commodityModelNo;
+	}
+
+	public void setCommodityModelNo(String commodityModelNo) {
+		this.commodityModelNo = commodityModelNo;
+	}
+
+	public String getPic() {
+		return pic;
+	}
+
+	public void setPic(String pic) {
+		this.pic = pic;
+	}
+
+	public String getCommoditycategoryNames() {
+		return commoditycategoryNames;
+	}
+
+	public void setCommoditycategoryNames(String commoditycategoryNames) {
+		this.commoditycategoryNames = commoditycategoryNames;
+	}
+
+	public String getCreateBy() {
+		return createBy;
+	}
+
+	public void setCreateBy(String createBy) {
+		this.createBy = createBy;
 	}
 }
