467f977051b4a93fc9bc7a6855bc64c5ba4578f5
yuzhijian <yuxia2217@163.com>
2017-01-11 16:58:14 +0800

    分享接口

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/malllife/vo/ShareTitleVo.java
@@ -0,0 +1,54 @@
+package com.rongyi.easy.malllife.vo;
+
+import java.io.Serializable;
+
+/*
+ * Description:  
+ * Author: yuzhijian 
+ * History: 变更记录
+ * <Author>     <time>    <version>    <desc>
+ * 俞志坚       2017/1/11    1.0        创建文件 
+ * Copyright (C): 上海容易网电子商务股份有限公司
+ */
+public class ShareTitleVo implements Serializable {
+
+
+    static final long serialVersionUID = 1L;
+
+   private  String objId;
+    private  String shareTile;
+    private  String  shareDesc;
+    private  String shareUrl;
+
+    public String getObjId() {
+        return objId;
+    }
+
+    public void setObjId(String objId) {
+        this.objId = objId;
+    }
+
+    public String getShareTile() {
+        return shareTile;
+    }
+
+    public void setShareTile(String shareTile) {
+        this.shareTile = shareTile;
+    }
+
+    public String getShareDesc() {
+        return shareDesc;
+    }
+
+    public void setShareDesc(String shareDesc) {
+        this.shareDesc = shareDesc;
+    }
+
+    public String getShareUrl() {
+        return shareUrl;
+    }
+
+    public void setShareUrl(String shareUrl) {
+        this.shareUrl = shareUrl;
+    }
+}
