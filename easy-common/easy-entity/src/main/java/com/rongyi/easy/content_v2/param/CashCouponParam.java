a386dbd02ee48f955ab5c0cf5999f5e24ed5af65
wuhui <wuhui@rongyi.com>
2017-01-18 18:18:26 +0800

    封装 请求

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/content_v2/param/CashCouponParam.java
@@ -0,0 +1,28 @@
+package com.rongyi.easy.content_v2.param;
+
+import com.rongyi.easy.rmmm.base.BaseParam;
+
+import java.io.Serializable;
+
+/**
+ * Created by WUH
+ * on 2017/1/18.
+ */
+public class CashCouponParam extends BaseParam  implements Serializable {
+    private Integer specialId;
+
+    public Integer getSpecialId() {
+        return specialId;
+    }
+
+    public void setSpecialId(Integer specialId) {
+        this.specialId = specialId;
+    }
+
+    @Override
+    public String toString() {
+        return "CashCouponParam{" +
+                "specialId=" + specialId +
+                '}';
+    }
+}
