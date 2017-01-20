22b1b0eab9fb2d22505f0b2fd0f1e0376a706fef
chenjun <chenjun@rongyi.com>
2017-01-20 11:35:01 +0800

    提现金额小于0的错误码

--- easy-common/easy-core/src/main/java/com/rongyi/core/enumerate/CodeEnum.java
+++ easy-common/easy-core/src/main/java/com/rongyi/core/enumerate/CodeEnum.java
@@ -267,6 +267,7 @@ public enum CodeEnum {
 
     ERROR_EXIST_PASSWORD("1300000", "虚拟账户未设置提现密码"),
     SUCCESS_EXIST_PASSWORD("1300001", "虚拟账户已设置提现密码"),
+    DRAW_AMOUNT_LOW("1300002", "提现金额不能小于0元"),
 	ERROR_GUIDE_ISDISABLED("1062002","此店铺休息中，无法为您服务"),
 
     ERROR_TAG_EXISTED("-111111", "标签名称已存在"),
