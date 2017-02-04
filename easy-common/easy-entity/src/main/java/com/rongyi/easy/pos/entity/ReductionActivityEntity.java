ff671f404bbc15d408f50549a606119e011018c8
wangjianhua <wangjianhua@rongyi.com>
2017-02-04 16:30:30 +0800

    满减订单查询实体

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/pos/entity/ReductionActivityEntity.java
@@ -0,0 +1,13 @@
+package com.rongyi.easy.pos.entity;
+
+import java.io.Serializable;
+
+/**
+ * 满减活动查询结果实体
+ *
+ * @author wangjh7
+ * @date 2017-02-04
+ */
+public class ReductionActivityEntity implements Serializable {
+
+}
