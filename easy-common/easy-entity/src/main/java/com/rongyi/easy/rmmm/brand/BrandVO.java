5706be0568a79895eb37e581378b4e49b1a80234
zhangzhengping <zhangzhengping@rongyi.com>
2017-02-07 10:31:48 +0800

    增加品牌logo

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/brand/BrandVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/brand/BrandVO.java
@@ -10,6 +10,7 @@ public class BrandVO implements Serializable{
 	private static final long serialVersionUID = -9083923891159691011L;
 	private String id;//品牌mongoId
 	private String brandName;//品牌名称
+	private String logo;//品牌logo
 	public String getId() {
 		return id;
 	}
@@ -22,9 +23,16 @@ public class BrandVO implements Serializable{
 	public void setBrandName(String brandName) {
 		this.brandName = brandName;
 	}
+	public String getLogo() {
+		return logo;
+	}
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
 	@Override
 	public String toString() {
-		return "BrandVO [id=" + id + ", brandName=" + brandName + "]";
+		return "BrandVO [id=" + id + ", brandName=" + brandName + ", logo="
+				+ logo + "]";
 	}
 	
 }
