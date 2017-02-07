43397a1f48ceaeeba49bf24ae7e27e10e55e4691
yangyang <yangyang@rongyi.com>
2017-02-07 11:39:44 +0800

    breakcodeGoods

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/coupon/entity/CouponBatchEntity.java
@@ -0,0 +1,57 @@
+package com.rongyi.easy.coupon.entity;
+
+import java.io.Serializable;
+
+public class CouponBatchEntity implements Serializable {
+    private Integer id;
+
+    private String couponId;
+
+    private String couponName;
+
+    private String batchNo;
+
+    private Byte isDeleted;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCouponId() {
+        return couponId;
+    }
+
+    public void setCouponId(String couponId) {
+        this.couponId = couponId;
+    }
+
+    public String getCouponName() {
+        return couponName;
+    }
+
+    public void setCouponName(String couponName) {
+        this.couponName = couponName;
+    }
+
+    public String getBatchNo() {
+        return batchNo;
+    }
+
+    public void setBatchNo(String batchNo) {
+        this.batchNo = batchNo;
+    }
+
+    public Byte getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Byte isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+}
---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/coupon/entity/CouponSendRecordEntity.java
@@ -0,0 +1,138 @@
+package com.rongyi.easy.coupon.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class CouponSendRecordEntity implements Serializable {
+    private Integer id;
+
+    private String batchNo;
+
+    private Integer type;
+
+    private String typeVal;
+
+    private Integer count;
+
+    private Integer acceptId;
+
+    private String phone;
+
+    private String email;
+
+    private Byte sendNum;
+
+    private String createUser;
+
+    private Date createAt;
+
+    private Date updateAt;
+
+    private Byte isDeleted;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBatchNo() {
+        return batchNo;
+    }
+
+    public void setBatchNo(String batchNo) {
+        this.batchNo = batchNo;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getTypeVal() {
+        return typeVal;
+    }
+
+    public void setTypeVal(String typeVal) {
+        this.typeVal = typeVal;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public Integer getAcceptId() {
+        return acceptId;
+    }
+
+    public void setAcceptId(Integer acceptId) {
+        this.acceptId = acceptId;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Byte getSendNum() {
+        return sendNum;
+    }
+
+    public void setSendNum(Byte sendNum) {
+        this.sendNum = sendNum;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public Byte getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Byte isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+}
