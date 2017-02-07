2b7e75ae61f93af43c756a8d6c68d48978f180bd
袁波 <yuanbo@rongyi.com>
2017-02-07 18:14:02 +0800

    Merge branch 'easy-common-bq-0210' of C:\rongyi -project\12版\easy-common with conflicts.

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/coupon/param/OutCouponDetailQueryParam.java
@@ -0,0 +1,11 @@
+package com.rongyi.easy.coupon.param;
+
+/**
+ * Description:
+ * Author: yb
+ * DATE: 2017/2/7 11:39
+ * Package:com.rongyi.easy.coupon.param
+ * Project:message-center
+ */
+public class OutCouponDetailQueryParam {
+}
