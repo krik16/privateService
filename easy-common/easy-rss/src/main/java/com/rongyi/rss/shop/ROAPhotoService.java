685cb986001e54e7a57c4d6b28567fe6e6893e1c
caohongjiang <caohongjiang@rongyi.com>
2017-02-15 17:33:32 +0800

    Signed-off-by: caohongjiang <caohongjiang@rongyi.com>

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/shop/ROAPhotoService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/shop/ROAPhotoService.java
@@ -6,5 +6,5 @@ public interface ROAPhotoService {
 	
 	public List<String> getShopPhotosByOwnerIdAndOwnerType(String ownerId,String ownerType);
 
-	public  void updateShopByType(String ownerId,String ownerType);
+	public  void delShopByType(String ownerId,String ownerType);
 }
