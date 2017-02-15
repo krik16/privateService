d41e154010873832a949daa72f9384e97f18292b
caohongjiang <caohongjiang@rongyi.com>
2017-02-15 15:47:06 +0800

    商家后台图片

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/shop/ROAPhotoService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/shop/ROAPhotoService.java
@@ -5,4 +5,6 @@ import java.util.List;
 public interface ROAPhotoService {
 	
 	public List<String> getShopPhotosByOwnerIdAndOwnerType(String ownerId,String ownerType);
+
+	public  void updateShopByType(String ownerId,String ownerType);
 }
