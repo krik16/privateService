22236447d5251250aa7577a30034cfd24386b619
yaoyiwei <yaoyiwei@rongyi.com>
2017-02-27 11:53:27 +0800

    提交

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/vo/SearchParamsForWechat.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/vo/SearchParamsForWechat.java
@@ -20,6 +20,33 @@ public class SearchParamsForWechat implements Serializable{
     private String keyword;
     private String displayStatus;
     private String category;
+    private String type;
+    private Date startTime;
+    private Date endTime;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
 
     public String getCategory() {
         return category;
