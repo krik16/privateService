b09112368620920d99ffdef415e110ab8d2d0e45
zhangzhengping <zhangzhengping@rongyi.com>
2017-03-03 11:52:10 +0800

    submit

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/vo/CommoditySpecColumnClassificationVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/mcmc/vo/CommoditySpecColumnClassificationVO.java
@@ -1,12 +1,18 @@
 package com.rongyi.easy.mcmc.vo;
 
+import java.io.Serializable;
 import java.util.List;
 
 
-public class CommoditySpecColumnClassificationVO{
+public class CommoditySpecColumnClassificationVO implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String id;
 	private String color;//颜色规格项对应的色系
+	private String colorType;//颜色类型：1 标准色系，2 特殊色系
 	private String name;//规格类名
 	private List<CommoditySpecColumnClassificationVO> children;
 	
@@ -22,6 +28,12 @@ public class CommoditySpecColumnClassificationVO{
 	public void setColor(String color) {
 		this.color = color;
 	}
+	public String getColorType() {
+		return colorType;
+	}
+	public void setColorType(String colorType) {
+		this.colorType = colorType;
+	}
 	public String getName() {
 		return name;
 	}
@@ -34,6 +46,11 @@ public class CommoditySpecColumnClassificationVO{
 	public void setChildren(List<CommoditySpecColumnClassificationVO> children) {
 		this.children = children;
 	}
-	
+	@Override
+	public String toString() {
+		return "CommoditySpecColumnClassificationVO [id=" + id + ", color="
+				+ color + ", colorType=" + colorType + ", name=" + name
+				+ ", children=" + children + "]";
+	}
 	
 }
