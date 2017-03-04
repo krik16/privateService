3e8be4da0493eba8a20cba2747eb01bf27c301f1
kejun <kejun@rongyi.com>
2017-03-04 10:58:50 +0800

    开放平台服务接口

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/rpb/IweBankService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/rpb/IweBankService.java
@@ -20,5 +20,25 @@ public interface IweBankService {
      */
     Map<String,Object> webankWechatPunchCardPay(RyMchVo ryMchVo,WwPunchCardPayVo wwPunchCardPayVo);
 
-    Map<String,Object> weBankWechatPayQuery(RyMchVo ryMchVo);
+    /**
+     * 微众微信支付查询
+     * @param ryMchVo 容易商户信息
+     * @param orderNo 订单号
+     * @param payType 支付方式(0:支付宝,1:微信)
+     * @param weBankMchNo 订单号
+     * @return map
+     */
+    Map<String,Object> weBankWechatPayQuery(RyMchVo ryMchVo,String orderNo,Integer payType,String weBankMchNo);
+
+
+    /**
+     * 微众银行微信退款
+     * @param ryMchVo 容易网商户信息
+     * @param orderNo 订单号
+     * @param refundAmount 退款金额
+     * @param refundReason 退款原因
+     * @param weBankMchNo 微众商户号
+     * @return map
+     */
+    Map<String,Object> webankWechatRefund(RyMchVo ryMchVo,String orderNo,Integer refundAmount,String refundReason,String weBankMchNo);
 }
