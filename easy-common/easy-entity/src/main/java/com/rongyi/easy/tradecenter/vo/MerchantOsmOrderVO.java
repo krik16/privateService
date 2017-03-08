239a6f8fb3ee3c3b24b9db4597ac091edc940f18
wangjianhua <wangjianhua@rongyi.com>
2017-03-08 16:18:48 +0800

    返回满减金额

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/tradecenter/vo/MerchantOsmOrderVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/tradecenter/vo/MerchantOsmOrderVO.java
@@ -53,6 +53,7 @@ public class MerchantOsmOrderVO implements Serializable{
     private String reveiveTime;//收货时间
     private BigDecimal merchantRedDiscount;//商家红包补贴金额
     private BigDecimal merchantRebateDiscount;//商家抵扣券补贴金额
+    private BigDecimal reductionFee; ///< 满减金额
 
     public Integer getOrderSource() {
         return orderSource;
@@ -367,6 +368,14 @@ public class MerchantOsmOrderVO implements Serializable{
 
     public void setWeixinAppId(String weixinAppId) {
         this.weixinAppId = weixinAppId;
+    }
+
+    public BigDecimal getReductionFee() {
+        return reductionFee;
+    }
+
+    public void setReductionFee(BigDecimal reductionFee) {
+        this.reductionFee = reductionFee;
     }
 
     @Override
