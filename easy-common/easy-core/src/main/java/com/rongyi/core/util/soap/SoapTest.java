1cafad048a9a16bde74d45e7e29e62a100e7ae33
yuzhijian <yuxia2217@163.com>
2017-03-13 18:51:22 +0800

    品类接口

--- easy-common/easy-core/src/main/java/com/rongyi/core/util/soap/SoapTest.java
+++ easy-common/easy-core/src/main/java/com/rongyi/core/util/soap/SoapTest.java
@@ -25,10 +25,43 @@ public class SoapTest {
             //获取店铺
             soapTest.getShopInfo();
 
+            //获取分类
+            soapTest.getCategory();
         } catch (Exception e) {
         }
 
     }
+
+    //下载分类信息
+    public void getCategory(){
+        try {
+            SoapTest soapTest = new SoapTest();
+            //请求体
+            String soap = "<soapenv:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:urn=\"urn:HsNavWebSrvIntf-IHsNavWebSrv\">\n" +
+                    "   <soapenv:Header/>\n" +
+                    "   <soapenv:Body>\n" +
+                    "      <urn:INavOperateIntf soapenv:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\">\n" +
+                    "         <psOperateTypeCode xsi:type=\"xsd:int\">2025</psOperateTypeCode>\n" +
+                    "         <psDataType xsi:type=\"xsd:string\">0</psDataType>\n" +
+                    "         <psXMLData xsi:type=\"xsd:string\">\n" +
+                    "         <IMPORTDATA>\n" +
+                    "        <OPERATION>1</OPERATION>\n" +
+                    "        </IMPORTDATA>\n" +
+                    "         </psXMLData>\n" +
+                    "      </urn:INavOperateIntf>\n" +
+                    "   </soapenv:Body>\n" +
+                    "</soapenv:Envelope>";
+            URL wsUrl = new URL("http://192.168.1.218:8080/HsNavWebSrv.dll/soap/IHsNavWebSrv?psOperateTypeCode=2025");
+            String result = SoapClientUtil.post(soap, wsUrl);
+            String ss = SoapXmlUtil.parseSoapMessage(result);
+
+            System.out.println("下载分类信息:====" + soapTest.decode(ss));
+        } catch (Exception e) {
+        }
+
+    }
+
+
 
     /**
      * 获取店信息
