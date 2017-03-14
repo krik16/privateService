6d534f077fad5298b3d53232d449dbca64c0c05b
yangyang <yangyang@rongyi.com>
2017-03-14 16:18:54 +0800

    海信-微信下单-用户会员姓名

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/osm/entity/PluData.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/osm/entity/PluData.java
@@ -1,18 +1,34 @@
 package com.rongyi.easy.osm.entity;
 
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
 import java.io.Serializable;
 
 /**
  * Created by yangyang on 2017/3/10.
  */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(propOrder = { "billNo","serialNo","pluCode","pluName","pfCount","pfpRice","barCode"})
 public class PluData implements Serializable {
 
+    @XmlElement(name = "BILLNO")
     private String billNo;
+    @XmlElement(name = "SERIALNO")
     private String serialNo;
+    @XmlElement(name = "PLUCODE")
     private String pluCode;
+    @XmlElement(name = "PLUNAME")
     private String pluName;
+    @XmlElement(name = "PFCOUNT")
     private String pfCount;
+    @XmlElement(name = "PFPRICE")
     private String pfpRice;
+    @XmlElement(name = "BARCODE")
     private String barCode;
 
     public String getBillNo() {
@@ -69,5 +85,9 @@ public class PluData implements Serializable {
 
     public void setPfpRice(String pfpRice) {
         this.pfpRice = pfpRice;
+    }
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.DEFAULT_STYLE);
     }
 }
