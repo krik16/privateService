0ad3e1a14f14c8f958361f2679e3a3f418304573
sujuan <sujuan@rongyi.com>
2017-03-15 14:38:07 +0800

    实体

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/rpb/IQueryService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/rpb/IQueryService.java
@@ -11,6 +11,12 @@ import java.util.List;
  **/
 public interface IQueryService {
 
+    /**
+     * 根据支付流水号及类型查询订单详情
+     * @param orderNoList 支付流水号集合
+     * @param tradeType 0支付  1退款
+     * @return 订单详情列表
+     */
     List<PaymentEntity> batchQueryByOrderNos(List<String> orderNoList,Integer tradeType);
 
    }
