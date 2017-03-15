cf99ff79d59a873636e1e508f8ad96a52b7118ce
kejun <kejun@rongyi.com>
2017-03-15 17:26:49 +0800

    福利订单微众微信支付商品标题不能为空修改

---
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/rpb/ICashPayService.java
@@ -0,0 +1,32 @@
+package com.rongyi.rss.rpb;
+
+import com.rongyi.easy.rpb.vo.CashPayVo;
+import com.rongyi.easy.rpb.vo.RyMchVo;
+
+import java.util.Map;
+
+/**
+ * 现金支付服务接口
+ * conan
+ * 2017/3/15 15:15
+ **/
+public interface ICashPayService {
+
+    /**
+     * 现金支付
+     * @param ryMchVo 容易商户
+     * @param cashPayVo 支付参数
+     * @param orderType 订单类型
+     * @return map
+     */
+    Map<String, Object> cashPay(RyMchVo ryMchVo, CashPayVo cashPayVo,Integer orderType);
+
+    /**
+     * 现金支付查询
+     * @param ryMchVo 容易商户
+     * @param orderNo 订单号
+     * @param payType 支付方式
+     * @return
+     */
+    Map<String, Object> cashPayQuery(RyMchVo ryMchVo, String orderNo,Integer payType);
+}
