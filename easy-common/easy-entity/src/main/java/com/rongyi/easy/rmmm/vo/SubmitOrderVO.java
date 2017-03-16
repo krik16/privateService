8603e799d2b6eadfb08b0fd00c0b8737eb020983
wangjianhua <wangjianhua@rongyi.com>
2017-03-16 14:47:09 +0800

    代码优化

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/vo/SubmitOrderVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/vo/SubmitOrderVO.java
@@ -5,10 +5,12 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public class SubmitOrderVO implements Serializable {
-
-	private List<String> parentOrderNumList;// 父订单号集合
-
-	private BigDecimal totalPrice;//实际需要支付价格
+	// 父订单号集合
+	private List<String> parentOrderNumList;
+	// 电子会员卡券+商品的自提券码
+	private List<String> couponCodeList ;
+	// 实际需要支付价格
+	private BigDecimal totalPrice;
 
 	public List<String> getCouponCodeList() {
 		return couponCodeList;
@@ -17,8 +19,6 @@ public class SubmitOrderVO implements Serializable {
 	public void setCouponCodeList(List<String> couponCodeList) {
 		this.couponCodeList = couponCodeList;
 	}
-
-	private List<String> couponCodeList ; // 电子会员卡券+商品的自提券码
 
 	public List<String> getParentOrderNumList() {
 		return parentOrderNumList;
