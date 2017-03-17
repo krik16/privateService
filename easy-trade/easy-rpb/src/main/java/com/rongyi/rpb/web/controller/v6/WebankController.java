3931f6cb94cd2a3969a012bedccc9e6220a704c3
sujuan <sujuan@rongyi.com>
2017-03-17 11:57:41 +0800

    对账单接收

--- easy-trade/easy-rpb/src/main/java/com/rongyi/rpb/web/controller/v6/WebankController.java
+++ easy-trade/easy-rpb/src/main/java/com/rongyi/rpb/web/controller/v6/WebankController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 import java.util.Map;
 
 /**
@@ -24,6 +25,33 @@ public class WebankController {
         try {
             Map map =  request.getParameterMap();
             LOGGER.info("微众对账单通知下载map:{}",map);
+
+            String sign = request.getParameter("sign");
+            String nonce = request.getParameter("nonce");
+            String timestamp = request.getParameter("timestamp");
+            LOGGER.info("sign:{},nonce:{},timestamp:{}",sign,nonce,timestamp);
+            String body = request.getParameter("body");
+            LOGGER.info("body:{}",body);
+            String Body = request.getParameter("Body");
+            LOGGER.info("Body:{}",Body);
+            String jsonString = request.getParameter("JsonString");
+            LOGGER.info("jsonString:{}", jsonString);
+
+
+
+            Enumeration e =  request.getAttributeNames();
+            LOGGER.info("e:{}",e);
+            Object sign1 = request.getAttribute("sign");
+            Object nonce1 = request.getAttribute("nonce");
+            Object timestamp1 = request.getAttribute("timestamp");
+            LOGGER.info("sign1:{},nonce1:{},timestamp1:{}",sign1,nonce1,timestamp1);
+            Object body1 = request.getAttribute("body");
+            LOGGER.info("body1:{}",body1);
+            Object Body1 = request.getAttribute("Body");
+            LOGGER.info("Body1:{}",Body1);
+            Object jsonString1 = request.getAttribute("JsonString");
+            LOGGER.info("jsonString1:{}", jsonString1);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
