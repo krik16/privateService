13217ae21a3adb8aa61a677f8806544f7d62ef7a
chenjun <chenjun@rongyi.com>
2017-03-22 11:27:50 +0800

    下单返回价格

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/vo/SubmitOrderVO.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/rmmm/vo/SubmitOrderVO.java
@@ -1,11 +1,14 @@
 package com.rongyi.easy.rmmm.vo;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.List;
 
 public class SubmitOrderVO implements Serializable {
 
 	private List<String> parentOrderNumList;// 父订单号集合
+	// 实际需要支付价格
+	private BigDecimal totalPrice;
 
 	public List<String> getCouponCodeList() {
 		return couponCodeList;
@@ -25,4 +28,11 @@ public class SubmitOrderVO implements Serializable {
 		this.parentOrderNumList = parentOrderNumList;
 	}
 
+	public BigDecimal getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(BigDecimal totalPrice) {
+		this.totalPrice = totalPrice;
+	}
 }
