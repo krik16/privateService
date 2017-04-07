27cd3410561bbd7f98af38283cc9cb197b583a68
gitwudi <490840770@qq.com>
2017-04-07 15:37:21 +0800

    Pos微众银行二期-常量类

--- easy-common/easy-core/src/main/java/com/rongyi/core/constant/PaymentOrderConst.java
+++ easy-common/easy-core/src/main/java/com/rongyi/core/constant/PaymentOrderConst.java
@@ -8,4 +8,11 @@ public class PaymentOrderConst {
 	public final static Integer PAYER_RECON_FLAG_TODO = 0;
 	public final static Integer PAYER_RECON_FLAG_DONE = 1;
 	public final static Integer PAYER_RECON_FLAG_ERROR = 2;
+
+	/**
+	 * 支付类型：1-付款，2-退款
+	 */
+	public final static Integer TRADE_TYPE_PAY = 0;
+	public final static Integer TRADE_TYPE_REFUND = 1;
+
 }
