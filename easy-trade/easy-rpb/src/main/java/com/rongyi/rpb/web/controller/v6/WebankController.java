ed6993cd9f2978ee47a68c19c493ba13912ab7c2
sujuan <sujuan@rongyi.com>
2017-04-07 10:52:46 +0800

    修改。。。

--- easy-trade/easy-rpb/src/main/java/com/rongyi/rpb/web/controller/v6/WebankController.java
+++ easy-trade/easy-rpb/src/main/java/com/rongyi/rpb/web/controller/v6/WebankController.java
@@ -56,39 +56,50 @@ public class WebankController {
 
     @RequestMapping("statementDown")
     public void statementDown(HttpServletRequest request) {
-        LOGGER.info("微众对账单通知下载");
+//        LOGGER.info("微众对账单通知下载");
+//        try {
+//            Map map =  request.getParameterMap();
+//            LOGGER.info("微众对账单通知下载map:{}",map);
+//
+//            String sign = request.getParameter("sign");
+//            String nonce = request.getParameter("nonce");
+//            String timestamp = request.getParameter("timestamp");
+//            LOGGER.info("sign:{},nonce:{},timestamp:{}",sign,nonce,timestamp);
+//            String body = request.getParameter("body");
+//            LOGGER.info("body:{}",body);
+//            String Body = request.getParameter("Body");
+//            LOGGER.info("Body:{}",Body);
+//            String jsonString = request.getParameter("JsonString");
+//            LOGGER.info("jsonString:{}", jsonString);
+//
+//
+//
+//            Enumeration e =  request.getAttributeNames();
+//            LOGGER.info("e:{}",e);
+//            Object sign1 = request.getAttribute("sign");
+//            Object nonce1 = request.getAttribute("nonce");
+//            Object timestamp1 = request.getAttribute("timestamp");
+//            LOGGER.info("sign1:{},nonce1:{},timestamp1:{}",sign1,nonce1,timestamp1);
+//            Object body1 = request.getAttribute("body");
+//            LOGGER.info("body1:{}",body1);
+//            Object Body1 = request.getAttribute("Body");
+//            LOGGER.info("Body1:{}",Body1);
+//            Object jsonString1 = request.getAttribute("JsonString");
+//            LOGGER.info("jsonString1:{}", jsonString1);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+
+        LOGGER.info("微众支付宝扫码支付异步通知start");
         try {
-            Map map =  request.getParameterMap();
-            LOGGER.info("微众对账单通知下载map:{}",map);
-
-            String sign = request.getParameter("sign");
-            String nonce = request.getParameter("nonce");
-            String timestamp = request.getParameter("timestamp");
-            LOGGER.info("sign:{},nonce:{},timestamp:{}",sign,nonce,timestamp);
-            String body = request.getParameter("body");
-            LOGGER.info("body:{}",body);
-            String Body = request.getParameter("Body");
-            LOGGER.info("Body:{}",Body);
-            String jsonString = request.getParameter("JsonString");
-            LOGGER.info("jsonString:{}", jsonString);
-
-
-
-            Enumeration e =  request.getAttributeNames();
-            LOGGER.info("e:{}",e);
-            Object sign1 = request.getAttribute("sign");
-            Object nonce1 = request.getAttribute("nonce");
-            Object timestamp1 = request.getAttribute("timestamp");
-            LOGGER.info("sign1:{},nonce1:{},timestamp1:{}",sign1,nonce1,timestamp1);
-            Object body1 = request.getAttribute("body");
-            LOGGER.info("body1:{}",body1);
-            Object Body1 = request.getAttribute("Body");
-            LOGGER.info("Body1:{}",Body1);
-            Object jsonString1 = request.getAttribute("JsonString");
-            LOGGER.info("jsonString1:{}", jsonString1);
-
+            Map<String, String> map = Utils.getRequestParams(request);
+            payNotifyBizz.webankAlipayNotify(map);
         } catch (Exception e) {
             e.printStackTrace();
+            LOGGER.info("微众支付宝扫码支付异步通知处理异常");
         }
+
     }
 }
