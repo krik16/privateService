6245c1ac54badb63fb74bb3880f14c109e7f7354
chenjun <chenjun@rongyi.com>
2017-04-10 15:43:41 +0800

    修改申诉参数

--- easy-common/easy-entity/src/main/java/com/rongyi/easy/tradecenter/param/ComplaintParam.java
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/tradecenter/param/ComplaintParam.java
@@ -1,6 +1,7 @@
 package com.rongyi.easy.tradecenter.param;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * 申诉参数
@@ -9,19 +10,22 @@ import java.io.Serializable;
  * @date 2017-04-10
  **/
 public class ComplaintParam implements Serializable {
+
+    // 用户id
+    private String memberId;
     // 退款单号
     private String refundNo;
     // 申诉原因
     private String remark;
     // 申诉凭证（多个用逗号分隔）
-    private String imageUrl;
+    private List<String> pics;
 
-    public String getImageUrl() {
-        return imageUrl;
+    public List<String> getPics() {
+        return pics;
     }
 
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
+    public void setPics(List<String> pics) {
+        this.pics = pics;
     }
 
     public String getRefundNo() {
@@ -40,12 +44,21 @@ public class ComplaintParam implements Serializable {
         this.remark = remark;
     }
 
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
     @Override
     public String toString() {
         return "ComplaintParam{" +
-                "imageUrl='" + imageUrl + '\'' +
+                "pics='" + pics + '\'' +
                 ", refundNo='" + refundNo + '\'' +
                 ", remark='" + remark + '\'' +
+                ", memberId='" + memberId + '\'' +
                 '}';
     }
 }
