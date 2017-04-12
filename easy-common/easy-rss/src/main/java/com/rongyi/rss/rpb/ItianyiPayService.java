f803dfdcc017ed9f2e420708f02e3f13ce84718b
kejun <kejun@rongyi.com>
2017-04-12 14:17:49 +0800

    退款接口

--- easy-common/easy-rss/src/main/java/com/rongyi/rss/rpb/ItianyiPayService.java
+++ easy-common/easy-rss/src/main/java/com/rongyi/rss/rpb/ItianyiPayService.java
@@ -1,9 +1,21 @@
 package com.rongyi.rss.rpb;
 
+import com.rongyi.core.Exception.TradePayException;
+
+import java.util.Map;
+
 /**
  * 天翼支付服务接口
  * Created by sujuan on 2017/4/11.
  */
 public interface ItianyiPayService {
+
+    /**
+     * 普通支付退款
+     * orderNo 订单号
+     * refundAmount 退款金额(单位分)
+     * wechatConfigureVo 支付参数
+     **/
+    Map<String, Object> refund(String orderNo, int refundAmount) throws TradePayException;
 
 }
