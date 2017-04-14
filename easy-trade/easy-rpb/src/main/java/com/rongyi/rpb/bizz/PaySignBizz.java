2471884c5e588a1adfdb3af283f6f08854c6cb53
kejun <kejun@rongyi.com>
2017-04-14 18:28:19 +0800

    天翼支付已存在时更新支付流水号，防止重复提示

--- easy-trade/easy-rpb/src/main/java/com/rongyi/rpb/bizz/PaySignBizz.java
+++ easy-trade/easy-rpb/src/main/java/com/rongyi/rpb/bizz/PaySignBizz.java
@@ -5,7 +5,6 @@ import com.rongyi.easy.rpb.vo.RyMchVo;
 import com.rongyi.easy.ryoms.entity.WechatTianyiPayVo;
 import com.rongyi.pay.core.ali.config.AliConfigure;
 import com.rongyi.pay.core.ali.model.reqData.AliScanPayReqData;
-import com.rongyi.pay.core.tianyi.param.TianyiOrderParam;
 import com.rongyi.pay.core.tianyi.param.TianyiParam;
 import com.rongyi.pay.core.unit.AliPayUnit;
 import com.rongyi.pay.core.unit.TianyiPayUnit;
@@ -23,6 +22,7 @@ import com.rongyi.rpb.constants.Constants;
 import com.rongyi.rpb.unit.PayConfigInitUnit;
 import com.rongyi.rpb.unit.SaveUnit;
 import com.rongyi.rss.malllife.service.IRedisService;
+import com.rongyi.rss.rpb.OrderNoGenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -42,6 +42,8 @@ public class PaySignBizz extends BaseBizz{
     IRedisService redisService;
     @Autowired
     PayConfigInitUnit payConfigInitUnit;
+    @Autowired
+    OrderNoGenService orderNoGenService;
 
     /**
      * 微信扫码支付签名
@@ -150,7 +152,7 @@ public class PaySignBizz extends BaseBizz{
 
     /**
      * 翼支付获取唤起H5收银台url
-     * @param tianyiOrderParam 业务请求参数
+     * @param tianyiParam 业务请求参数
      * @param orderType 订单类型
      * @param source 来源
      * @return 唤起h5收银台url
@@ -159,6 +161,9 @@ public class PaySignBizz extends BaseBizz{
         //初始化支付记录
         PaymentEntity paymentEntity = initPaymentEntity(wechatTianyiPayVo, tianyiParam.getTianyiOrderParam().getOrderSeq(), Integer.parseInt(tianyiParam.getTianyiOrderParam().getOrderAmt()),
                 Constants.PAYMENT_PAY_CHANNEL.PAY_CHANNEL6, orderType, ConstantEnum.PAY_SCENE_SCAN.getCodeInt(), source);
+        if(paymentEntity.getId() != null){
+            paymentEntity.setPayNo(orderNoGenService.getOrderNo("0"));
+        }
         tianyiParam.getTianyiOrderParam().setOrderReqTranseq(paymentEntity.getPayNo());
         tianyiParam.getPayDetailParam().setOrderReqTranseq(paymentEntity.getPayNo());
         //获取h5支付url
