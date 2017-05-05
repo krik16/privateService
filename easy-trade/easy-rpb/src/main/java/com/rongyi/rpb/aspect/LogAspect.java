95bde359f824083a7dc61b7df4f5abd011c0cc00
kejun <kejun@rongyi.com>
2017-05-05 10:57:09 +0800

    第三方渠道增加日志耗时aop

---
+++ easy-trade/easy-rpb/src/main/java/com/rongyi/rpb/aspect/LogAspect.java
@@ -0,0 +1,32 @@
+package com.rongyi.rpb.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LogAspect {
+
+	private static final Logger log = LoggerFactory.getLogger(LogAspect.class);
+
+	@Pointcut("execution(* com.rongyi.pay.core.unit..*(..)) ")
+	public void aspect() {
+
+	}
+
+
+	//声明环绕通知
+	@Around("aspect()")
+	public Object doAround(ProceedingJoinPoint pjp) throws Throwable {
+		long begin = System.nanoTime();
+		Object o = pjp.proceed();
+		long end = System.nanoTime();
+		log.info("{}:{}",pjp.getTarget().getClass()+"."+pjp.getSignature().getName(),(end-begin)/1000000);
+		return o;
+	}
+}
