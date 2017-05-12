67948282e46ce2506092803ca9791db2a2337481
gitwudi <490840770@qq.com>
2017-05-12 15:34:16 +0800

    pos微众银行-上传对账单

---
+++ easy-common/easy-entity/src/main/java/com/rongyi/easy/rpb/dto/SendDto.java
@@ -0,0 +1,40 @@
+package com.rongyi.easy.rpb.dto;
+
+import java.util.List;
+import java.util.Set;
+
+public class SendDto extends BaseDto {
+
+	private static final long serialVersionUID = -2747523528086825257L;
+
+	private List<String> fileNameList;
+
+	private String fileName;
+
+	private Set<String> toAddsSet;
+
+	public List<String> getFileNameList() {
+		return fileNameList;
+	}
+
+	public void setFileNameList(List<String> fileNameList) {
+		this.fileNameList = fileNameList;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public Set<String> getToAddsSet() {
+		return toAddsSet;
+	}
+
+	public void setToAddsSet(Set<String> toAddsSet) {
+		this.toAddsSet = toAddsSet;
+	}
+
+}
