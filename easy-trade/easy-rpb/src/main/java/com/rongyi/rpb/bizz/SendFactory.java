ff2e2a44a7ecbbb26559cd7ff98c04437de9bd50
zengzhimin <zengzhimin@rongyi.com>
2017-05-19 11:57:10 +0800

    启动报错

--- easy-trade/easy-rpb/src/main/java/com/rongyi/rpb/bizz/SendFactory.java
+++ easy-trade/easy-rpb/src/main/java/com/rongyi/rpb/bizz/SendFactory.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class SendFactory {
 
+	@Autowired
 	private static SendMail sendMail;
 
 	public static SendProvider sendMail() {
