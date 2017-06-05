9bd65020f701ecde23fc9106a006ded0cdb0a7e3
shaozhou <shaozhou@.rongyi.com>
2017-06-05 19:40:36 +0800

    返回请求

--- easy-trade/easy-rpb/src/main/java/com/rongyi/rpb/web/controller/v6/WebankController.java
+++ easy-trade/easy-rpb/src/main/java/com/rongyi/rpb/web/controller/v6/WebankController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -83,6 +84,7 @@ public class WebankController {
     }
 
     @RequestMapping("/statementDown")
+    @ResponseBody
     public ResponseVO statementDown(HttpServletRequest request) {
         LOGGER.info("微众对账单通知下载");
         try {
